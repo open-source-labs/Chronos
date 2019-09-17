a2081b3f2f9aeb81ec0d241a598ae9d58d0e414c
Michelle Herrera <mesherrera@aol.com>
2019-09-17 10:44:54 -0400

    Initial setup for managing user experience with app. Conditionals used to control user experience based on state at start.

--- app/index.js
+++ app/index.js
@@ -2,14 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import SignUp from "./components/SignUp.jsx";
+import ServiceOverview from "./components/ServiceOverview.jsx";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // const stateData = fs.readFileSync('../user/start.json', (err, data) => {
+    //   if(err) console.log(err);
+    //   return data.json();
+    // });
+    // this.state = stateData;
+    this.state = {
+      setupRequired: true,
+      services: [],
+      database: '',
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <SignUp />
-      </div>
-    );
+    const { setupRequired } = this.state;
+    if(setupRequired) return <SignUp />;
+    return <ServiceOverview />;
   }
 }
 
