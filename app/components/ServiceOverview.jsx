e776210f88784f7a9c5920460be24a358f26355b
Michelle Herrera <mesherrera@aol.com>
2019-09-17 10:44:54 -0400

    Initial setup for managing user experience with app. Conditionals used to control user experience based on state at start.

---
+++ app/components/ServiceOverview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ServiceOverview extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>HELLOOOOOOOO?!?</h1>
+      </div>
+    );
+  }
+}
+
+export default ServiceOverview;
