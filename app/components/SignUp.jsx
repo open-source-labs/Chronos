ae1258ca52c00e9599b826585fd82fdaf7127742
Michelle Herrera <mesherrera@aol.com>
2019-09-17 10:42:59 -0400

    Used React Hooks to create a dynamic for user setup.
    Co-authored-by: Duane McFarlane duane11003

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -1,17 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
+const uuidv1 = require('uuid/v1');
 
-class SignUp extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Enter Microservices URL</h1>
-        <input placeholder="Microservices URL"></input>
-        <input placeholder="Label"></input>
-        <input placeholder="Database URL"></input>
-        <button>Submit</button>
-      </div>
-    );
+const SignUp = () => {
+  const blankEntry = () => {
+    const entry = {
+      label: '',
+      url: '',
+      id: '',
+    };
+    return entry;
   }
+  const startState = [new blankEntry(), new blankEntry(), new blankEntry()];
+  const [formState, setFormState] = useState(startState);
+  const [dbState, setdbState] = useState('');
+
+  const addEntry = () => {
+    event.preventDefault();
+    setFormState([...formState, new blankEntry]);
+  };
+
+  const deleteEntry = (id) => {
+    event.preventDefault();
+    const adjustedState = formState.filter((entry) => {
+      return entry.id !== id
+    })
+    setFormState([...adjustedState]);
+  }
+
+  return (
+    <div>
+      <h1>Setup Your Microservices</h1>
+      <form>
+        {
+          formState.map((val, idx) => {
+            const keyID = uuidv1()
+            val.id = keyID;
+            return(
+              <div key={`${keyID}`}>
+                <button listid={`${keyID}`} onClick={() => deleteEntry(keyID)}>X</button>
+                <input
+                  onChange = {(e) => {
+                    val.label = e.target.value 
+                  }}
+                  type='text'
+                  placeholder={val.label}
+                />
+                <input
+                  onChange = {(e) => {
+                    val.url = e.target.value
+                  }}
+                  type='text'
+                  placeholder={val.url}
+                />
+              </div>
+           )
+          })
+        }
+        <input
+          onChange={(e) => {
+            setdbState(e.target.value) 
+          }}
+          placeholder={dbState}
+        />
+        <br></br>
+        <button onClick={addEntry}>Add Service</button>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default SignUp;
