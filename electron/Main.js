57d5def75c4cfcb7ca02ce1591d65541177d9cfe
Michelle Herrera <mesherrera@aol.com>
2019-09-18 22:13:00 -0400

    Established IPC connection with the render process to return state.

--- electron/Main.js
+++ electron/Main.js
@@ -1,27 +1,45 @@
-const { app, BrowserWindow } = require('electron');
-let win;
-function createWindow (){
-   win = new BrowserWindow({
-       width: 900,
-       height: 700,
-       webPreferences: {
-           nodeIntegration: true
-       }
-   })
-   win.loadURL('http://localhost:8080/')
-   win.on('closed', () => {
-       win = null
-   })
-}
-app.on('ready', createWindow)
-app.on('window-all-closed', () => {
-   if (process.platform !== 'darwin') {
-       app.quit()
-   }
-})
-app.on('activate', () => {
-   if (win === null) {
-       createWindow()
-   }
-})
+const { app, BrowserWindow, ipcMain } = require('electron');
+const fs = require('fs');
+const path = require('path');
+require('../server/server.js');
 
+let win;
+function createWindow() {
+  win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  win.loadURL('http://localhost:8081/');
+  ipcMain.on('state', (message) => {
+    const state = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'), {
+      encoding: 'UTF-8',
+    });
+    // rendererWindow.webContents.send(state);
+    message.returnValue = state;
+    console.log(state);
+  });
+  ipcMain.on('submit', (message, state) => {
+    console.log(state, ' is the message');
+    fs.writeFileSync(path.resolve(__dirname, '../user/settings.json'), state);
+    const updatedState = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'));
+    message.returnValue = updatedState;
+    console.log('state from main.js');
+  });
+  win.on('closed', () => {
+    win = null;
+  });
+}
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
