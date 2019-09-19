245ab23aa856bc52b56a8bb3404b6a6f41a91211
Natalie <44040061+nmczormick@users.noreply.github.com>
2019-09-19 10:47:46 -0400

    Add files via upload
    Express middleware to intercept microservice communications and push to mongo database

---
+++ middleware.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+app.use(bodyParser.json());
+
+//Required to get rid of deprecation warnings
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useNewUrlParser", true);
+
+  module.exports = function(req,res,next) {
+    return function (req,res,next) {
+      mongoose.connect(
+        "mongodb+srv://numanzor:Nu121692.@microservice-tutorial-hq75f.mongodb.net/chronos-access",
+        () => {
+          console.log("Chronos database is connected...");
+        }
+      )
+      const currentMicroservicePath = path.resolve(__dirname);
+
+      require('./HealthInfo.js')
+      const HealthInfo = mongoose.model("HealthInfo")
+
+        const newHealthPoint = {
+          currentMicroservice: currentMicroservicePath,
+          targetedEndpoint: req.originalUrl,
+          reqType: req.method,
+          timeSent: Date.now()
+        };
+
+        const healthPoint = new HealthInfo(newHealthPoint);
+
+        healthPoint.save().then(()=> {
+          console.log('New Health Point Created')
+        }).catch((err) => {
+          if (err) {
+            throw err;
+          }
+        })
+        next();
+    }
+  }
