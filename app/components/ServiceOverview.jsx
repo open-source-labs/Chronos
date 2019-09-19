011a548091d99b70661e6d674cfa90c0e4921a34
Michelle Herrera <mesherrera@aol.com>
2019-09-19 11:27:32 -0400

    Created reset functionality to restore state back to initial set up.
    Co-authored-by: Duane McFarlane duane11003

--- app/components/ServiceOverview.jsx
+++ app/components/ServiceOverview.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+const { ipcRenderer } = window.require('electron');
+
+// needs to be revised.
 class ServiceOverview extends React.Component {
   render() {
+  // IPC communication used to reset settings JSON back to initial state. 
+    const reset = () => {
+      event.preventDefault();
+      const state = {
+        setupRequired: true,
+        services: [],
+        database: '',
+      };
+      const resetState = ipcRenderer.sendSync('submit', JSON.stringify(state));
+    };
+    
     return (
       <div>
         <h1>HELLOOOOOOOO?!?</h1>
+        <button onClick={reset}>Reset</button>
+        <button>Fetch</button>
       </div>
     );
   }
