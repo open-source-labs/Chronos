3c0b12a632ab18d2221d48610435576a28a1bbb0
Michelle Herrera <mesherrera@aol.com>
2019-09-21 10:49:46 -0400

    Began working IPC listener to query database.
    Co-authored-by: Duane McFarlane duane11003

--- electron/Main.js
+++ electron/Main.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
+// const https = require('https');
 const path = require('path');
-require('../server/server.js');
+// const { Pool, Client } = require('pg');
+// const mongoose = require('mongoose');
+// const HealthInfoA = require('./InfoSchema');
+// require('../server/server.js');
 
 let win;
 function createWindow() {
@@ -20,7 +24,6 @@ function createWindow() {
       encoding: 'UTF-8',
     });
     message.returnValue = state;
-    console.log(state);
   });
 
   // Load settings JSON and returns updated state back to the render process.
@@ -29,18 +32,56 @@ function createWindow() {
     const updatedState = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'));
     message.returnValue = updatedState;
   });
+
+  ipcMain.on('all', (message, db) => {
+    mongoose.connect(db, { useNewUrlParser: true }, () => {
+      console.log('Connected!');
+      console.log(db);
+      // console.log('what da hell am i ', HealthInfoA.find({}));
+
+      HealthInfoA.find({}, (err, doc) => {
+        console.log('error =>', err);
+        console.log('data', doc);
+      });
+      // HealthInfo.find().then((data) => {
+      //   console.log('HELLLLLLLLLLLOOOOOOOO!');
+      //   console.log('inside query');
+      //   message.reply('queryResponse', data);
+      // });
+    });
+  });
+
   win.on('closed', () => {
     win = null;
   });
 }
 app.on('ready', createWindow);
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
 app.on('activate', () => {
   if (win === null) {
     createWindow();
   }
 });
+
+// console.log('HELLLLLLLLO?');
+// console.log(db);
+// const server = https.createServer();
+
+// SQL
+// const pool = new Pool({
+//   connectionString: db,
+// });
+
+// message.sender.send('queryResponse',
+//   pool.query('SELECT * FROM message').then((err, data) => {
+//     if (err) console.log(err);
+//     const stuff = data.json();
+//     return stuff;
+//   }),
+// );
