b0c9f09ff7064a40799bb29ef40c09dac2bff066
Michelle Herrera <mesherrera@aol.com>
2019-09-26 15:51:08 -0400

    Established IPC communication to send and recieve query requests in order to render microservice communications queries.
    Co-authored-by: Duane McFarlane duane11003

--- app/components/ServiceOverview.jsx
+++ app/components/ServiceOverview.jsx
@@ -1,68 +1,33 @@
+import React from 'react';
+
 const { ipcRenderer } = window.require('electron');
 
-import React from 'react';
 
 class ServiceOverview extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: {}
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    ipcRenderer.send('queryRequest');
+    // IPC communication used to initiate query for information on microservices.
+    const messageToMain = ipcRenderer.send('queryRequest');
+    // IPC listener responsible for retrieving infomation from asynchronous main process message.
     ipcRenderer.on('queryResponse', (event, data) => {
-      console.log('in ipcRender')
-      // console.log(JSON.parse(data));
-      const potato = JSON.parse(data)
-      console.log('potato => ', [potato])
-      
-      this.setState({ data: JSON.parse(data) });
-      // return potato;
+      // Adds microservice data to state.
+      this.setState({ ...JSON.parse(data) });
     });
-    // console.log('QUERY RETURNED: ', now);
-    // this.setState({ queryResults: results });
-    console.log('state', this.state)
   }
 
-  render() {
+  render () {
     return (
       <div>
-        <h1>HELLOOOOOOOO?!?</h1>
-        {/* {this.state.queryResults.toString()} */}
+        <h1>Services Overview</h1>
+        <p>{JSON.stringify(this.state)}</p>
       </div>
     );
   }
 }
 
-// // needs to be revised.
-// const ServiceOverview = () => {
-//   // IPC communication used to reset settings JSON back to initial state.
-//   const reset = () => {
-//     event.preventDefault();
-//     const state = {
-//       setupRequired: true,
-//       services: [],
-//       database: '',
-//     };
-//     const resetState = ipcRenderer.sendSync('submit', JSON.stringify(state));
-//   };
-
-//   // const fetch = () => {
-//   //   event.preventDefault();
-//   //   console.log(this.props.db)
-//   //   const data = ipcRenderer.sendSync('getAll');
-//   //   console.log(data);
-//   // }
-
-//   return (
-//     <div>
-//       <h1>HELLOOOOOOOO?!?</h1>
-//       <button onClick={reset}>Reset</button>
-//       {/* <button onClick={fetch}>Fetch</button> */}
-//     </div>
-//   );
-// };
 
 export default ServiceOverview;
