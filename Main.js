b38b8698689e1d058aeb2c24e3e729770b612fad
Michelle Herrera <mesherrera@aol.com>
2019-09-28 12:41:21 -0400

    Modified file structure to align with Spectron docs.

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
 const path = require('path');
-const HealthInfoModel = require('./InfoSchema');
+const HealthInfoModel = require('./model/InfoSchema');
 
 let win;
 function createWindow() {
@@ -39,7 +39,7 @@ app.on('activate', () => {
 
 // Load settings JSON and returns state back to the render process.
 ipcMain.on('state', (message) => {
-  const state = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'), {
+  const state = fs.readFileSync(path.resolve(__dirname, './user/settings.json'), {
     encoding: 'UTF-8',
   });
   message.returnValue = state;
@@ -47,30 +47,35 @@ ipcMain.on('state', (message) => {
 
 // Load settings JSON and returns updated state back to the render process.
 ipcMain.on('submit', (message, state) => {
-  fs.writeFileSync(path.resolve(__dirname, '../user/settings.json'), state);
-  const updatedState = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'));
+  fs.writeFileSync(path.resolve(__dirname, './user/settings.json'), state);
+  const updatedState = fs.readFileSync(path.resolve(__dirname, './user/settings.json'));
   message.returnValue = updatedState;
 });
 
 // Queries the database for information and returns it back to the render process.
 ipcMain.on('overviewRequest', (message) => {
-  HealthInfoModel.find({}).select({ _id: 0, currentMicroservice: 1, targetedEndpoint: 1 }).exec((err, data) => {
-    if (err) console.log(`An error occured while querying the database: ${err}`);
-    const queryResults = JSON.stringify(data);
-    // Asynchronous event emitter used to transmit query results back to the render process.
-    message.sender.send('overviewResponse', queryResults);
-  });
+  HealthInfoModel.find({})
+    .select({ _id: 0, currentMicroservice: 1, targetedEndpoint: 1 })
+    .exec((err, data) => {
+      if (err) {
+        console.log(`An error occured while querying the database: ${err}`);
+        message.sender.send('overviewResponse', null);
+      }
+      const queryResults = JSON.stringify(data);
+      // Asynchronous event emitter used to transmit query results back to the render process.
+      message.sender.send('overviewResponse', queryResults);
+    });
 });
 
 // SQL
 // const pool = new Pool({
-  //   connectionString: db,
-  // });
-  
-  // message.sender.send('queryResponse',
-  //   pool.query('SELECT * FROM message').then((err, data) => {
-    //     if (err) console.log(err);
-    //     const stuff = data.json();
-    //     return stuff;
-    //   }),
-    // );
+//   connectionString: db,
+// });
+
+// message.sender.send('queryResponse',
+//   pool.query('SELECT * FROM message').then((err, data) => {
+//     if (err) console.log(err);
+//     const stuff = data.json();
+//     return stuff;
+//   }),
+// );
