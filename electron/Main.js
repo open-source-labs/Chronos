7c6db35219fca0f340cdef2ffd9d9aa539f1dd11
Michelle Herrera <mesherrera@aol.com>
2019-09-28 11:36:11 -0400

    Reorganized IPC event listeners. Modified mongoose query (overviewRequest) to select only the specific fields needed for frontend functionality. Potentially fixed production mode configuration.

--- electron/Main.js
+++ electron/Main.js
@@ -2,7 +2,6 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
 const path = require('path');
 const HealthInfoModel = require('./InfoSchema');
-// require('../server/server.js');
 
 let win;
 function createWindow() {
@@ -13,39 +12,12 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
+
+  // Development
   win.loadURL('http://localhost:8080/');
 
-  // Load settings JSON and returns state back to the render process.
-  ipcMain.on('state', (message) => {
-    const state = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'), {
-      encoding: 'UTF-8',
-    });
-    message.returnValue = state;
-  });
-
-  // Load settings JSON and returns updated state back to the render process.
-  ipcMain.on('submit', (message, state) => {
-    fs.writeFileSync(path.resolve(__dirname, '../user/settings.json'), state);
-    const updatedState = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'));
-    message.returnValue = updatedState;
-  });
-
-  // Queries the database for information and returns it back to the render process.
-  ipcMain.on('overviewRequest', (message) => {
-    console.log('Hello');
-    HealthInfoModel.find({}).select({ _id: 0, currentMicroservice: 1, targetedEndpoint: 1 }).exec((err, data) => {
-      if (err) console.log(`An error occured while querying the database: ${err}`);
-      const queryResults = JSON.stringify(data);
-      // Asynchronous event emitter used to transmit query results back to the render process.
-      message.sender.send('overviewResponse', queryResults);
-    });
-    // HealthInfoModel.find({}, (err, data) => {
-    //   if (err) console.log(`An error occured while querying the database: ${err}`);
-    //   const queryResults = JSON.stringify(data);
-    //   // Asynchronous event emitter used to transmit query results back to the render process.
-    //   message.sender.send('queryResponse', queryResults);
-    // });
-  });
+  // Production
+  // win.loadURL(`file://${path.join(__dirname, './dist/index.html')}`);
 
   win.on('closed', () => {
     win = null;
@@ -65,15 +37,40 @@ app.on('activate', () => {
   }
 });
 
+// Load settings JSON and returns state back to the render process.
+ipcMain.on('state', (message) => {
+  const state = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'), {
+    encoding: 'UTF-8',
+  });
+  message.returnValue = state;
+});
+
+// Load settings JSON and returns updated state back to the render process.
+ipcMain.on('submit', (message, state) => {
+  fs.writeFileSync(path.resolve(__dirname, '../user/settings.json'), state);
+  const updatedState = fs.readFileSync(path.resolve(__dirname, '../user/settings.json'));
+  message.returnValue = updatedState;
+});
+
+// Queries the database for information and returns it back to the render process.
+ipcMain.on('overviewRequest', (message) => {
+  HealthInfoModel.find({}).select({ _id: 0, currentMicroservice: 1, targetedEndpoint: 1 }).exec((err, data) => {
+    if (err) console.log(`An error occured while querying the database: ${err}`);
+    const queryResults = JSON.stringify(data);
+    // Asynchronous event emitter used to transmit query results back to the render process.
+    message.sender.send('overviewResponse', queryResults);
+  });
+});
+
 // SQL
 // const pool = new Pool({
-//   connectionString: db,
-// });
-
-// message.sender.send('queryResponse',
-//   pool.query('SELECT * FROM message').then((err, data) => {
-//     if (err) console.log(err);
-//     const stuff = data.json();
-//     return stuff;
-//   }),
-// );
+  //   connectionString: db,
+  // });
+  
+  // message.sender.send('queryResponse',
+  //   pool.query('SELECT * FROM message').then((err, data) => {
+    //     if (err) console.log(err);
+    //     const stuff = data.json();
+    //     return stuff;
+    //   }),
+    // );
