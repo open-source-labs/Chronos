060b5e4aa06339a665f9ecb2747c5dfd476be91b
Michelle Herrera <mesherrera@aol.com>
2019-10-02 11:55:11 -0400

    Modified submit listner to support multiple microservice entries.

--- Main.js
+++ Main.js
@@ -1,8 +1,9 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
 const path = require('path');
-// const DatabaseController = require('./model/database-controller');
 const CommunicationSchema = require('./model/mongoose-communicatonSchema');
+// const HealthInfoSchema = require('./model/mongoose-healthInfoShema');
+const pool = require('./model/sql-connect');
 
 let win;
 function createWindow() {
@@ -38,8 +39,8 @@ app.on('activate', () => {
   }
 });
 
-// Load settings JSON and returns state back to the render process.
-ipcMain.on('state', (message) => {
+// Load settings JSON and returns current setup status back to the render process.
+ipcMain.on('setup', (message) => {
   const state = JSON.parse(fs.readFileSync(path.resolve(__dirname, './user/settings.json'), {
     encoding: 'UTF-8',
   }));
@@ -47,11 +48,35 @@ ipcMain.on('state', (message) => {
   message.returnValue = setupRequired;
 });
 
+// Loads existing settings JSON and update settings to include new services entered by the user.
+ipcMain.on('submit', (message, newService) => {
+  const state = JSON.parse(fs.readFileSync(path.resolve(__dirname, './user/settings.json'), {
+    encoding: 'UTF-8',
+  }));
+  // if statement is used to remove hard coded data.
+  if (state.michelleWasHere) {
+    state.setupRequired = false;
+    state.michelleWasHere = false;
+    state.services = [JSON.parse(newService)];
+    fs.writeFileSync(path.resolve(__dirname, './user/settings.json'), JSON.stringify(state));
+  } else {
+    state.setupRequired = false;
+    state.services.push(JSON.parse(newService));
+    fs.writeFileSync(path.resolve(__dirname, './user/settings.json'), JSON.stringify(state));
+  }
+});
+
 // Load settings JSON and returns updated state back to the render process.
-ipcMain.on('submit', (message, state) => {
-  fs.writeFileSync(path.resolve(__dirname, './user/settings.json'), state);
-  const updatedState = fs.readFileSync(path.resolve(__dirname, './user/settings.json'), { encoding: 'UTF-8' });
-  message.returnValue = JSON.parse(updatedState).setupRequired;
+ipcMain.on('dashboard', (message) => {
+  const state = JSON.parse(fs.readFileSync(path.resolve(__dirname, './user/settings.json'), {
+    encoding: 'UTF-8',
+  }));
+  const { services } = state;
+  const dashboardList = services.reduce((acc, curVal) => {
+    acc.push(curVal[0]);
+    return acc;
+  }, []);
+  message.returnValue = dashboardList;
 });
 
 // Queries the database for information and returns it back to the render process.
@@ -70,7 +95,19 @@ ipcMain.on('overviewRequest', (message) => {
         message.sender.send('overviewResponse', queryResults);
       });
   }
-  // SQL Query
+  if (databaseType === 'SQL') {
+    const getCommunications = 'SELECT currentMicroservice, targetedEndpoint FROM communications';
+    pool.query(getCommunications, (err, result) => {
+      if (err) {
+        console.log(err);
+        message.sender.send(JSON.stringify('Database info could not be retreived.'));
+      }
+      const queryResults = JSON.stringify(result.rows);
+      console.log(result);
+      // Asynchronous event emitter used to transmit query results back to the render process.
+      message.sender.send('overviewResponse', queryResults);
+    });
+  }
 });
 
 // SQL
