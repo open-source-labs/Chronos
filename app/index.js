79130964b5d15a794bf9cf8818ad93b2c84ae878
Michelle Herrera <mesherrera@aol.com>
2019-10-02 16:56:09 -0400

    Added GettingStarted.

--- app/index.js
+++ app/index.js
@@ -2,13 +2,12 @@ import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import SetupContext from './context/SetupContext.js';
 import './index.css';
-import SignUp from './components/SignUp.jsx';
+import GettingStarted from './components/GettingStarted.jsx';
 import ServiceDashboard from './components/ServicesDashboard.jsx';
-// import ServiceOverview from './components/ServiceOverview.jsx';
 
 const App = () => {
   const chronosSetup = useContext(SetupContext);
-  return chronosSetup.setupRequired ? <SignUp /> : <ServiceDashboard />;
+  return chronosSetup.setupRequired ? <GettingStarted /> : <ServiceDashboard />;
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
