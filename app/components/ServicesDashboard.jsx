63b8c7810cb22ecf6aba40005d58a061a40bc158
Duane McFarlane <duanemcfarlane@yahoo.com>
2019-10-02 17:00:14 -0400

    added functinality to services dashboard where user clicks on their database on the left side, the list of microservices are displayed on the right side

--- app/components/ServicesDashboard.jsx
+++ app/components/ServicesDashboard.jsx
@@ -4,37 +4,62 @@ import SignUp from './SignUp.jsx';
 import DashboardContext from '../context/DashboardContext';
 import SetupContext from '../context/SetupContext'
 
+let clicked = false;
+function toggleClick(){
+  clicked = true;
+  // return clicked;
+}
+
+// console.log(toggleClick())
 const ServicesDashboard = () => {
   const renderServiceList = (context) => {
     const buttonStore = [];
     for (let i = 0; i < context.length; i += 1) {
-      console.log(context[i]);
+      console.log(context[i], ' is context');
       buttonStore.push(
-        <button
-          type="button"
-          key={`${i}${context[i]}`}
-          onClick={() => setSelection(<ServiceOverview index={i} />)}
+        <button className='microserviceBtn'
+        type="button"
+        key={`${i}${context[i]}`}
+        onClick={() => setSelection(<ServiceOverview index={i} />)
+        }
+        // onClick={() => toggleClick()}
         >
           { context[i] }
         </button>,
       );
     }
+    // console.log(clicked)
     return buttonStore;
   };
-  const [listState, setList] = useState(renderServiceList(useContext(DashboardContext)));
-  const [serviceSelected, setSelection] = useState();
 
-  if (serviceSelected) return serviceSelected;
+  console.log(clicked, 'clicked should be true here')
+  const [listState, setList] = useState(renderServiceList(useContext(DashboardContext)));
+  const [serviceSelected, setSelection,] = useState();
+
+
+  // if (serviceSelected) return serviceSelected;
   return (
-    <div>
-      <h3>Your Microservices</h3>
-      <div>{listState}</div>
-      <button
-        type="button"
-        onClick={() => setSelection(<SignUp />)}
-      >
-        Add Service
-      </button>
+    <div className='servicesDashboardContainer'>
+      <div className='left'>
+        <div className='leftTopContainer'>
+          <div className='left-top'>
+            <h2>Your Microservices</h2>
+            {listState}
+            {/* <div className='servicesList'>{listState}</div> */}
+          </div>
+        </div>
+        <div className='left-bottom'>
+          <button className='submitBtn'
+            type="button"
+            onClick={() => setSelection(<SignUp />)}>
+            Add Service
+          </button>
+          {/* <button>Add Microservice</button> */}
+        </div>
+      </div>
+      <div>
+        {serviceSelected}
+      </div>
     </div>
   );
 };
