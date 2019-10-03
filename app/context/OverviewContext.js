27c1170798ba64c52c11b2f9613635fc3319126a
Michelle Herrera <mesherrera@aol.com>
2019-10-03 11:00:39 -0400

    Edited context to share data collected from IPC communications

--- app/context/OverviewContext.js
+++ app/context/OverviewContext.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-const { ipcRenderer } = window.require('electron');
-
-const OverviewContext = React.createContext((index) => {
-  // IPC communication used to initiate query for information on microservices.
-  ipcRenderer.send('overviewRequest', index);
-
-  // IPC listener responsible for retrieving infomation from asynchronous main process message.
-  ipcRenderer.on('overviewResponse', (event, data) => Object.values(JSON.parse(data)));
-
-    return communications;
-  });
+const OverviewContext = React.createContext({
+  overviewData: null,
+  overviewStatus: true,
+  toggleOverview: (status) => {
+    if (status) return false;
+    return true;
+  },
 });
 
 export const OverviewProvider = OverviewContext.Provider;
