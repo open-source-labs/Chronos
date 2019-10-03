541958dcebbb782e7ebde12c6c663715e667ac59
Michelle Herrera <mesherrera@aol.com>
2019-10-03 11:49:05 -0400

    Importing charts components.

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
-import CommunicationsContext from '../context/OverviewContext';
+import HealthInformationContext from '../context/DetailsContext';
+import RequestTypesChart from '../charts/request-type-chart.jsx';
 
 const { ipcRenderer } = window.require('electron');
 
 const ServiceDetails = (props) => {
-  const communicationsData = useContext(CommunicationsContext);
+  const healthdata = useContext(HealthInformationContext);
   const [detailsState, setDetails] = useState();
+
   useEffect(() => {
     // IPC communication used to initiate query for information on microservices.
     ipcRenderer.send('detailsRequest', props.index);
@@ -13,10 +15,11 @@ const ServiceDetails = (props) => {
     // IPC listener responsible for retrieving infomation from asynchronous main process message.
     ipcRenderer.on('detailsResponse', (event, data) => {
       setDetails(Object.values(JSON.parse(data)));
+      healthdata.detailData = detailsState;
     });
   }, []);
 
-  return <h1>{props.index}</h1>;
+  return <div><RequestTypesChart /></div>;
 };
 
 export default ServiceDetails;
