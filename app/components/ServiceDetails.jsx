6a54f34d9e6f4a0ce62a87a1d7cde01f0d8ab7a5
Michelle Herrera <mesherrera@aol.com>
2019-10-03 17:00:23 -0400

    Removed unnecessary state hook. Fixed how data from IPC DetailsResponse was handled.

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -2,27 +2,24 @@ import React, { useEffect, useState, useContext } from 'react';
 import HealthInformationContext from '../context/DetailsContext';
 import RequestTypesChart from '../charts/request-type-chart.jsx';
 import ResponseCodesChart from '../charts/response-code-chart.jsx';
-import SpeedChart from '../charts/speed-chart.jsx';
+// import SpeedChart from '../charts/speed-chart.jsx';
+import MemoryChart from '../charts/memory-chart.jsx';
 
 const { ipcRenderer } = window.require('electron');
 
 const ServiceDetails = (props) => {
   const healthdata = useContext(HealthInformationContext);
-  const [detailsState, setDetails] = useState();
-
 
   useEffect(() => {
     // IPC communication used to initiate query for information on microservices.
     ipcRenderer.send('detailsRequest', props.index);
- 
+
     // IPC listener responsible for retrieving infomation from asynchronous main process message.
     ipcRenderer.on('detailsResponse', (event, data) => {
-      setDetails(Object.values(JSON.parse(data)));
-      healthdata.detailData = detailsState;
+      healthdata.detailData = [...JSON.parse(data)];
     });
   }, []);
- 
-  console.log(detailsState);
+
   return (
     <div>
       <div>
@@ -34,8 +31,8 @@ const ServiceDetails = (props) => {
         <ResponseCodesChart />
       </div>
       <div>
-        {/* <h3>Speed Chart</h3>
-        <SpeedChart /> */}
+        <h3>Memory Chart</h3>
+        <MemoryChart />
       </div>
     </div>
   );
