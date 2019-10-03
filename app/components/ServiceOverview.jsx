a9315ef55089aeae00a5d4a2393f7bc161497322
Michelle Herrera <mesherrera@aol.com>
2019-10-03 10:39:07 -0400

    Implemented on click functions to render details component.

--- app/components/ServiceOverview.jsx
+++ app/components/ServiceOverview.jsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Line, Bar, Pie } from 'react-chartjs-2';
-// import OverviewContext from '../context/OverviewContext';
+import OverviewContext from '../context/OverviewContext';
+import ServiceDetails from './ServiceDetails.jsx';
 
 const { ipcRenderer } = window.require('electron');
 
 const ServiceOverview = (props) => {
   const [overviewState, setOverviewState] = useState([]);
-  // const serviceComponents = useContext(OverviewContext);
+  const [detailsSelected, setDetails] = useState();
+  const serviceComponents = useContext(OverviewContext);
 
   useEffect(() => {
     // IPC communication used to initiate query for information on microservices.
     ipcRenderer.send('overviewRequest', props.index);
 
     // IPC listener responsible for retrieving infomation from asynchronous main process message.
-    ipcRenderer.on('overviewResponse', (event, data) => setOverviewState(Object.values(JSON.parse(data))));
+    ipcRenderer.on('overviewResponse', (event, data) => {
+      setOverviewState(Object.values(JSON.parse(data)));
+      serviceComponents.overviewData = Object.values(JSON.parse(data));
+    });
   }, []);
 
   const renderState = () => {
@@ -24,7 +29,15 @@ const ServiceOverview = (props) => {
       // SQL
       if (element.currentmicroservice) {
         if (!(element.currentmicroservice in serviceCache)) {
-          const button = <button>{element.currentmicroservice}</button>;
+          const button = (
+            <button
+              type="button"
+              key={`serviceItem${props.index}${i}`}
+              onClick={() => setDetails(<ServiceDetails index={props.index} />)}
+            >
+              {element.currentmicroservice}
+            </button>
+          );
           componentButtons.push(button);
           serviceCache[element.currentmicroservice] = 1;
         } else {
@@ -34,7 +47,15 @@ const ServiceOverview = (props) => {
         // Mongo
         if (element.currentMicroservice) {
           if (!(element.currentMicroservice in serviceCache)) {
-            const button = <button>{element.currentMicroservice}</button>;
+            const button = (
+              <button
+                type="button"
+                key={`serviceItem${props.index}${i}`}
+                onClick={() => setDetails(<ServiceDetails index={props.index} />)}
+              >
+                {element.currentMicroservice}
+              </button>
+            );
             componentButtons.push(button);
             serviceCache[element.currentMicroservice] = 1;
           } else {
@@ -80,6 +101,7 @@ const ServiceOverview = (props) => {
     return data;
   };
 
+  if (detailsSelected) return detailsSelected;
   return (
     <div className="mainContainer">
       <div>
