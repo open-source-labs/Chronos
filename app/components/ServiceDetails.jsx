a76c2e8047024eb1f341671569873c35b29f1484
Michelle Herrera <mesherrera@aol.com>
2019-10-03 15:56:47 -0400

    Added console.log statement for debugging.

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import HealthInformationContext from '../context/DetailsContext';
 import RequestTypesChart from '../charts/request-type-chart.jsx';
 import ResponseCodesChart from '../charts/response-code-chart.jsx';
+import SpeedChart from '../charts/speed-chart.jsx';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -9,17 +10,19 @@ const ServiceDetails = (props) => {
   const healthdata = useContext(HealthInformationContext);
   const [detailsState, setDetails] = useState();
 
+  
   useEffect(() => {
     // IPC communication used to initiate query for information on microservices.
     ipcRenderer.send('detailsRequest', props.index);
-
+    
     // IPC listener responsible for retrieving infomation from asynchronous main process message.
     ipcRenderer.on('detailsResponse', (event, data) => {
       setDetails(Object.values(JSON.parse(data)));
       healthdata.detailData = detailsState;
     });
   }, []);
-
+  
+  console.log(detailsState);
   return (
     <div>
       <div>
@@ -29,6 +32,10 @@ const ServiceDetails = (props) => {
       <div>
         <h3>Response Codes</h3>
         <ResponseCodesChart />
+      </div>
+      <div>
+        {/* <h3>Speed Chart</h3>
+        <SpeedChart /> */}
       </div>
     </div>
   );
