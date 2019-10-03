96ca38081b0b83305370af07ab59894f40f75540
Michelle Herrera <mesherrera@aol.com>
2019-10-02 23:34:27 -0400

    Refactored return.

--- model/sql-connect.js
+++ model/sql-connect.js
@@ -3,11 +3,9 @@ const { services } = require('../user/settings.json');
 
 const connectSQL = (i) => {
   const URI = services[i][2];
-  const pool = new Pool({
+  return new Pool({
     connectionString: URI,
   });
-
-  return pool;
 };
 
 module.exports = connectSQL;
