5d092eb199bb56861a6970a90d6583259fdacc0b
Michelle Herrera <mesherrera@aol.com>
2019-10-14 12:39:11 -0400

    Removed unnecessary bulk used for information gathering. Passing props.

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -1,25 +1,13 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import HealthInformationContext from '../context/DetailsContext';
 import RequestTypesChart from '../charts/request-type-chart.jsx';
 import ResponseCodesChart from '../charts/response-code-chart.jsx';
-// import SpeedChart from '../charts/speed-chart.jsx';
+import SpeedChart from '../charts/speed-chart.jsx';
 import MemoryChart from '../charts/memory-chart.jsx';
 
 const { ipcRenderer } = window.require('electron');
 
 const ServiceDetails = (props) => {
-  const healthdata = useContext(HealthInformationContext);
-
-  useEffect(() => {
-    // IPC communication used to initiate query for information on microservices.
-    ipcRenderer.send('detailsRequest', props.index);
-
-    // IPC listener responsible for retrieving infomation from asynchronous main process message.
-    ipcRenderer.on('detailsResponse', (event, data) => {
-      healthdata.detailData = [...JSON.parse(data)];
-    });
-  }, []);
-
   return (
     <div>
       <div>
@@ -32,7 +20,7 @@ const ServiceDetails = (props) => {
       </div>
       <div>
         <h3>Memory Chart</h3>
-        <MemoryChart />
+        <MemoryChart service={props.service} />
       </div>
     </div>
   );
