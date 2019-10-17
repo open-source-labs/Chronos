b106689b01f441a480a129a5eb4c3f27ee9ace91
Michelle Herrera <mesherrera@aol.com>
2019-10-16 20:41:42 -0400

    Prop drilled to communication charts. Added completed speed chart.

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -3,7 +3,7 @@ import HealthInformationContext from '../context/DetailsContext';
 import RequestTypesChart from '../charts/request-type-chart.jsx';
 import ResponseCodesChart from '../charts/response-code-chart.jsx';
 import SpeedChart from '../charts/speed-chart.jsx';
-import MemoryChart from '../charts/memory-chart.jsx';
+// import MemoryChart from '../charts/memory-chart.jsx';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -12,15 +12,15 @@ const ServiceDetails = (props) => {
     <div>
       <div>
         <h3>Request Types</h3>
-        <RequestTypesChart />
+        <RequestTypesChart service={props.service} />
       </div>
       <div>
-        <h3>Response Codes</h3>
-        <ResponseCodesChart />
+        <h3>Response Codes </h3>
+        <ResponseCodesChart service={props.service} />
       </div>
       <div>
         <h3>Memory Chart</h3>
-        <MemoryChart service={props.service} />
+        <SpeedChart service={props.service} />
       </div>
     </div>
   );
