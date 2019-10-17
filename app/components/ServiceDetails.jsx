167cbb39c461c91f3ad6f914f8558913e2e4dadd
Duane McFarlane <duanemcfarlane@yahoo.com>
2019-10-16 20:38:01 -0400

    added line chart for latency

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -4,6 +4,7 @@ import RequestTypesChart from '../charts/request-type-chart.jsx';
 import ResponseCodesChart from '../charts/response-code-chart.jsx';
 import SpeedChart from '../charts/speed-chart.jsx';
 import MemoryChart from '../charts/memory-chart.jsx';
+import LatencyChart from '../charts/latency-chart.jsx';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -21,6 +22,10 @@ const ServiceDetails = (props) => {
       <div>
         <h3>Memory Chart</h3>
         <MemoryChart service={props.service} />
+      </div>
+      <div>
+        <h3>Latency</h3>
+        <LatencyChart service={props.service}/>
       </div>
     </div>
   );
