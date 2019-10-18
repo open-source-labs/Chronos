3af21f98a20579baad2481e3aa8581bc0757374c
Michelle Herrera <mesherrera@aol.com>
2019-10-18 18:31:49 -0400

    Commented out unused import.

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -3,9 +3,9 @@ import RequestTypesChart from '../charts/request-type-chart.jsx';
 import ResponseCodesChart from '../charts/response-code-chart.jsx';
 import SpeedChart from '../charts/speed-chart.jsx';
 import ProcessesChart from '../charts/processes-chart.jsx';
-import MemoryChart from '../charts/memory-chart.jsx';
 import TemperatureChart from '../charts/temperature-chart.jsx'
 import LatencyChart from '../charts/latency-chart.jsx';
+// import MemoryChart from '../charts/memory-chart.jsx';
 
 const ServiceDetails = (props) => {
   return (
