1d3c3346a926ad37aa51a6e64491fa96946ace25
Michelle Herrera <mesherrera@aol.com>
2019-10-21 17:31:44 -0400

    Added comments.

--- app/context/DetailsContext.js
+++ app/context/DetailsContext.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Used to save query results. Laid foundation for conditional rendering similar to how SetupContext was used. 
 const DetailContext = React.createContext({
   detailData: null,
   detailSelected: null,
