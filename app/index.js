f456978247ca077d8cd71ab1e8348b9359763749
Michelle Herrera <mesherrera@aol.com>
2019-10-21 16:54:53 -0400

    Replaced GettingStarted with AddService.

--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import SetupContext from './context/SetupContext.js';
 import './index.css';
-import GettingStarted from './components/GettingStarted.jsx';
+// import GettingStarted from './components/GettingStarted.jsx';
+import AddService from './components/AddService.jsx'
 import ServiceDashboard from './components/ServicesDashboard.jsx';
 
 const App = () => {
   const chronosSetup = useContext(SetupContext);
-  return chronosSetup.setupRequired ? <GettingStarted /> : <ServiceDashboard />;
+  return chronosSetup.setupRequired ? <AddService /> : <ServiceDashboard />;
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
