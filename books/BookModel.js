c87a91e25b5146e0d8585141908f0531c019ffe8
Tim <tim.atapagra@gmail.com>
2019-12-18 14:27:18 -0500

    Books Model updated

--- books/BookModel.js
+++ books/BookModel.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 //  database link
+//  can change but woun't advice that because the book application database lives here
 const myURI = 'mongodb+srv://tim:tim@cluster0-khxef.mongodb.net/test?retryWrites=true&w=majority';
 
 const URI = process.env.MONGO_URI || myURI;
@@ -34,56 +35,7 @@ const BooksSchema = new Schema({
   },
 });
 
+// Database Model
 const BookModel = mongoose.model('BookModel', BooksSchema);
 
 module.exports = BookModel;
-
-
-// mongoose.model('Book', {
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   author: {
-//     type: String,
-//     required: true,
-//   },
-//   numberPages: {
-//     type: Number,
-//     required: false,
-//   },
-//   publisher: {
-//     type: String,
-//     required: false,
-//   },
-// });
-
-
-// const mongoose = require('mongoose');
-
-// const { Schema } = mongoose;
-// // v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
-
-// const myURI = 'mongodb+srv://tim:tim@cluster0-aady4.mongodb.net/test?retryWrites=true&w=majority';
-
-
-// // UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
-
-// mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to DB! *********'))
-//   .catch((err) => console.log('Connection Error ', err));
-
-// const messageSchema = new Schema({
-//   message: { type: String },
-//   password: { type: String },
-//   created_at: { type: Date, default: Date.now },
-// });
-
-// const Message = mongoose.model('Message', messageSchema);
-
-// // UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
-// // const URI = process.env.PG_URI || myURI;
-
-
-// module.exports = Message; // <-- export your model
