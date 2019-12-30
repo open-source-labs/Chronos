e9bc9470571815b3c9e365c53e77ecb25e374467
Ousman <ordiallo@gmail.com>
2019-12-30 12:14:20 -0500

    Ousman front end changes

--- app/components/ServiceOverview.jsx
+++ app/components/ServiceOverview.jsx
@@ -1,64 +1,99 @@
 import React, { useState, useEffect, useContext } from 'react';
 import OverviewContext from '../context/OverviewContext';
+import HealthInformationContext from '../context/DetailsContext';
 import ServiceDetails from './ServiceDetails.jsx';
 
 const { ipcRenderer } = window.require('electron');
 
 const ServiceOverview = (props) => {
+  // Overview state used to create service buttons
   const [overviewState, setOverviewState] = useState([]);
-  const [detailsSelected, setDetails] = useState();
-  const serviceComponents = useContext(OverviewContext);
 
+  // Details state used to cause rerender on user selection.
+  const [detailsSelected, setDetails] = useState();
+
+  // Contexts have data added to them following successful IPC return. Data is later used to create charts.
+  const serviceComponents = useContext(OverviewContext);
+  const healthdata = useContext(HealthInformationContext);
+
+  // Only happens when the component mounts.
   useEffect(() => {
     // IPC communication used to initiate query for information on microservices.
     ipcRenderer.send('overviewRequest', props.index);
 
     // IPC listener responsible for retrieving infomation from asynchronous main process message.
     ipcRenderer.on('overviewResponse', (event, data) => {
+      // Adds to state and context.
+      console.log('THIS IS ARRAY IN OVERVIEWRESPONSE: ', Object.values(JSON.parse(data)));
       setOverviewState(Object.values(JSON.parse(data)));
-      serviceComponents.overviewData = Object.values(JSON.parse(data));
+      // dialog.showMessageBox({shit:'Broke'});
+      serviceComponents.overviewData = JSON.parse(data);
     });
   }, []);
 
-  const renderState = () => {
+  // Filters data received from IPC to the communications database to create a list of the services tracked in the provided database,
+  const serviceList = () => {
+    // Holds the buttons generated for unique services.
     const componentButtons = [];
+
+    // Tracks which services already have button created.
     const serviceCache = {};
+
     for (let i = 0; i < overviewState.length; i += 1) {
       const element = overviewState[i];
-      // SQL
+      // If SQL
       if (element.currentmicroservice) {
         if (!(element.currentmicroservice in serviceCache)) {
           const button = (
             <button
+            className='servicesBtn'
               type="button"
               key={`serviceItem${props.index}${i}`}
-              onClick={() => setDetails(<ServiceDetails index={props.index} />)}
+              onClick={() => {
+                // IPC communication used to initiate query for information on microservice health information.
+                ipcRenderer.send('detailsRequest', props.index);
+
+                // IPC listener responsible for retrieving infomation from asynchronous main process message.
+                ipcRenderer.on('detailsResponse', (event, data) => {
+                  // Adds returned data to context
+                  healthdata.detailData = Object.values(JSON.parse(data));
+                  // Updates state. Triggers rerender.
+                  setDetails(<ServiceDetails service={element.currentmicroservice} />);
+                });
+              }}
             >
               {element.currentmicroservice}
             </button>
           );
           componentButtons.push(button);
-          serviceCache[element.currentmicroservice] = 1;
-        } else {
-          serviceCache[element.currentmicroservice] += 1;
+          serviceCache[element.currentmicroservice] = true;
         }
       } else if (element.currentMicroservice) {
-        // Mongo
+        // If Mongo
         if (element.currentMicroservice) {
           if (!(element.currentMicroservice in serviceCache)) {
             const button = (
               <button
+                className='servicesBtn'
                 type="button"
                 key={`serviceItem${props.index}${i}`}
-                onClick={() => setDetails(<ServiceDetails index={props.index} />)}
+                onClick={() => {
+                  ipcRenderer.send('detailsRequest', props.index);
+
+                  // IPC listener responsible for retrieving infomation from asynchronous main process message.
+                  ipcRenderer.on('detailsResponse', (event, data) => {
+                    // Adds returned data to context.
+                    healthdata.detailData = Object.values(JSON.parse(data));
+                    // Updates state. Triggers rerender.
+                    setDetails(<ServiceDetails service={element.currentMicroservice} />);
+                  });
+                }}
               >
                 {element.currentMicroservice}
               </button>
             );
             componentButtons.push(button);
-            serviceCache[element.currentMicroservice] = 1;
-          } else {
-            serviceCache[element.currentMicroservice] += 1;
+            serviceCache[element.currentMicroservice] = true;
           }
         }
       }
@@ -71,10 +106,10 @@ const ServiceOverview = (props) => {
   return (
     <div className="mainContainer">
       <div>
-        <h1>Microservices Overview</h1>
+        <h1 className='overviewTitle'>Microservices Overview</h1>
       </div>
       <div />
-      <div className="servicesList">{renderState()}</div>
+      <div className="servicesList">{serviceList()}</div>
     </div>
   );
 };
