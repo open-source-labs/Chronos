8f0b977bc8ea801c83eb0bd0a0f3f0a860d1e29b
Ousman Diallo <47230143+Dialloousman@users.noreply.github.com>
2019-12-31 12:39:30 -0500

    Update mongoose-connect.js

--- model/mongoose-connect.js
+++ model/mongoose-connect.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const { services } = require('../user/settings.json');
 
 // Mongoose connection wrapped in function that takes the index of the selected database as the parameter. This index is used to target the correct database for querying.
-const connectMongoose = (i) => {
+const connectMongoose = (i, URI) => {
   const URI = services[i][2];
   return mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) console.log(err);
