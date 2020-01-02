f73305100164843cc0c248eb08f295ff2a7896c4
jenae <jenaepen@gmail.com>
2020-01-01 23:46:10 -0500

    updated route tracer so that it is outside of the health section and fixed the mongodb order issue and added temp icons to buttons

--- app/charts/route-trace.jsx
+++ app/charts/route-trace.jsx
@@ -1,34 +1,50 @@
 import React, { useContext } from 'react';
-import { Doughnut } from 'react-chartjs-2';
 import CommunicationsContext from '../context/OverviewContext';
 
 const RouteLocations = (props) => {
   const communicationsData = useContext(CommunicationsContext).overviewData;
 
-  console.log('THIS IS ROUTE TRACE: ', communicationsData);
-    
+  //initialize an empty object resObj. This object will store the microservice names as values and its corresponding correlatingId or correlatingid as keys. The microservice names will be stored in array within the order it was to the database.    
   const resObj = {};
-  for (let i = 0; i < communicationsData.length; i+=1) {
+
+  if(communicationsData.length>0 && communicationsData[0]["_id"]){
+    //Sort the communication array from latest to earliest document
+    communicationsData.sort((a,b)=>{ new Date(a.timeSent) - new Date(b.timeSent)})
+    
+    //Iterate over sorted communicationsData array from the end to the beginning
+    for (let i = communicationsData.length-1; i >= 0; i--) {
+      //declare a constant element and initialize it as the object at index i of the communicationsData array
+      const element = communicationsData[i];
+      //Pushes the microservice name into the object  
+      if (resObj[element.correlatingId]) {
+        resObj[element.correlatingId].push(element.currentMicroservice);
+      }
+      else resObj[element.correlatingId] = [element.currentMicroservice];
+      // initializing the object with the first microservice
+    }
+  }
+  else{
+  for (let i = communicationsData.length-1; i >= 0; i--) {
       const element = communicationsData[i];
       if (resObj[element.correlatingid]) resObj[element.correlatingid].push(element.currentmicroservice);
-      else {resObj[element.correlatingid] = [element.currentmicroservice]};
+      else resObj[element.correlatingid] = [element.currentmicroservice];
+      // initializing the object with the first microservice
   };
+}
 
   //use object values to destructure locations
   const tracePoints = Object.values(resObj);
-  console.log('THIS IS TRACEPOINTES: ', tracePoints[0]);
-
+  let position = tracePoints.length-1;
+ 
   const resArray = [];
-  for (let i = 0; i < tracePoints[0].length; i+=1) {
+  for (let i = 0; i < tracePoints[position].length; i+=1) {
     resArray.push(
         <div className="RouteCircle" key={i}>
-        <p id="routeText">Point {i+1}: {tracePoints[0][i]}</p>
+        <p id="routeText">Point {i+1}: {tracePoints[position][i]}</p>
         </div>
     )
   };
 
-
-//   console.log('ARRAY OF LOCATIONS: ', resObj);
   return (
       <div>
     {resArray}
