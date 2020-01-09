9cb490b02dfcfdba1426087abf6919a7ecb187d1
jenae <jenaepen@gmail.com>
2020-01-09 10:47:04 -0500

    still working on testing

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-const { JSDOM } = require('jsdom');
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
-const { window } = jsdom;
+const expect = require('chai').expect;
+//const waitForExpect = require("wait-for-expect");
+import DeleteSevice from '../app/components/DeleteService.jsx';
+//window.require = ()=>{ return {ipcRenderer: jest.fn()}
 
-import DeleteService from '../app/components/DeleteService.jsx'
-
+console.log('window:', window.require)
 
 configure({ adapter: new Adapter() });
 
 describe('Delete a Service', () => {
-    beforeAll(() => {
-        let wrapper = shallow(<DeleteService/>);
-      });
+    // beforeAll(() => {
+    //     let wrapper = shallow(<DeleteService/>);
+    //   });
 
     xit('returns a div',()=>{
         expect(wrapper.type()).toEqual('div')
