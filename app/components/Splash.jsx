954c198065968e71a555f8e52bf8f53dd6e5b58a
Chris Romano <chrispaulromano@gmail.com>
2020-01-09 18:47:30 -0500

    splash ready.  only renders once per session

--- app/components/Splash.jsx
+++ app/components/Splash.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react';
 
+const { ipcRenderer } = window.require('electron');
+
 
 const Splash = (props) => {
   const { toggleSplash } = props;
-  useEffect(() => { setTimeout(() => toggleSplash(false), 3000); });
+  // I know this line is dense.  Bear with me...useEffect hook that fires whenever splash
+  // component is rendered, which, after 3 seconds fires toggleSplash passing in what ipcMain
+  // returns when it receives the 'toggleSplash' message (which will be false boolean).  This then
+  // toggles the splash state-property inside of the App component which rerenders app and no longer
+  // displays the splash page.
+  useEffect(() => { setTimeout(() => toggleSplash(ipcRenderer.sendSync('toggleSplash')), 3000); });
 
   return (
     <div id="splash">
