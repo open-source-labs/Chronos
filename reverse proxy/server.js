fee20f5dbdb418b33e6e5e0413d41b37120d829c
Tim <tim.atapagra@gmail.com>
2020-01-15 11:39:34 -0500

    Linter and syntax fixes

--- reverse proxy/server.js
+++ reverse proxy/server.js
@@ -3,14 +3,16 @@
 // cmd.propagate();
 
 const PORT = 3000;
-const express  = require('express');
+const express = require('express');
 const cors = require('cors');
+
 const app = express();
 const httpProxy = require('http-proxy');
+
 const apiProxy = httpProxy.createProxyServer();
-const books = 'http://localhost:4545',
-      customers = 'http://localhost:5555',
-      orders = 'http://localhost:7777';
+const books = 'http://localhost:4545';
+const customers = 'http://localhost:5555';
+const orders = 'http://localhost:7777';
 
 // UNCOMMENT THE LINE BELOW AND PASS IN YOUR CHOSEN ARGUMENTS
 // app.use('/', cmd.microCom('microserviceName', 'databaseType', 'databaseURL', 'wantMicroHealth', 'queryFrequency'))
@@ -18,22 +20,22 @@ const books = 'http://localhost:4545',
 app.use(cors());
 app.use('/', express.static('../frontend'));
 
-app.all("/books/*", function(req, res) {
-    console.log('redirecting to books');
-    apiProxy.web(req, res, {target: books});
+app.all('/books/*', (req, res) => {
+  console.log('redirecting to books');
+  apiProxy.web(req, res, { target: books });
 });
 
-app.all("/customers/*", function(req, res) {
-    console.log('redirecting to customers');
-    apiProxy.web(req, res, {target: customers});
+app.all('/customers/*', (req, res) => {
+  console.log('redirecting to customers');
+  apiProxy.web(req, res, { target: customers });
 });
 
-app.all("/orders/*", function(req, res) {
-    console.log('redirecting to orders');
-    apiProxy.web(req, res, {target: orders});
+app.all('/orders/*', (req, res) => {
+  console.log('redirecting to orders');
+  apiProxy.web(req, res, { target: orders });
 });
 
 // Open and listen to server on specified port
 app.listen(PORT, () => {
-    console.log(`Frontend server running on port ${PORT} ...`);
+  console.log(`Frontend server running on port ${PORT} ...`);
 });
