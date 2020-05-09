77a74a65557af7e32ed37966314804eaddc2ca14
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-05-08 17:39:32 -0700

    Re-render latency chart

--- app/charts/latency-chart.jsx
+++ app/charts/latency-chart.jsx
@@ -1,37 +1,46 @@
 import React, { useContext } from 'react';
 import { Line } from 'react-chartjs-2';
 import HealthContext from '../context/DetailsContext';
+import Plot from 'react-plotly.js';
 
 const LatencyChart = (props) =>  {
-  const xAxis = [];
-  const yAxis = [];
   const healthData = useContext(HealthContext).detailData;
-  for (let i = 0; i < healthData.length; i++) {
-    const element = healthData[i];
-    if (element.currentmicroservice === props.service || element.currentMicroservice === props.service) {
-      xAxis.push(i);
-      yAxis.push(element.latency);
+  const createChart = () => {
+    const xAxis = [];
+    const yAxis = [];
+    for (let i = 0; i < healthData.length; i++) {
+      const element = healthData[i];
+      if (element.currentmicroservice === props.service || element.currentMicroservice === props.service) {
+        xAxis.push(i);
+        yAxis.push(element.latency);
+      }
     }
-  }
-  const chartData = {
-    datasets: [
-      {
-        label: `CPU latency of ${props.service}`,
-        data: yAxis,
-        backgroundColor: ['rgb(254, 255, 0)'],
-      },
-    ],
-    options: {
-      xAxisID: 'TBD',
-      yAxisID: 'TBD',
-    },
-    labels: xAxis,
-  };
-  return (
-    <div>
-      <Line data={chartData} />
-    </div>
-  );
+
+    return (
+      <Plot
+        data = {[{
+          type: 'scatter',
+          x: xAxis,
+          y: yAxis,
+          mode: 'lines',
+          rangemode: 'nonnegative',
+          name: `${props.service} CPU Latency`,
+          marker: {
+              color: '#155263',
+              size: 1
+          }
+        }]}
+        layout = {{
+          width: 500,
+          height: 500,
+          paper_bgcolor: '#fffbe0',
+          plot_bgcolor: '#fffbe0',
+          showlegend: true
+        }}
+      />
+    )};
+
+  return <div>{createChart()}</div>;
 };
 
 export default LatencyChart;
