8d12828ee7eace5c0214e626c8664ca1f7bb753c
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-05-11 16:30:51 -0700

    Change graph sizing before beginning UI configuration

--- app/charts/latency-chart.jsx
+++ app/charts/latency-chart.jsx
@@ -31,8 +31,8 @@ const LatencyChart = (props) =>  {
           },
         }]}
         layout = {{
-          width: 500,
-          height: 500,
+          width: 400,
+          height: 400,
           paper_bgcolor: '#fffbe0',
           plot_bgcolor: '#fffbe0',
           showlegend: true,
