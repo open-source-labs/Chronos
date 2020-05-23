3e424b127b85a4b225099983da112d86392b7c5f
ajlee12 <rach.on@gmail.com>
2020-05-23 14:54:29 -0700

    Update Mongoose schema to accomodate docker stats.

--- MicroserviceHealth.js
+++ MicroserviceHealth.js
@@ -42,5 +42,35 @@ mongoose.model("MicroserviceHealth", {
   },
   timestamp: {
     type: Date,
-  }
+  },
+  // Alan: Aded schema for Docker container stats (9).
+  containerId: {
+    type: String,
+  },
+  containerMemUsage: {
+    type: Number, // bytes
+  },
+  containerMemLimit: {
+    type: Number,
+  },
+  containerMemPercent: {
+    type: Number,
+  },
+  containerCpuPercent: {
+    type: Number,
+  },
+  networkReceived: {
+    type: Number, // bytes
+    default: 0,
+  },
+  networkSent: {
+    type: Number, // bytes
+    default: 0,
+  },
+  containerProcessCount: {
+    type: Number, // count
+  },
+  containerRestartCount: {
+    type: Number, // count
+  },
 });
