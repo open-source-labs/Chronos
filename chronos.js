7f1b22acf615f1abeebcd0463407a506fb1bd81b
ajlee12 <rach.on@gmail.com>
2020-05-26 14:22:06 -0700

    Add 1 comment re. the new isDockerized param. Fix spacing.

--- chronos.js
+++ chronos.js
@@ -16,19 +16,21 @@ chronos.propagate = () => {
 // microCom logs all microservice-microservice and microservice-client communication into the user-owned database.
 
 /*
-    * microserviceName: what the user wants current microservice to be called
-    * databaseType: type of database user is providing: Mongo or PostgreSQL
-    * userOwnedDB: URL to user database
-    * queryFreq: Not necessary for microCom as microCom monitors only when an endpoint is hit
- */
+  * microserviceName: what the user wants current microservice to be called
+  * databaseType: type of database user is providing: Mongo or PostgreSQL
+  * userOwnedDB: URL to user database
+  * queryFreq: Not necessary for microCom as microCom monitors only when an endpoint is hit
+  * wantMicroHealth: "yes" or "no". If yes, .microHealth is invoked.
+  * isDockerized: "yes" or "no". If yes, .microDocker is invoked to log container stats instead.
+*/
 chronos.microCom = (microserviceName, databaseType, userOwnedDB, wantMicroHealth, queryFreq = 'm', req, res, next) => {
   // Handles if user inputs an array. Grabs information and assigns to correct parameters
   if (Array.isArray(microserviceName) === true && microserviceName.length >= 4) {
-    userOwnedDB = microserviceName[2];
+    microserviceName = microserviceName[0];
     databaseType = microserviceName[1];
+    userOwnedDB = microserviceName[2];
     wantMicroHealth = microserviceName[3];
     queryFreq = microserviceName[4] || 'm';
-    microserviceName = microserviceName[0];
   }
   // Changes user inputs to lowercase to account for any capitalization errors
   databaseType = databaseType.toLowerCase();
@@ -57,9 +59,9 @@ chronos.microCom = (microserviceName, databaseType, userOwnedDB, wantMicroHealth
 
   // Checks the type of database provided by the user and uses appropriate middleware files. Throws error if input db type is not supported
   if (databaseType === 'mongo' || databaseType === 'mongodb') {
-    return mongoMiddleware.microCom(userOwnedDB, microserviceName, wantMicroHealth,queryFreq);
+    return mongoMiddleware.microCom(userOwnedDB, microserviceName, wantMicroHealth, queryFreq);
   } if (databaseType === 'sql' || databaseType === 'postgresql') {
-    return sqlMiddleware.microCom(userOwnedDB, microserviceName, wantMicroHealth,queryFreq);
+    return sqlMiddleware.microCom(userOwnedDB, microserviceName, wantMicroHealth, queryFreq);
   }
   throw new Error(
     'Chronos currently only supports Mongo and PostgreSQL databases. Please enter "mongo" or "sql"',
