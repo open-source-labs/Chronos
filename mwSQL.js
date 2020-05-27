e8afd1c9ced792c4fee8f09d1efac6c26106c25d
ajlee12 <rach.on@gmail.com>
2020-05-27 00:37:45 -0700

    Use setTimeout to retry microDocker in 3 sec if 1st invoc. doesn't yield valid containerId. Remove extra lines and param.

--- mwSQL.js
+++ mwSQL.js
@@ -42,7 +42,7 @@ chronos.microCom = (userOwnedDB, userInputMSName, wantMicroHealth, queryFreq, is
   if (wantMicroHealth === 'yes' || wantMicroHealth === 'y') {
     chronos.microHealth(userInputMSName, queryFreq);
   } else if (isDockerized === 'yes' || wantMicroHealth === 'y') {
-    chronos.microDocker(userInputMSName, uri, queryFreq);
+    chronos.microDocker(userInputMSName, queryFreq);
   }
 
   // query created DB and create table if it doesn't already exist and create the columns. Throws error if needed.
@@ -119,9 +119,6 @@ chronos.microHealth = (userInputMSName, queryFreq) => {
   let numBlockedProcesses;
   let numRunningProcesses;
   let numSleepingProcesses;
-  // Docker data:
-  let containerId, containerMemUsage, containerMemLimit, containerMemPercent, containerCpuPercent;
-  let networkReceived, networkSent, containerProcessCount, containerRestartCount;
 
   currentMicroservice = userInputMSName;
 
@@ -340,7 +337,9 @@ chronos.microDocker = function (userInputMSName, uri, queryFreq) {
       }, queryObj[queryFreq]);
     }
     else {
-      throw new Error('Cannot find container data matching the microservice name.');
+      // throw new Error('Cannot find container data matching the microservice name.');
+      // If si wasn't able to read container stats on first try, try again in 3 sec.
+      setTimeout(chronos.microDocker, 3000);
     }
   })["catch"](function (err) {
     throw err;
