5450d5d174da94c86e0fc37c5f94210bc08b25a1
ajlee12 <rach.on@gmail.com>
2020-05-27 12:30:49 -0700

    Move container stats schema OUT of microsvc health. Will have another schema file for container stats.

--- MicroserviceHealth.js
+++ MicroserviceHealth.js
@@ -43,34 +43,4 @@ mongoose.model("MicroserviceHealth", {
   timestamp: {
     type: Date,
   },
-  // Added additional schema for Docker container stats (9).
-  containerId: {
-    type: String,
-  },
-  containerMemUsage: {
-    type: Number, // bytes
-  },
-  containerMemLimit: {
-    type: Number,
-  },
-  containerMemPercent: {
-    type: Number,
-  },
-  containerCpuPercent: {
-    type: Number,
-  },
-  networkReceived: {
-    type: Number, // bytes
-    default: 0,
-  },
-  networkSent: {
-    type: Number, // bytes
-    default: 0,
-  },
-  containerProcessCount: {
-    type: Number, // count
-  },
-  containerRestartCount: {
-    type: Number, // count
-  },
 });
