bba676dd297826d974b2caf4794bdd0c1d84fb29
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-20 19:23:09 -0700

    Merge with master

--- app/components/MonitorHeader.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// const MonitorHeader = (props) => {
-//   return (
-//     <div id="Dashboard">
-//       {title}
-//     </div>
-//   );
-// }
--- app/context/SetupContext.js
+++ app/context/SetupContext.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 const { ipcRenderer } = window.require('electron');
 
-// Obtains setup and splash status on application start. Toggles setup status if user wants to add a new service.
+// Toggles setup status if user wants to add a new service.
 const SetupContext = React.createContext({
   setupRequired: JSON.parse(ipcRenderer.sendSync('setup')),
-  splash: JSON.parse(ipcRenderer.sendSync('checkSplash')),
-  toggleSetup: (currentSetup) => {
-    if (currentSetup) return false;
-    return true;
-  },
+  toggleSetup: setupRequired => !setupRequired,
+  // splash: JSON.parse(ipcRenderer.sendSync('checkSplash')),
+  // toggleSplash: () => ipcRenderer.sendSync('toggleSplash'),
 });
 
 export const SetupProvider = SetupContext.Provider;
