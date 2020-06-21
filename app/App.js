a5427f09dc67ea12971cb1c7d3b459e5f92ec5ff
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-21 10:04:16 -0700

    Delete SetupContext

--- app/App.js
+++ app/App.js
@@ -1,12 +1,8 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import Splash from './components/Splash';
-import AddService from './components/AddService';
 import DashboardContainer from './containers/DashboardContainer';
-import SetupContext from './context/SetupContext';
 
 const App = () => {
-  const { setupRequired } = useContext(SetupContext);
-
   // HARD CODED for development
   const [firstVisit, setFirstVisit] = useState(false);
 
@@ -14,9 +10,7 @@ const App = () => {
   // const [firstVisit, setFirstVisit] = useState(true);
 
   // Splash image on startup
-  if (firstVisit) return <Splash setFirstVisit={setFirstVisit} />;
-
-  return setupRequired ? <AddService /> : <DashboardContainer />;
+  return firstVisit ? <Splash setFirstVisit={setFirstVisit} /> : <DashboardContainer />;
 };
 
 export default App;
