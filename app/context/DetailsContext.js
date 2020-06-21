c2d158754bc3e0e5704b37bff56be7ce552dab98
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-21 09:00:24 -0700

    Refactor in provider components

--- app/context/DetailsContext.js
+++ app/context/DetailsContext.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// Used to save query results. Laid foundation for conditional rendering similar to how SetupContext was used. 
-const DetailContext = React.createContext({
-  detailData: null,
-  detailSelected: null,
-  detailStatus: false,
-  toggleOverview: (status) => {
-    if (status) return false;
-    return true;
-  },
-});
+// Used to save query results. Laid foundation for conditional rendering similar to how SetupContext was used.
+export const DetailsContext = React.createContext();
 
-export const DetailProvider = DetailContext.Provider;
-export const DetailConsumer = DetailContext.Consumer;
-export default DetailContext;
+const DetailsContextProvider = ({ children }) => {
+  const [detailsData, setDetailsData] = useState(null);
+  // const [detailSelected, setDetailSelected] = useState(null);
+  // const [detailStatus, setDetailStatus] = useState(null);
+  // const toggleOverview = status => !status;
+
+  return (
+    <DetailsContext.Provider value={{ setDetailsData, detailsData }}>
+      {children}
+    </DetailsContext.Provider>
+  );
+};
+
+export default DetailsContextProvider;
+
--- app/context/OverviewContext.js
+++ app/context/OverviewContext.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Used to save query results. Laid foundation for conditional rendering similar to how SetupContext was used.
-const OverviewContext = React.createContext({
-  overviewData: null,
-  overviewStatus: true,
-  toggleOverview: (status) => {
-    if (status) return false;
-    return true;
-  },
-});
+// const OverviewContext = React.createContext({
+//   overviewData: null,
+//   overviewStatus: true,
+//   toggleOverview: status => !status,
+// });
 
-export const OverviewProvider = OverviewContext.Provider;
-export const OverviewConsumer = OverviewContext.Consumer;
-export default OverviewContext;
+export const OverviewContext = React.createContext();
+
+const OverviewContextProvider = ({ children }) => {
+  const [overviewData, setOverviewData] = useState(null);
+  const [overviewStatus, setOverviewStatus] = useState(true);
+
+  return (
+    <OverviewContext.Provider value={{ overviewData, setOverviewData }}>
+      {children}
+    </OverviewContext.Provider>
+  )
+};
+
+export default OverviewContextProvider;
