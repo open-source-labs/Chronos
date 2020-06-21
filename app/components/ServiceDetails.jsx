4496832a30d3bd0b1f7f74e82724b7d6313b56f7
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-21 09:22:58 -0700

    Clean up code

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -1,14 +1,8 @@
-/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
-// import Modal from './Modal.jsx';
-import GraphsContainer from '../containers/GraphsContainer.jsx';
+import GraphsContainer from '../containers/GraphsContainer';
 
-// Renders charts created with health and communication data for a selected database.
-const ServiceDetails = (props) => {
-  // Renders health info detail buttons
-  const { service } = props;
-  
-  //Capitalize service name
+const ServiceDetails = ({ service }) => {
+  // Capitalize service title
   const title = service[0].toUpperCase() + service.substring(1);
 
   return (
