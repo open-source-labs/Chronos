b3ae60160cd56a49d2178c518f964f66090cf59e
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-21 09:31:50 -0700

    Change header

--- app/components/ServiceDetails.jsx
+++ app/components/ServiceDetails.jsx
@@ -3,11 +3,10 @@ import GraphsContainer from '../containers/GraphsContainer';
 
 const ServiceDetails = ({ service }) => {
   // Capitalize service title
-  const title = service[0].toUpperCase() + service.substring(1);
 
   return (
     <div id="serviceDetailsContainer">
-      <h3 id="microserviceHealthTitle">{title}</h3>
+      <h3 id="microserviceHealthTitle">Microservice: {service}</h3>
       <GraphsContainer service={service} />
     </div>
   );
