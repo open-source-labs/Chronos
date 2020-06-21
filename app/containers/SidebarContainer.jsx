8892c1529686dded783d3b014b8212ed1ce12442
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-21 09:18:55 -0700

    Add toggle to application buttons and annotate code

--- app/containers/SidebarContainer.jsx
+++ app/containers/SidebarContainer.jsx
@@ -5,17 +5,20 @@ import Applications from '../components/Applications';
 import ServicesList from '../components/ServicesList';
 import AddService from '../components/AddService';
 import DeleteService from '../components/DeleteService';
-import '../stylesheets/sidebar.css';
+import '../stylesheets/SidebarContainer.css';
 
 const SidebarContainer = ({ setDetails }) => {
   const setup = useContext(SetupContext);
 
   // Set view to selected index
-  // Index is dependent on microservice button clicked
+  // Index is dependent on which microservice button is clicked
   const [index, setIndex] = useState(null);
 
-  const handleClick = e => {
-    setIndex(e.target.id);
+  const handleClick = id => {
+    // Toggle index to show/hide the app microservices
+    setIndex(index === null ? id : null);
+
+    // Clear main container
     setDetails(null);
   };
 
@@ -31,12 +34,15 @@ const SidebarContainer = ({ setDetails }) => {
         <Applications handleClick={handleClick} />
         {index && <ServicesList index={index} setDetails={setDetails} />}
         <div className="btn-container">
+          {/* Route to AddService component */}
           <button type="button" onClick={AddClick}>
             +
           </button>
+          {/* Route to DeleteService component */}
           <button type="button" onClick={() => setDetails(<DeleteService />)}>
             -
           </button>
+          {/* Refresh page */}
           <button type="button" onClick={() => location.reload()}>
             Refresh
           </button>
