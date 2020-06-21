3daa2a561340f1bada4a0277ce26632e00f78eef
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-21 09:22:03 -0700

    Rename MonitoringContainer to Main

--- app/containers/MonitoringContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../stylesheets/monitoring.css';
-
-// Details is the current microservice being displayed
-// This is set in Sidebar Container and Services List Component
-const MonitoringContainer = ({ details }) => <>{details || null}</>;
-
-export default MonitoringContainer;
