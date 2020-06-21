a5bf33c04f36ab73fb88d31a16d28d3963cbc26e
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-21 08:29:50 -0700

    Remove ids

--- app/components/Splash.jsx
+++ app/components/Splash.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import '../stylesheets/splash.css';
+import '../stylesheets/Splash.css';
 
 const Splash = ({ setFirstVisit }) => {
   // Display splash for 3 seconds
@@ -9,10 +9,8 @@ const Splash = ({ setFirstVisit }) => {
 
   return (
     <div id="splash">
-      <img alt="Chronos Logo" src="app/assets/icons/icon.png" id="splashLogo" />
-      <span id="splashText" style={{ fontFamily: 'Baloo Bhaijaan' }}>
-        chronos
-      </span>
+      <img alt="Chronos Logo" src="app/assets/icons/icon.png" />
+      <span>chronos</span>
     </div>
   );
 };
