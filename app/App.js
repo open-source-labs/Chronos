6a17eea4f9de422fb383f324ca79e6b3bd49611e
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-22 16:22:51 -0700

    Disable splash

--- app/App.js
+++ app/App.js
@@ -4,10 +4,10 @@ import DashboardContainer from './containers/DashboardContainer';
 
 const App = () => {
   // HARD CODED for development
-  // const [firstVisit, setFirstVisit] = useState(false);
+  const [firstVisit, setFirstVisit] = useState(false);
 
   // UNCOMMENT this for splash demo
-  const [firstVisit, setFirstVisit] = useState(true);
+  // const [firstVisit, setFirstVisit] = useState(true);
 
   // Splash image on startup
   return firstVisit ? <Splash setFirstVisit={setFirstVisit} /> : <DashboardContainer />;
