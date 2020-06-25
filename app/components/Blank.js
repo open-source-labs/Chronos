93e665af0e57c83bda2963731ee3fed35ea26c08
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-25 15:55:43 -0700

    Created blank component for home page

---
+++ app/components/Blank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import icon from '../assets/pangolin.png';
+import '../stylesheets/Blank.css';
+
+const Blank = props => {
+  return (
+    <div className="blank">
+      <img width="300" height="auto" src={icon} alt="Chronos logo" />
+      <h1>Welcome to Chronos!</h1>
+      <p>Select your application to get started!</p>
+    </div>
+  );
+};
+
+export default Blank;
