fe92becc1e0c83c345773b2c2c734f7cc457fe7a
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-25 15:56:04 -0700

    Copy pangolin

--- app/components/SidebarHeader.tsx
+++ app/components/SidebarHeader.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-// import icon2Cropped from '../assets/icons/icon2Cropped.png'
+// import icon2Cropped from '../assets/icon2Cropped.png';
 import '../stylesheets/SidebarHeader.css';
 
 const Header = () => {
   return (
     <>
-      <img alt="Chronos Logo" id="serviceDashLogo" />
+      <img alt="Chronos Logo" id="serviceDashLogo" /* src={icon2Cropped} */ />
       <h2>Databases</h2>
     </>
   );
