73edb071ec6d30bad7aef43f3d32420d961f8111
gregpalace <greg.palasciano@gmail.com>
2020-06-25 20:09:33 -0700

    TypeScript Update for Comms Context

---
+++ app/context/CommsContext.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Electron from 'electron';
+
+const { ipcRenderer } = window.require('electron');
+
+export const CommsContext = React.createContext<any>(null);
+
+interface IProp {
+  id: number;
+  microservice: string;
+  endpoint: string;
+  request: string;
+  responsestatus: string;
+  responsemessage: string;
+  time: string;
+  correlatingid: string;
+}
+
+const CommsContextProvider: React.FC = ({ children }) => {
+  const [commsData, setCommsData] = useState([]);
+
+  // Fetches all data related to communication for a particular app
+  const fetchCommsData = (index: number) => {
+    ipcRenderer.send('commsRequest', index);
+    ipcRenderer.on('commsResponse', (event: Electron.Event, data) => {
+      console.log('DATAAAAA', data);
+      // Store resulting data in local state
+      const result = JSON.parse(data);
+      console.log('Number of data points (comms):', result.length);
+      setCommsData(result);
+    });
+  };
+
+  return (
+    <CommsContext.Provider value={{ commsData, setCommsData, fetchCommsData }}>
+      {children}
+    </CommsContext.Provider>
+  );
+};
+
+export default CommsContextProvider;
