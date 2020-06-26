39ef933abf18d122195914a6547fba4d9a7d14f5
gregpalace <greg.palasciano@gmail.com>
2020-06-26 10:07:13 -0700

    TS Refactor Applications.jsx

--- app/components/Applications.tsx
+++ app/components/Applications.tsx
@@ -2,17 +2,17 @@ import React, { useContext } from 'react';
 import DashboardContext from '../context/DashboardContext';
 import '../stylesheets/Applications.css';
 
-const Applications = ({ handleClick }) => {
+const Applications = ({ handleClick }: any) => {
   const { applicationsList } = useContext(DashboardContext);
 
   // Create a button for each app
-  return applicationsList.map((app, i) => (
+  return applicationsList.map((app: string, i: string | undefined) => (
     <button
       className="app-btn"
       type="button"
       id={i}
       key={i}
-      onClick={e => handleClick(e.target.id)}
+      onClick={(event: React.MouseEvent<HTMLElement>) => handleClick(event.currentTarget.id)}
     >
       {app}
     </button>
