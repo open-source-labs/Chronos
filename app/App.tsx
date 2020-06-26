6af2b38599b5b6b1868d1f2e2d16ea40510f8b2b
gregpalace <greg.palasciano@gmail.com>
2020-06-25 17:39:59 -0700

    Initial TS Refactor

--- app/App.tsx
+++ app/App.tsx
@@ -2,15 +2,31 @@ import React, { useState } from 'react';
 import Splash from './components/Splash';
 import DashboardContainer from './containers/DashboardContainer';
 
-const App = () => {
-  // HARD CODED for development
-  // const [firstVisit, setFirstVisit] = useState(false);
+// const App = () => {
+//   // HARD CODED for development
+//   // const [firstVisit, setFirstVisit] = useState(false);
 
-  // UNCOMMENT this for splash demo
+//   // UNCOMMENT this for splash demo
+//   const [firstVisit, setFirstVisit] = useState(true);
+
+//   // Splash image on startup
+//   return firstVisit ? <Splash setFirstVisit={setFirstVisit} /> : <DashboardContainer />;
+// };
+
+// export default App;
+
+interface IProps {
+  children: (
+    firstVisit: boolean,
+    setFirstVisit: React.Dispatch<React.SetStateAction<boolean>>
+  ) => JSX.Element | null;
+  console.log(children);
+}
+const App: React.FC<IProps> = () => {
   const [firstVisit, setFirstVisit] = useState(true);
 
   // Splash image on startup
-  return firstVisit ? <Splash setFirstVisit={setFirstVisit} /> : <DashboardContainer />;
+  return firstVisit ? <Splash /> : <DashboardContainer />;
 };
 
 export default App;
