079cdf333890391c3b5a9350e6e6a834c958f612
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-25 18:20:27 -0700

    Setup config file

--- chronos.js
+++ chronos.js
@@ -1,124 +1,59 @@
 const hpropagate = require('hpropagate');
 const mongoMiddleware = require('./controllers/mwMongo.js');
 const sqlMiddleware = require('./controllers/mwSQL.js');
+const { validateInput, addNotifications } = require('./controllers/helpers');
 
+let userConfig = {};
 const chronos = {};
 
 /**
- * Testing the config file
+ * CMD CONFIG FILE SETUP
+ *
+ * @field name {string} REQUIRED
+ *    The user specified name for the microservice being diagnosed
+ *
+ * @field interval {number} DEFAULT 60000
+ *    The interval for every microservice health check in milliseconds
+ *    This defaults to 60000 ms or 1 minute
+ *
+ * @field dockerized {boolean} DEFAULT False
+ *    True if the microservice is containerized. False otherwise
+ *
+ * @field database {Object}
+ *    Takes two properties
+ *    - type {string} Either PostgreSQL or MongoDB
+ *    - URI {string} Database uri
+ *
+ * @field notifications {array} OPTIONAL
+ *    Varies per notification method
  */
 chronos.use = config => {
-  console.log('in use!!', config);
+  // Validate all input fields exist
+  validateInput(config);
+
+  // Add notifications
+  addNotifications(config);
+
+  // Save config globally
+  userConfig = config;
 };
 
-/* 
-propagate places an unique x-correlating-id into the headers of each request/response.
-The same id persist thru the whole life cycle of the request.
-*/
+/**
+ * Places an unique x-correlating-id into the headers of each request/response.
+ * This is used for tracking the life cycle of the request until the response
+ */
 chronos.propagate = () => {
   hpropagate({ propagateInResponses: true });
 };
 
-// microCom logs all microservice-microservice and microservice-client comm. into the user-owned database.
+chronos.diagnose = () => {
+  const { database } = userConfig;
 
-/* PARAMETERS:
- * microserviceName: what the user wants current microservice to be called
- * databaseType: type of database user is providing: Mongo or PostgreSQL
- * userOwnedDB: URL to user database
- * queryFreq: Not necessary for microCom as microCom monitors only when an endpoint is hit
- * wantMicroHealth: "yes" or "no". If yes, .microHealth is invoked.
- * isDockerized: "yes" or "no". If yes, .microDocker is invoked to log container stats instead.
- * Defaults to 'no'.
- */
-chronos.microCom = (
-  microserviceName,
-  databaseType,
-  userOwnedDB,
-  wantMicroHealth,
-  queryFreq = 'm',
-  isDockerized = 'no',
-  SlackUrl, //slackwebhook url
-  emailList, // receipient email addresses
-  emailHost, // email host
-  emailPort, // host email port
-  user, // email address-username
-  password, // email account password
-  req,
-  res,
-  next
-) => {
-  // Handles if user inputs an array. Grabs information and assigns to correct parameters
-  // console.log('config file!', config);
-  if (Array.isArray(microserviceName) === true && microserviceName.length >= 4) {
-    microserviceName = microserviceName[0];
-    databaseType = microserviceName[1];
-    userOwnedDB = microserviceName[2];
-    wantMicroHealth = microserviceName[3];
-    queryFreq = microserviceName[4] || 'm';
-    isDockerized = isDockerized[5] || 'no';
+  if (database.type === 'MongoDB') {
+    return mongoMiddleware.microCom(userConfig);
   }
-  // Changes user inputs to lowercase to account for any capitalization errors.
-  microserviceName = microserviceName.toLowerCase();
-  databaseType = databaseType.toLowerCase();
-  wantMicroHealth = wantMicroHealth.toLowerCase();
-  queryFreq = queryFreq.toLowerCase();
-  isDockerized = isDockerized.toLowerCase();
-
-  // Ensures that the required parameters are entered, errors out otherwise
-  if (!microserviceName || !databaseType || !userOwnedDB || !wantMicroHealth) {
-    throw new Error('Please verify that you have provided all four required parameters');
-  }
-
-  // Verifies that the user has enteres strings, throws error otherwise
-  if (
-    typeof microserviceName !== 'string' ||
-    typeof databaseType !== 'string' ||
-    typeof userOwnedDB !== 'string' ||
-    typeof wantMicroHealth !== 'string' ||
-    typeof queryFreq !== 'string' ||
-    typeof isDockerized !==
-      'string' /* ||
-    typeof SlackUrl !== 'string' ||
-    typeof emailList !== 'string' ||
-    typeof emailHost !== 'string' ||
-    typeof emailPort !== 'number' ||
-    typeof user !== 'string' ||
-    typeof password !== 'string' */
-  ) {
-    throw new Error('Please verify that the parameters you entered are all strings');
-  }
-
-  // Checks the type of database provided by the user and uses appropriate middleware files.
-  // Throws error if input db type is not supported
-  if (databaseType === 'mongo' || databaseType === 'mongodb') {
-    return mongoMiddleware.microCom(
-      microserviceName,
-      userOwnedDB,
-      wantMicroHealth,
-      queryFreq,
-      isDockerized,
-      SlackUrl,
-      emailList,
-      emailHost,
-      emailPort,
-      user,
-      password
-    );
-  }
-  if (databaseType === 'sql' || databaseType === 'postgresql') {
-    return sqlMiddleware.microCom(
-      microserviceName,
-      userOwnedDB,
-      wantMicroHealth,
-      queryFreq,
-      isDockerized,
-      SlackUrl,
-      emailList,
-      emailHost,
-      emailPort,
-      user,
-      password
-    );
+  if (database.type === 'PostgreSQL') {
+    return sqlMiddleware.microCom(userConfig);
   }
   throw new Error(
     'Chronos currently only supports Mongo and PostgreSQL databases. Please enter "mongo" or "sql"'
---
+++ controllers/helpers.js
@@ -0,0 +1,52 @@
+const helpers = {
+  validateInput(config) {
+    let { name, database, interval, dockerized } = config;
+
+    // Validate all required fields exist and are valid input types
+    if (!name || typeof name !== 'string') {
+      throw new Error('Invalid input "name": Please provide a name for your microservice');
+    }
+
+    if (!database.type || typeof database.type !== 'string') {
+      throw new Error('Invalid input "database type": Chronos supports PostgreSQL and MongoDB');
+    }
+
+    if (!database.URI || typeof database.URI !== 'string') {
+      throw new Error('Invalid input "database URI": Please provide the URI to your database');
+    }
+
+    // Validate database type
+    if (database.type !== 'PostgreSQL' || database.type !== 'MongoDB') {
+      throw new Error(
+        `Invalid input "${database.type}". Chronos only supports PostgreSQL and MongoDB.`
+      );
+    }
+
+    // Default interval to one minute
+    if (!interval || typeof interval !== 'number') config.interval = 60000;
+
+    // Default dockerized to false
+    if (dockerized === undefined || dockerized !== 'boolean') config.dockerized = false;
+  },
+
+  addNotifications(config) {
+    const { notifications } = config;
+
+    // Current notification methods supported
+    const features = ['slack', 'email', 'sms'];
+
+    // Setup notifications for user
+    notifications.forEach(obj => {
+      const type = obj.type;
+
+      // Throw errors on unsupported notification methods
+      if (!features.includes(type)) {
+        throw new Error(`${type} is not a supported notification method for Chronos`);
+      } else {
+        config[type] = true;
+      }
+    });
+  },
+};
+
+module.exports = helpers;
