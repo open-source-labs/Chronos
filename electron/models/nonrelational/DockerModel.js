ea7427e479b7e88e3afe33c75f8b82f695702f63
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-26 17:43:54 -0700

    Create DockerSchema

---
+++ electron/models/nonrelational/DockerModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const DockerSchema = new Schema({
+  containername: {
+    type: String,
+    required: true,
+  },
+  containerid: {
+    type: String,
+    required: true,
+  },
+  platform: {
+    type: String,
+    required: true,
+  },
+  starttime: {
+    type: String,
+    required: true,
+  },
+  memoryusage: {
+    type: Number,
+    required: true,
+  },
+  memorylimit: {
+    type: Number,
+    required: true,
+  },
+  memorypercent: {
+    type: Number,
+    required: true,
+  },
+  cpupercent: {
+    type: Number,
+    required: true,
+  },
+  networkreceived: {
+    type: Number,
+    required: true,
+  },
+  networksent: {
+    type: Number,
+    required: true,
+  },
+  processcount: {
+    type: Number,
+    required: true,
+  },
+  restartcount: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = service => mongoose.model(service, DockerSchema);
