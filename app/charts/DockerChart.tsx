ea4dc5de402d918fa9781b42998d778db93bd8ab
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-26 17:31:21 -0700

    Convert dockerChart to TS

---
+++ app/charts/DockerChart.tsx
@@ -0,0 +1,68 @@
+import React, { useContext } from 'react';
+import { HealthContext } from '../context/HealthContext';
+
+interface IContainer {
+  containername: string;
+  containerid: string;
+  platform: string;
+  starttime: string;
+  memoryusage: number;
+  memorylimit: number;
+  memorypercent: number;
+  cpupercent: number;
+  networkreceived: number;
+  networksent: number;
+  processcount: number;
+  restartcount: number;
+}
+
+/**
+ * Table that displays real-time Docker container information using the
+ * latest data point available ???
+ */
+const DockerStatsChart = () => {
+  const { healthData } = useContext(HealthContext);
+  const {
+    containername,
+    containerid,
+    platform,
+    starttime,
+    memoryusage,
+    memorylimit,
+    memorypercent,
+    cpupercent,
+    networkreceived,
+    networksent,
+    processcount,
+    restartcount,
+  }: IContainer = healthData;
+
+  return containerid ? (
+    // Docker data
+    <div id="docker-stats-chart">
+      <header id="docker-stats-chart-header">Docker Container Stats</header>
+      <span>Container Name: {containername}</span>
+      <span>Container ID: {containerid}</span>
+      <span>Platform: {platform}</span>
+      <span>Start time: {starttime}</span>
+      <span>Memory Usage: {(memoryusage / 1000000).toFixed(2)}</span>
+      <span>Memory Limit: {(memorylimit / 1000000).toFixed(2)}</span>
+      <span>Memory Percent: {memorypercent.toFixed(2)}%</span>
+      <span>CPU percent: {cpupercent.toFixed(2)}%</span>
+      <span>Network I/O - Received (Kb): {networkreceived / 1000}</span>
+      <span>Network I/O - Sent (Kb): {networksent / 1000}</span>
+      <span>Process Count: {processcount}</span>
+      <span>Restart Count: {restartcount}</span>
+    </div>
+  ) : (
+    // No Docker data:
+    <div id="docker-stats-chart">
+      <header id="docker-stats-chart-header">
+        No valid container ID for current microservice. Please ensure the microservice is running in
+        a Docker container.
+      </header>
+    </div>
+  );
+};
+
+export default DockerStatsChart;
