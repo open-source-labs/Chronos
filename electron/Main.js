c61523ee98c95c528db914187b74ae50da3f3874
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-26 18:21:28 -0700

    Install types for pg and mongoose

--- electron/Main.js
+++ electron/Main.js
@@ -1,8 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 const path = require('path');
-require('./routes/dashboard.js');
-require('./routes/data.js');
+require('./routes/dashboard');
+require('./routes/data');
 
 // Install React Dev Tools
 app.whenReady().then(() => {
