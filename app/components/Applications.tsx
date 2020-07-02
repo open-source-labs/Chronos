bb94e50af347a8517c22428fd5fb331f7919f57b
Michael <wang.3101@osu.edu>
2020-07-01 21:37:40 -0700

    Implement db deletion

--- app/components/Applications.tsx
+++ app/components/Applications.tsx
@@ -3,13 +3,28 @@ import { DashboardContext } from '../context/DashboardContext';
 import { makeStyles } from '@material-ui/core/styles';
 import { Paper, Grid, Button, IconButton } from '@material-ui/core';
 import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+// import { DashboardContext } from '../context/DashboardContext';
 
 const Applications = () => {
-  const { applications, getApplications } = useContext(DashboardContext);
+  const { applications, getApplications, deleteApp } = useContext(DashboardContext);
 
   useEffect(() => {
     getApplications();
   }, []);
+
+  // const { applications, deleteApp } = useContext(DashboardContext);
+
+  // Sends request to Main.js to delete application data
+  const onDelete = (index: number) => {
+    deleteApp(index);
+  };
+
+  // Ask user for deletetion confirmation
+  const confirmDelete = (app: string, i: number) => {
+    const message = `The application '${app}' will be permanently deleted. Continue?`;
+    // Proceed with deletion if user confirms
+    if (confirm(message)) onDelete(i);
+  };
 
   const useStyles = makeStyles(theme => ({
     container: {
@@ -38,18 +53,23 @@ const Applications = () => {
 
   const classes = useStyles();
   // Create a button for each app
-  return applications.map((app: string, i: string | undefined) => (
-    <Paper
-      className={classes.paper}
-      id={i}
-      key={i}
-      onClick={(event: React.MouseEvent<HTMLElement>) => console.log('clicking app!')}
-    >
-      {app}
-      <IconButton className={classes.customHoverFocus} aria-label="Delete">
+  return applications.map((app: string, i: number | any | string | undefined) => (
+    <div>
+      <Paper
+        className={classes.paper}
+        id={i}
+        key={i}
+        onClick={(event: React.MouseEvent<HTMLElement>) => console.log('clicking app!')}
+      ></Paper>
+      <IconButton
+        className={classes.customHoverFocus}
+        aria-label="Delete"
+        onClick={(event: React.MouseEvent<HTMLElement>) => confirmDelete(app, i)}
+      >
+        {app}
         <DeleteForeverOutlinedIcon />
       </IconButton>
-    </Paper>
+    </div>
   ));
 };
 
