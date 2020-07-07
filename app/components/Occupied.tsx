44602659fe475c537dcbc18c84c264a1e8c1af03
gregpalace <greg.palasciano@gmail.com>
2020-07-07 09:47:34 -0700

    Fix Grid Positioning

--- app/components/Occupied.tsx
+++ app/components/Occupied.tsx
@@ -12,13 +12,12 @@ const Occupied: React.FC = () => {
 
   const useStyles = makeStyles(theme => ({
     paper: {
-      position: 'relative',
-      height: 340,
-      width: '50%',
+      padding: theme.spacing(1),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
       whiteSpace: 'nowrap',
       marginBottom: theme.spacing(1),
       background: 'rgb(33, 34, 41)',
-      border: '2px solid black',
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
       '&:hover, &.Mui-focusVisible': { background: 'rgb(33, 34, 41)' },
     },
@@ -29,7 +28,6 @@ const Occupied: React.FC = () => {
       '&:hover, &.Mui-focusVisible': { color: 'rgb(255, 243, 72)' },
     },
     heroContent: {
-      // backgroundColor: '#f4f4f4'/* theme.palette.background.paper */,
       padding: theme.spacing(8, 0, 6),
     },
   }));
@@ -52,13 +50,11 @@ const Occupied: React.FC = () => {
         <AddModal setOpen={setOpen} />
       </Modal>
       {/* Grid Needs Aligning */}
-      <Grid container direction="column" alignItems="center" justify="space-between">
-        <Grid container item xs={12} sm={6}>
-          <Applications />
-          <Button className={classes.paper} onClick={() => setOpen(true)}>
-            <AddCircleOutlineTwoToneIcon className={classes.icon} style={{ boxShadow: 'none' }} />
-          </Button>
-        </Grid>
+      <Grid container spacing={6}>
+        <Applications />
+        <Button className={classes.paper} onClick={() => setOpen(true)}>
+          <AddCircleOutlineTwoToneIcon className={classes.icon} style={{ boxShadow: 'none' }} />
+        </Button>
       </Grid>
     </div>
   );
