cce4aa2e92e8d548880b43dee35552cb906124bb
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-09 16:19:03 -0700

    Prepare tests for <Home />

--- __tests__/app/components/Home.test.js
+++ __tests__/app/components/Home.test.js
@@ -1,1 +1,7 @@
+import chai from 'chai';
 
+describe('<Home />', () => {
+  it('should render an <img/> tag of the Chronos pangolin');
+  it("should render an <h1> tag that says 'Welcome to Chronos!'");
+  it("should render a <Link> tag that is labeled 'Get Started'");
+});
