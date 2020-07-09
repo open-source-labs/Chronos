7dceeaaf194b53e87f51e5a00dd1121eb26f5291
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-09 16:18:27 -0700

    Prepare tests for <DashboardContainer />

---
+++ __tests__/app/containers/DashboardContainer.test.js
@@ -0,0 +1,7 @@
+import chai from 'chai';
+
+describe('<DashboardContainer />', () => {
+  it('should render <SidebarContainer />');
+  it('should render <MainContainer />');
+  it('should render all of the contexts');
+});
