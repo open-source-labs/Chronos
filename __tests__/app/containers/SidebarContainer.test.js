dfd75747df622cc632416a598cf99a41197eaa71
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-09 16:24:51 -0700

    Prepare tests <SidebarContainer />

---
+++ __tests__/app/containers/SidebarContainer.test.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+describe('<SidebarContainer />', () => {
+  it('should render an <img> tag with the Chronos logo');
+  it("should render a <Link /> with the label as 'Home'");
+  it("should render a <Link /> with the label as 'About'");
+  it("should render a <Link /> with the label as 'Contact'");
+  it("should render a <Link /> with the label as 'Settings'");
+});
