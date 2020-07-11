155fd2f677d6b764502e9066a635b87ae72b384d
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-10 18:24:16 -0700

    Write unit test for Home.tsx

---
+++ __tests__/app/components/Home.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+// import Adapter from 'enzyme-adapter-react-16';
+import { mount } from 'enzyme';
+
+import Home from '../../../app/components/Home';
+import { DashboardContext } from '../../../app/context/DashboardContext';
+
+// configure({ adapter: new Adapter() });
+
+describe('<Home />', () => {
+  let wrapper: any;
+
+  beforeAll(() => {
+    const applications: any = [];
+    const getApplications: any = jest.fn();
+
+    const mockHome = (
+      <DashboardContext.Provider value={{ applications, getApplications }}>
+        <Home />
+      </DashboardContext.Provider>
+    );
+    wrapper = mount(mockHome);
+  });
+
+  it('should render an <img/> tag of the Chronos pangolin', () => {
+    const image = wrapper.find('img');
+    expect(image).toBeDefined();
+
+    const imageProps = image.props();
+    expect(imageProps).toHaveProperty('width');
+    expect(imageProps).toHaveProperty('height');
+    expect(imageProps).toHaveProperty('src');
+    expect(imageProps).toHaveProperty('alt');
+  });
+
+  it("should render an <h1> tag that says 'Welcome to Chronos!'", () => {
+    const h1Tag = wrapper.find('h1');
+    expect(h1Tag.text()).toMatch('Welcome to Chronos!');
+  });
+
+  it("should render a <Link> tag that is labeled 'Get Started'", () => {
+    const linkTag = wrapper.find('Link');
+    expect(linkTag).toBeDefined();
+    expect(linkTag.text()).toMatch('Get started');
+    expect(linkTag.prop('to')).toEqual('/applications');
+  });
+});
