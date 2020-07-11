71b9e52aa2e77d715d3da9a938a76c5b00df3ffc
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-10 18:23:50 -0700

    Configure jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
-const { defaults } = require('jest-config');
+// const { defaults } = require('jest-config');
 
 module.exports = {
-  preset: 'ts-jest',
+  verbose: true,
+  setupFiles: ['<rootDir>/__tests__/setup/test-setup.js'],
+  setupFilesAfterEnv: ['./__tests__/setup/windowMock.js'],
+  preset: 'ts-jest/presets/js-with-ts',
   moduleNameMapper: {
-    '\\.css$': '<rootDir>/__tests__/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__tests__/setup/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__tests__/setup/styleMock.js',
   },
+  collectCoverage: true,
 };
