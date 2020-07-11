0f4aa01f0a6f1239dd6e59a4adc2e258f8ff583e
gregpalace <greg.palasciano@gmail.com>
2020-07-11 16:35:58 -0700

    Add Render Test for Occupied

--- __tests__/app/components/Occupied.test.tsx
+++ __tests__/app/components/Occupied.test.tsx
@@ -3,18 +3,28 @@ import Enzyme, { mount } from 'enzyme';
 import { createMount, createShallow } from '@material-ui/core/test-utils';
 import Adapter from 'enzyme-adapter-react-16';
 
+import Occupied from '../../../app/components/Occupied';
+
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('React unit test', () => {
   describe('<Occupied />', () => {
     let mount: any;
+    let shallow: any;
 
     beforeEach(() => {
       mount = createMount();
+      shallow = createShallow();
     });
 
     afterEach(() => {
       mount.cleanUp();
+    });
+
+    it('Renders correctly', () => {
+      const wrapper = shallow(<Occupied />);
+      console.log(wrapper.debug());
+      expect(wrapper).toMatchSnapshot();
     });
   });
 });
