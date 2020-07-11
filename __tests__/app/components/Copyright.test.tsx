cedb8868d894f851ebc1265c9b10af1e52074183
gregpalace <greg.palasciano@gmail.com>
2020-07-11 14:41:41 -0700

    Creat tests for Copyright

--- __tests__/app/components/Copyright.test.tsx
+++ __tests__/app/components/Copyright.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { createMount, createShallow } from '@material-ui/core/test-utils';
+import { createMount, createShallow, createRender } from '@material-ui/core/test-utils';
+import { Link } from '@material-ui/core';
 
 import Copyright from '../../../app/components/Copyright';
 
@@ -9,20 +10,28 @@ describe('React unit test', () => {
     let wrapper: any;
     let mount: any;
     let shallow: any;
+    let copy: any = <Copyright />;
 
-    beforeAll(() => {
+    beforeEach(() => {
       mount = createMount();
-      shallow = createShallow();
-      wrapper = mount(<Copyright />);
+      shallow = createShallow(copy);
+      // wrapper = mount(<Copyright />);
     });
 
-    afterAll(() => {
+    afterEach(() => {
       mount.cleanUp();
     });
 
+    it('renders correctly', () => {
+      const wrapper = shallow(<Copyright />);
+      console.log(wrapper.debug());
+      expect(wrapper).toMatchSnapshot();
+    });
+
     it('should contain link to chronos website', () => {
-      const linkTag = wrapper.find('Link');
-      expect(linkTag).toBeDefined();
+      const wrapper = shallow(<Copyright />);
+      expect(wrapper.find(Link)).toHaveLength(1);
+      expect(wrapper.find(Link)).toHaveProperty('href');
     });
   });
 });
