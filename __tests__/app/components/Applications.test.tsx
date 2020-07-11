f969f535188bee3c153ab382bf0a96972b561fd6
gregpalace <greg.palasciano@gmail.com>
2020-07-11 13:06:47 -0700

    Perform First Test on App

--- __tests__/app/components/Applications.test.tsx
+++ __tests__/app/components/Applications.test.tsx
@@ -1,40 +1,35 @@
-xdescribe('<Applications />', () => {
-  it('should');
+import React from 'react';
+import { mount } from 'enzyme';
+import { createMount, createShallow } from '@material-ui/core/test-utils';
+
+import Applications from '../../../app/components/Applications';
+import { DashboardContext } from '../../../app/context/DashboardContext';
+
+describe('React unit tests', () => {
+  describe('<Application/>', () => {
+    let wrapper: any;
+    let mount: any;
+
+    beforeAll(() => {
+      const applications: any = [];
+      const getApplications: any = jest.fn();
+      mount = createMount();
+
+      const mockApp = (
+        <DashboardContext.Provider value={{ applications, getApplications }}>
+          <Applications />
+        </DashboardContext.Provider>
+      );
+      wrapper = mount(mockApp);
+    });
+
+    afterAll(() => {
+      mount.cleanUp();
+    });
+
+    it('should contain a modal', () => {
+      console.log(wrapper.debug());
+      expect(wrapper.length).toBe(1);
+    });
+  });
 });
-
-
-// import React from 'react';
-// import { configure, shallow, mount } from 'enzyme';
-// import { expect } from 'chai';
-// import { createShallow } from '@material-ui/core/test-utils';
-// import Adapter from 'enzyme-adapter-react-16';
-
-// // Import our Component and Icons
-// import Applications from '../../../app/components/Applications';
-// import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
-
-// configure({ adapter: new Adapter() });
-
-// describe('React unit tests', () => {
-//   describe('<Applications />', () => {
-//     let wrapper;
-
-//     beforeAll(() => {
-//       wrapper = shallow(<Applications />);
-//       // shallow = createShallow();
-//     });
-
-//     xit('Should render a delete icon', () => {
-//       // expect(wrapper.find(<DeleteForeverOutlinedIcon />));
-//     });
-
-//     xit('Should have delete functionality', () => {});
-
-//     xit('Should contain grid items', () => {});
-
-//     xit('Should have three grid items', () => {
-//       const wrapper = shallow(<Applications />);
-//       expect(wrapper.toHaveLength(3));
-//     });
-//   });
-// });
