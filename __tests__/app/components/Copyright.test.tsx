8d0539f40bbc5d38d88196b530e9aad494c01347
gregpalace <greg.palasciano@gmail.com>
2020-07-11 14:57:11 -0700

    Create additional Copyright tests

--- __tests__/app/components/Copyright.test.tsx
+++ __tests__/app/components/Copyright.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import { createMount, createShallow, createRender } from '@material-ui/core/test-utils';
-import { Link } from '@material-ui/core';
+import { Link, Typography } from '@material-ui/core';
+import Adapter from 'enzyme-adapter-react-16';
 
 import Copyright from '../../../app/components/Copyright';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('React unit test', () => {
   describe('<Copyright/>', () => {
@@ -14,12 +17,13 @@ describe('React unit test', () => {
 
     beforeEach(() => {
       mount = createMount();
-      shallow = createShallow(copy);
+      shallow = createShallow();
       // wrapper = mount(<Copyright />);
     });
 
     afterEach(() => {
       mount.cleanUp();
+      // shallow.cleanUp();
     });
 
     it('renders correctly', () => {
@@ -31,7 +35,19 @@ describe('React unit test', () => {
     it('should contain link to chronos website', () => {
       const wrapper = shallow(<Copyright />);
       expect(wrapper.find(Link)).toHaveLength(1);
-      expect(wrapper.find(Link)).toHaveProperty('href');
+      // expect(wrapper.find(Link)).toHaveProperty('href');
+    });
+
+    it('should route user to Chronos website', () => {
+      const mockedHandleClick = jest.fn();
+      wrapper.instance().handleClick = mockedHandleClick;
+      wrapper.find(Link).first().props().onClick();
+      expect(mockedHandleClick).toHaveBeenCalledTimes(1);
+    });
+
+    it('should render <Typography /> to display copyright info', () => {
+      const wrapper = shallow(<Copyright />);
+      expect(wrapper.find(Typography)).toHaveLength(1);
     });
   });
 });
