345398e39e35380e6fbcc864e1cb7a7e240f0c18
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-11 11:41:31 -0700

    Set up tests for App and AddModal

--- __tests__/app/App.test.tsx
+++ __tests__/app/App.test.tsx
@@ -1,4 +1,15 @@
-xdescribe('<App />', () => {
-  it('should render <Splash /> on the first visit');
-  it('should render <DashboardContainer /> if not on the first visit');
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../app/App';
+describe('<App />', () => {
+  let wrapper: any;
+  beforeAll(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('should render <Splash /> on the first visit', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+  // xit('should render <DashboardContainer /> if not on the first visit');
 });
--- __tests__/app/modals/AddModal.test.tsx
+++ __tests__/app/modals/AddModal.test.tsx
@@ -1,3 +1,38 @@
-xdescribe('<AddModal />', () => {
-  it('should');
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+
+import AddModal from '../../../app/modals/AddModal';
+import { DashboardContext } from '../../../app/context/DashboardContext';
+
+describe('<AddModal />', () => {
+  let wrapper: any;
+  beforeAll(() => {
+    const setOpen = jest.fn();
+    const addApp = jest.fn();
+
+    const mockAddModal = (
+      <DashboardContext.Provider value={{ addApp }}>
+        <AddModal setOpen={setOpen} />
+      </DashboardContext.Provider>
+    );
+
+    wrapper = shallow(<AddModal setOpen={setOpen}/>);
+  });
+
+  it('should render a <form> with an onSubmit property', () => {
+    // const mockOnSubmit = jest.fn();
+    // console.log(wrapper.debug());
+    // const thing = wrapper.find('.add-container form');
+    // console.log('thing ==>>', thing);
+  });
+
+  // it('should render a <select> with an onChange property');
+
+  // it('should render a <input> for db types with an onChange property');
+
+  // it('should render a <input> for db URI with an onChange property');
+
+  // it('should render a <input> for db name with an onChange property');
+
+  // it("should render a <button> with a label 'Submit'");
 });
