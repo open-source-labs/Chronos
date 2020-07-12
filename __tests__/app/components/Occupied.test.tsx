811afcc4d77e9b9697c4aa1827e9dfe6f4c522ae
gregpalace <greg.palasciano@gmail.com>
2020-07-11 19:41:43 -0700

    Run Tests on Occupied

--- __tests__/app/components/Occupied.test.tsx
+++ __tests__/app/components/Occupied.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Enzyme, { mount } from 'enzyme';
+import { Typography, Grid, Button } from '@material-ui/core';
 import { createMount, createShallow } from '@material-ui/core/test-utils';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -26,9 +27,27 @@ describe('React unit test', () => {
       console.log(wrapper.debug());
       expect(wrapper).toMatchSnapshot();
     });
+
+    it('should display text of our application page', () => {
+      const wrapper = shallow(<Occupied />);
+      expect(wrapper.find(Typography).text()).toMatch('Applications');
+    });
+
+    it('should render a grid containing our applications', () => {
+      const wrapper = shallow(<Occupied />);
+      expect(wrapper.find(Grid)).toHaveLength(1);
+      expect(wrapper.find(Grid).prop('container'));
+      expect(wrapper.find(Grid).prop('spacing'));
+    });
+
+    it('should render a button with an onClick attribute', () => {
+      const wrapper = shallow(<Occupied />);
+      const mockOnClick = jest.fn();
+      const button = wrapper.find(Button);
+      // button.simulate('click');
+      expect(button).toHaveLength(1);
+      expect(button.prop('onClick'));
+      expect(mockOnClick.mock.calls.length).toBe(0);
+    });
   });
 });
-
-// xdescribe('<Occupied />', () => {
-//   it('should');
-// });
