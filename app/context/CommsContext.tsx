0fd020c73aa58ca837acce329c39fe7b67066bbc
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-12 10:37:29 -0700

    Add caching

--- app/context/CommsContext.tsx
+++ app/context/CommsContext.tsx
@@ -22,16 +22,25 @@ export const CommsContext = React.createContext<any>(null);
 
 const CommsContextProvider: React.FC = ({ children }) => {
   const [commsData, setCommsData] = useState([]);
+  const [currentApp, setCurrentApp] = useState('');
 
   // Fetches all data related to communication for a particular app
-  const fetchCommsData = (index: number) => {
-    ipcRenderer.send('commsRequest', index);
-    ipcRenderer.on('commsResponse', (event: Electron.Event, data: any) => {
-      // Store resulting data in local state
-      const result = JSON.parse(data);
-      console.log('Number of data points (comms):', result.length);
-      setCommsData(result);
-    });
+  const fetchCommsData = (app: string, live: boolean) => {
+    /**
+     * Caches results of requesting communication data for a specific app
+     * Communication data will be the same across the microservices. Prevents
+     * fetch requests that result in repeating data 
+     */
+    if (app !== currentApp || live) {
+      setCurrentApp(app)
+      ipcRenderer.send('commsRequest', app);
+      ipcRenderer.on('commsResponse', (event: Electron.Event, data: any) => {
+        // Store resulting data in local state
+        const result = JSON.parse(data);
+        console.log('Number of data points (comms):', result.length);
+        setCommsData(result);
+      });
+    }
   };
 
   return (
