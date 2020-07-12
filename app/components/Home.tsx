7f242d9d617e3020a870dda1b8a686063e7cb551
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-11 21:11:50 -0700

    Reorganize imports

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../stylesheets/Home.css';
-import { Link } from 'react-router-dom';
 
 const Home = () => (
   <div className="home">
