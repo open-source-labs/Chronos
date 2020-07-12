ba1cfa3f7eff8d1052b9ea4ac13857355f018a7f
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-11 21:02:55 -0700

    Add route to applications page

--- app/containers/MainContainer.tsx
+++ app/containers/MainContainer.tsx
@@ -4,12 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 import GraphsContainer from './GraphsContainer';
 import Home from '../components/Home';
 import Copyright from '../components/Copyright';
+import Occupied from '../components/Occupied';
 
-// Follows the links in the Sidebar component
 const MainContainer = () => (
   <div style={{ flex: '1' }}>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/applications" component={Occupied} />
       <Route exact path="/:service" component={GraphsContainer} />
     </Switch>
     <Copyright />
