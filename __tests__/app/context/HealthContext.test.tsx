2f67c1bb6015da02e2679dc206c81605bc4b56c1
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-11 18:30:16 -0700

    Write tests for ipc processes

--- __tests__/app/context/HealthContext.test.tsx
+++ __tests__/app/context/HealthContext.test.tsx
@@ -1,3 +1,46 @@
-xdescribe('<HealthContext />', () => {
-  xit('should');
+import React, { useContext } from 'react';
+import { mount } from 'enzyme';
+const { ipcRenderer } = require('electron');
+
+import HealthContextProvider, { HealthContext } from '../../../app/context/HealthContext';
+
+jest.mock('electron', () => ({ ipcRenderer: { on: jest.fn(), send: jest.fn() } }));
+
+describe('<HealthContext />', () => {
+  let wrapper: any;
+  beforeEach(() => {
+    const TestComponent = () => {
+      const { fetchHealthData, healthData, setHealthData } = useContext(HealthContext);
+
+      return (
+        <>
+          <p>{JSON.stringify(healthData)}</p>
+          <button id="fetchHealthData" onClick={() => fetchHealthData('books')}>
+            Test fetchHealthData
+          </button>
+          <button>Test setHealthData</button>
+        </>
+      );
+    };
+
+    wrapper = mount(
+      <HealthContextProvider>
+        <TestComponent />
+      </HealthContextProvider>
+    );
+  });
+
+  it('should render', () => {
+    expect(wrapper.exists).toBeTruthy;
+  });
+
+  it("should emit the 'healthRequest' event and listen on 'healthResponse' when invoking ", () => {
+    const button = wrapper.find('#fetchHealthData');
+    button.simulate('click');
+    expect(ipcRenderer.send).toHaveBeenCalledWith('healthRequest', 'books');
+    expect(ipcRenderer.on).toHaveBeenCalledWith('healthResponse', expect.any(Function));
+  });
+
+  it('should update healthData when setHealthData is invoked with new data', () => {
+  });
 });
