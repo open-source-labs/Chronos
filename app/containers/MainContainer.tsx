56d5f31b01a08c465da986a9562ebb2d792fddef
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-12 00:17:49 -0700

    Create new routes to the separated pieces of data

--- app/containers/MainContainer.tsx
+++ app/containers/MainContainer.tsx
@@ -5,13 +5,15 @@ import GraphsContainer from './GraphsContainer';
 import Home from '../components/Home';
 import Copyright from '../components/Copyright';
 import Occupied from '../components/Occupied';
+import TempContainer from './TempContainer';
 
 const MainContainer = () => (
   <div style={{ flex: '1' }}>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/applications" component={Occupied} />
-      <Route exact path="/:service" component={GraphsContainer} />
+      <Route exact path="/applications/:app/:service" component={TempContainer} />
+      {/* <Route exact path="/:service" component={GraphsContainer} /> */}
     </Switch>
     <Copyright />
   </div>
