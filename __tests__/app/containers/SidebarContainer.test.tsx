59c02ac902d31f1df8aa26ee20ac99ba6a145fc5
tdwolf6 <rodrickbuckner@gmail.com>
2020-07-13 14:51:22 -0700

    Sidebar tests for  component rendering complete

--- __tests__/app/containers/SidebarContainer.test.tsx
+++ __tests__/app/containers/SidebarContainer.test.tsx
@@ -1,7 +1,54 @@
-xdescribe('<SidebarContainer />', () => {
-  it('should render an <img> tag with the Chronos logo');
-  it("should render a <Link /> with the label as 'Home'");
-  it("should render a <Link /> with the label as 'About'");
-  it("should render a <Link /> with the label as 'Contact'");
-  it("should render a <Link /> with the label as 'Settings'");
+import React from 'react';
+import { configure, shallow, render } from 'enzyme';
+
+import SidebarContainer from '../../../app/containers/SidebarContainer'
+import { Link } from 'react-router-dom';
+
+
+describe('<SidebarContainer />', () => {
+  it('should render an <img> tag with the Chronos logo', () => {
+    const wrapper = shallow(<SidebarContainer />);
+    console.log(wrapper.debug({ verbose: true }))
+
+    expect(wrapper.contains(<img alt="Chronos Logo" id="serviceDashLogo" src="../assets/icon2Cropped.png" />)).toBe(true);
+  });
+  it("should render a <Link /> with the label as 'Home'", () => {
+    const wrapper = shallow(<SidebarContainer />);
+
+    expect(wrapper.contains(<Link className="sidebar-link" to="/">
+    Home
+  </Link>)).toBe(true)
+  });
+  it("should render a <Link /> with the label as 'About'", () => {
+    const wrapper = shallow(<SidebarContainer />);
+
+    expect(wrapper.contains(<Link className="sidebar-link" to="/"> 
+    About 
+  </Link>)).toBe(true)
+  });
+  it("should render a <Link /> with the label as 'Contact'", () => {
+    const wrapper = shallow(<SidebarContainer />);
+
+    expect(wrapper.contains(<Link className="sidebar-link" to="/"> 
+    Contact 
+  </Link>)).toBe(true)
+  });
+  it("should render a <Link /> with the label as 'Settings'", () => {
+    const wrapper = shallow(<SidebarContainer />);
+
+    expect(wrapper.contains(<Link className="sidebar-link" to="/"> 
+    Settings 
+  </Link>)).toBe(true)
+  });
+
+  
 });
+
+
+
+
+
+
+
+
+  
