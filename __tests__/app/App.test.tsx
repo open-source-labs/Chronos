51f91df48e53c23ed5b5f1506f7cc230f524341c
gregpalace <greg.palasciano@gmail.com>
2020-07-13 15:31:32 -0700

    Add Tests to Check for Crashing

--- __tests__/app/App.test.tsx
+++ __tests__/app/App.test.tsx
@@ -1,15 +1,21 @@
-
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from '../../app/App';
+
 describe('<App />', () => {
   let wrapper: any;
   beforeAll(() => {
     wrapper = shallow(<App />);
   });
 
-  it('should render <Splash /> on the first visit', () => {
-    expect(wrapper).toMatchSnapshot()
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
-  // xit('should render <DashboardContainer /> if not on the first visit');
+
+  it('should render <Splash /> on the first visit', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 });
