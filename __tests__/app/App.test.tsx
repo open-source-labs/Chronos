d071833b7e826aa24ace45993a228be81da01a45
gregpalace <greg.palasciano@gmail.com>
2020-07-13 15:58:00 -0700

    Handle Merge Conflicts

--- __tests__/app/App.test.tsx
+++ __tests__/app/App.test.tsx
@@ -1,27 +1,20 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-=======
 import { shallow, mount } from 'enzyme';
->>>>>>> master
 import App from '../../app/App';
-
 describe('<App />', () => {
   let wrapper: any;
 
   beforeAll(() => {
     wrapper = shallow(<App />);
   });
-
+  it('should render <Splash /> on the first visit', () => {
+    expect(wrapper).toBeDefined();
+  });
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
-  });
-
-  it('should render <Splash /> on the first visit', () => {
-    expect(wrapper).toBeDefined();
   });
   it('should render <DashboardContainer /> if not on the first visit', () => {
     jest.useFakeTimers();
