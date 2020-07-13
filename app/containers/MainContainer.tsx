44ad1866ee075a5243404e614cc7654a8cd563c9
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-13 14:49:09 -0700

    Move styling to stylesheet

--- app/containers/MainContainer.tsx
+++ app/containers/MainContainer.tsx
@@ -6,16 +6,21 @@ import Home from '../components/Home';
 import Copyright from '../components/Copyright';
 import Occupied from '../components/Occupied';
 import GraphsContainer from './GraphsContainer';
+import '../stylesheets/MainContainer.scss';
 
 const MainContainer = () => (
-  <div style={{ flex: '1' }}>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/applications" component={Occupied} />
-      <Route exact path="/applications/:app/:service" component={GraphsContainer} />
-      <Route path="*" render={() => <h1>Not found</h1>} />
-    </Switch>
-    <Copyright />
+  <div className="main-container">
+    <div className="main-routes">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/applications" component={Occupied} />
+        <Route exact path="/applications/:app/:service" component={GraphsContainer} />
+        <Route path="*" render={() => <h1>Not found</h1>} />
+      </Switch>
+    </div>
+    <div className="copyright-container">
+      <Copyright />
+    </div>
   </div>
 );
 
