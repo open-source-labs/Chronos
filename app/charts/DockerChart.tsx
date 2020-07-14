2d3dfc60102fb254869cd38e48d555f5404b23ec
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-14 08:00:21 -0700

    Add style to docker chart

--- app/charts/DockerChart.tsx
+++ app/charts/DockerChart.tsx
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+
+import moment from 'moment';
 import { DockerContext } from '../context/DockerContext';
 
 interface IContainer {
@@ -40,10 +42,10 @@ const DockerStatsChart = () => {
   // Render the component if there is available data
   return containerid ? (
     <div className="chart docker-chart">
-      <header id="docker-stats-chart-header">Docker Container Stats</header>
+      <h2>Docker Container Stats</h2>
       <span>Container Name: {containername}</span>
       <span>Platform: {platform}</span>
-      <span>Start time: {starttime}</span>
+      <span>Start time: {moment(starttime).format('LLL')}</span>
       <span>Memory Usage: {(memoryusage / 1000000).toFixed(2)}</span>
       <span>Memory Limit: {(memorylimit / 1000000).toFixed(2)}</span>
       <span>Memory Percent: {memorypercent.toFixed(2)}%</span>
