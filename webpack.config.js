026b1c212961aec236955e7902e5bf37db67d376
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-15 23:37:43 -0700

    Bundle jpegs

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|svg|jpg|gif|mov)$/,
+        test: /\.(png|svg|jpe?g|gif|mov)$/,
         use: ['file-loader'],
       },
     ],
