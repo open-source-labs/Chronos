08226e46684c2de4751c62ebcc8be6c25e47a529
Michael <wang.3101@osu.edu>
2020-07-15 22:38:16 -0700

    Add NY team to team.jsx

--- client/components/pages/Team.jsx
+++ client/components/pages/Team.jsx
@@ -1,7 +1,67 @@
 import React from 'react';
 
-const Team = () => {
-  return <div></div>;
-};
+const bios = [
+  {
+    name: 'Ousman Diallo',
+    bioParagraph:
+      "Ousman is a full-stack software engineer passionate about building performant, modular React applications giving special interest to mobile-driven web development and SPAs. Ousman's other interest lie in  microservices, architecting/optimizing communication between distributed systems, and all things written by Tim Ferris.",
+    picture: '../assets/Bio_picture_ousman.jpg',
+    contact: {
+      gitHub: 'https://github.com/Dialloousman',
+      linkedIn: 'https://www.linkedin.com/in/ordiallo/',
+    },
+    location: 'New York',
+  },
+  {
+    name: 'Ben Mizel',
+    bioParagraph:
+      'Ben Mizel is a full-stack software engineer passionate about building adaptable, scalable, and resilient applications. He enjoys talking about microservices and distributed computing as well as the modular brilliance of IKEA shelves and Hershey bars.',
+    picture: '../assets/Bio_picture_ben.jpg',
+    contact: {
+      gitHub: 'https://github.com/ben-mizel/',
+      linkedIn: 'https://www.linkedin.com/in/ben-mizel/',
+    },
+    location: 'New York',
+  },
+  {
+    name: 'Chris Romano',
+    bioParagraph:
+      "With an eye for detail and ability across the stack, Chris is a powerful addition to the Chronos team.  While his visually creative front end solutions often headline, he's equipped to contribute in countless other ways.  From Webpack to data management to server side solutions in Node/Express, he can do it all.",
+    picture: '../assets/Bio_picture_Chris.jpg',
+    contact: {
+      gitHub: 'https://github.com/robicano22',
+      linkedIn: 'https://www.linkedin.com/in/chris-p-romano/',
+    },
+    location: 'New York',
+  },
+  {
+    name: 'Jenae Pennie',
+    bioParagraph:
+      'Jenae Pennie is a full-stack JavaScript Engineer specializing in React and Express Node. She is deeply passionate about designing and implementing modular full-stack applications. She has experience with React-Redux, React (Context, Hooks, Effects),  Dockerizing Applications, and Creating Microservices.',
+    picture: '../assets/Bio_picture_jenae.jpg',
+    contact: {
+      gitHub: 'https://github.com/jenaepen',
+      linkedIn: 'https://www.linkedin.com/in/jenae-pennie',
+    },
+    location: 'New York',
+  },
+  {
+    name: 'Time Pagra',
+    bioParagraph:
+      'Tim is an experienced fullstack software engineer with niche focus on Node/Express servers and React frontends. He is passionate about composition and reusability in software development, giving special interest to decomposing monolithic applications into micro service applications.',
+    picture: '../assets/Bio_picture_Tim.jpg',
+    contact: {
+      gitHub: 'https://github.com/timpagra',
+      linkedIn: 'https://www.linkedin.com/in/timpagra/',
+    },
+    location: 'New York',
+  },
+];
+
+const Team = () => (
+  <div>
+    <div>Placeholder</div>
+  </div>
+);
 
 export default Team;
