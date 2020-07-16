4a8cf472b91b787a8c86e97b9f3a0012442c2f43
Michael <wang.3101@osu.edu>
2020-07-15 20:33:03 -0700

    Edit description in home.jsx

--- client/components/DropMenu.jsx
+++ client/components/DropMenu.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Logo from './Logo.jsx'
+import Logo from './Logo.jsx';
 import '../index.css';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const DropMenu = () => {
-    return (
-        <div className="DropMenu">
-            <li id="MenuItem"><Link to="/">Chronos</Link> </li>
-            <li id="MenuItem"><Link to="/demo">Demo</Link></li>
-            <li id="MenuItem"><a href="https://www.npmjs.com/package/chronos-microservice-debugger4">NPM</a></li>
-            <li id="MenuItem"><Link to="/team">Team</Link>  </li>
-            <li id="MenuItem"><a href="https://github.com/oslabs-beta/Chronos">Github</a></li>
-        </div>
-    )
+  return (
+    <div className="DropMenu">
+      <li id="MenuItem">
+        <Link to="/">Chronos</Link>{' '}
+      </li>
+      <li id="MenuItem">
+        <Link to="/demo">Demo</Link>
+      </li>
+      <li id="MenuItem">
+        <a href="https://www.npmjs.com/package/chronos-tracker">NPM</a>
+      </li>
+      <li id="MenuItem">
+        <Link to="/team">Team</Link>{' '}
+      </li>
+      <li id="MenuItem">
+        <Link to="/contact">Contact</Link>
+      </li>
+      <li id="MenuItem">
+        <a href="https://github.com/oslabs-beta/Chronos">Github</a>
+      </li>
+    </div>
+  );
 };
 
 export default DropMenu;
