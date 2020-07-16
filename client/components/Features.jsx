754b23737396e94bd8cbf6ca32c34901e44c4250
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-15 20:59:58 -0700

    Write features

---
+++ client/components/Features.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Features = () => (
+  <div className="section-features">
+    <div className="features-header">
+      <h2>What is Chronos?</h2>
+      <p>Something something something...</p>
+    </div>
+    <div class="features-content">
+      <div className="feature-1">
+        <h3>Monitors Application Information</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ad voluptatibus, a
+          doloribus modi explicabo! Ipsum harum eveniet perferendis
+        </p>
+      </div>
+      <div className="feature-2">
+        <h3>Configurable Notifications</h3>
+        <p>
+          Ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ad voluptatibus, a doloribus
+          modi explicabo! Ipsum harum eveniet perferendis eos, totam excepturi quo illum repellendus
+          eius nihil, ar
+        </p>
+      </div>
+      <div className="feature-3">
+        <h3>Open Source and Free to Use</h3>
+        <p>
+          abo! Ipsum harum eveniet perferendis eos, totam excepturi quo illum repellendus eius
+          nihil, architecto accusantium odit?
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Features;
