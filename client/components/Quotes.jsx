4d6da9c33a5e444e7e9942225991b7c405b599c4
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-16 11:46:01 -0700

    Add breakpoints

--- client/components/Quotes.jsx
+++ client/components/Quotes.jsx
@@ -7,10 +7,13 @@ const Quotes = () => (
   <div className="section-quotes">
     <div className="quote-container">
       <img id="dave-img" alt="none" src={Dave} />
+      <div className="quote-content">
       <p>
-        "The application is incredibly responsive. Wonderful stuff, a lot of work has been done
-        here."
+        <em>"The application is incredibly responsive. Wonderful stuff, a lot of work has been done
+        here."</em>
       </p>
+      <p>- Dave O'Sullivan</p>
+      </div>
     </div>
   </div>
 );
