20abb3fd7624a69fda30b3798a9cf12ba761edd8
Michael <wang.3101@osu.edu>
2020-07-16 16:18:45 -0700

    Swap lorem ipsum for correct descriptions in features

--- client/components/Features.jsx
+++ client/components/Features.jsx
@@ -7,9 +7,8 @@ const Features = () => (
     <div className="features-header">
       <h2>What is Chronos?</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum totam doloremque odit eum
-        eaque a. Sunt recusandae commodi blanditiis sint dolorem at, soluta, libero unde inventore
-        facilis officia explicabo quas.
+        Chronos is an Open Source network and health monitoring tool for your
+        system of services, containerized or not.
       </p>
     </div>
     <div class="features-content">
@@ -17,25 +16,24 @@ const Features = () => (
         <ion-icon name="videocam-outline"></ion-icon>
         <h3>Monitors Your Application</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ad voluptatibus, a
-          doloribus modi explicabo! Ipsum harum eveniet perferendis
+          Install an NPM package and create a configuration file to peer into
+          the inner workings of your application.
         </p>
       </div>
       <div className="feature-2">
         <ion-icon name="notifications-outline"></ion-icon>
         <h3>Configurable Notifications</h3>
         <p>
-          Ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ad voluptatibus, a doloribus
-          modi explicabo! Ipsum harum eveniet perferendis eos, totam excepturi quo illum repellendus
-          eius nihil, ar
+          Receive remote notifications via Slack or e-mail on critical failure
+          of services.
         </p>
       </div>
       <div className="feature-3">
         <ion-icon name="logo-github"></ion-icon>
         <h3>Open Source and Free to Use</h3>
         <p>
-          abo! Ipsum harum eveniet perferendis eos, totam excepturi quo illum repellendus eius
-          nihil, architecto accusantium odit?
+          Chronos is Open Source and free for the community. Contributions and
+          stars are welcome. Visit our GitHub!
         </p>
       </div>
     </div>
