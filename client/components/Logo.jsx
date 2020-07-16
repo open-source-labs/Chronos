2871f797d0100e08419154ea2a4ead19edebea75
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-07-15 21:00:20 -0700

    Create logo placeholder

--- client/components/Logo.jsx
+++ client/components/Logo.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
-import logo from '../assets/icon.png';
 
-class Logo extends React.Component {
-    render () {
-        return (
-            <div >
-                <img className="MainImage"  src={logo} />
-            </div>
-        )
-    }
-}
+const Logo = () => {
+  return <div></div>;
+};
 
 export default Logo;
