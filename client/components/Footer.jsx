32cfa89b4ccfcc36e61011e354e76fd46e8011ca
Michael <wang.3101@osu.edu>
2020-07-15 21:47:24 -0700

    Create footer

---
+++ client/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../index.css';
+import Logo from '../assets/icon.png';
+
+const Footer = () => (
+  <div className="footer-container">
+    <img id="logo-img" alt="none" src={Logo} />
+  </div>
+);
+
+export default Footer;
