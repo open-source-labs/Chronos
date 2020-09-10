b73c6620c08108750ba85e99bf64ba837839d833
kiso <dotkiso@gmail.com>
2020-09-10 11:05:38 -0400

    modified Occupied

--- app/components/Occupied.tsx
+++ app/components/Occupied.tsx
@@ -158,12 +158,12 @@ const Occupied: React.FC = () => {
               </Card>
             </div>
           ))}
-          {/* <Modal open={open} onClose={() => setOpen(false)}>
+          <Modal open={open} onClose={() => setOpen(false)}>
             <ServicesModal open={open} onClose={() => setOpen(false)} i={index} app={app} />
-          </Modal> */}
-          {/* <Modal open={open} onClose={() => setOpen(false)}>
+          </Modal>
+          <Modal open={open} onClose={() => setOpen(false)}>
             <AddModal open={open} onClose={() => setOpen(false)} setOpen={setOpen} />
-          </Modal> */}
+          </Modal>
         </div>
       </div>
     </div>
