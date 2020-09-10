83d2a6a9089ca1f87562209bae478a7300bddf8e
lu0713 <lumie.song@gmail.com>
2020-09-10 03:57:12 -0400

    fixed sidebar. about to fix main dashboard area

--- app/containers/SidebarContainer.tsx
+++ app/containers/SidebarContainer.tsx
@@ -5,28 +5,40 @@ import HomeSharpIcon from '@material-ui/icons/HomeSharp';
 import InfoIcon from '@material-ui/icons/Info';
 import ContactSupportIcon from '@material-ui/icons/ContactSupport';
 import SettingsIcon from '@material-ui/icons/Settings';
-import '../stylesheets/SidebarContainer.css';
+import '../stylesheets/SidebarContainer.scss';
 
 const SidebarContainer: React.FC = (): JSX.Element => (
   <div className="sidebar-container" id="mySidebar">
     <div className="sidebar">
-      <img alt="Chronos Logo Mini" id="miniLogo" src={'../assets/C.svg'} />
-      <Link className="sidebar-link" to="/" id="home">
-        <HomeSharpIcon style={{ boxShadow: 'none', width: '40px', height: '40px' }} />
-        &emsp;Home
-      </Link>
-      <Link className="sidebar-link" to="/about" id="about">
-        <InfoIcon style={{ boxShadow: 'none', width: '40px', height: '40px' }} />
-        &emsp;About
-      </Link>
-      <Link className="sidebar-link" to="/contact" id="contact">
-        <ContactSupportIcon style={{ boxShadow: 'none', width: '40px', height: '40px' }} />
-        &emsp;Contact
-      </Link>
-      <Link className="sidebar-link" to="/settings" id="settings">
-        <SettingsIcon style={{ boxShadow: 'none', width: '40px', height: '40px' }} />
-        &emsp;Settings
-      </Link>
+      <div className="firstRow">
+        <span><img alt="C" id="C" src={'../assets/C.svg'} /></span>
+        <img alt="Chronos" id="logo" src={'../assets/logo.svg'} />
+      </div>
+      <hr className="line" id="firstLine"></hr>
+
+      <div className="secondRow">
+      </div>
+
+      <hr className="line" id="secondLine"></hr>
+
+      <div className="thirdRow">
+        <Link className="sidebar-link" to="/" id="home">
+          <HomeSharpIcon style={{ boxShadow: 'none', width: '40px', height: '40px' }} />
+          &emsp;Home
+        </Link>
+        <Link className="sidebar-link" to="/about" id="about">
+          <InfoIcon style={{ boxShadow: 'none', width: '40px', height: '40px' }} />
+          &emsp;About
+        </Link>
+        <Link className="sidebar-link" to="/contact" id="contact">
+          <ContactSupportIcon style={{ boxShadow: 'none', width: '40px', height: '40px' }} />
+          &emsp;Contact
+        </Link>
+        <Link className="sidebar-link" to="/settings" id="settings">
+          <SettingsIcon style={{ boxShadow: 'none', width: '40px', height: '40px' }} />
+          &emsp;Settings
+        </Link>
+      </div>
     </div>
   </div>
 );
