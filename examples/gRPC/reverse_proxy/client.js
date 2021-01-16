747766216fc1b6fdec70c3a2c09853d208f1e010
Matt Jiang <mattljiang@gmail.com>
2021-01-15 20:14:50 -0800

    setup addBook. reverse_proxy client connected to books server

--- examples/gRPC/reverse_proxy/client.js
+++ examples/gRPC/reverse_proxy/client.js
@@ -1,19 +1,64 @@
-const PROTO_PATH = './reverseproxy.proto';
 
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
+const { ModuleResolutionKind } = require('typescript');
+const PROTO_PATH = './reverseproxy.proto';
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    arrays: true
+  keepCase: true,
+  longs: String,
+  enums: String,
+  arrays: true
 });
-console.log('Hi');
+
+/**
+ * Proxy to Order
+ */
 const ProxyToOrderService = grpc.loadPackageDefinition(packageDefinition).ProxyToOrder;
-const client = new ProxyToOrderService(
+const orderClient = new ProxyToOrderService(
     "localhost:30043",
     grpc.credentials.createInsecure()
 );
 
-module.exports = client;
+/**
+ * Proxy to Book
+ */
+const ProxyToBookService = grpc.loadPackageDefinition(packageDefinition).ProxyToBook;
+const bookClient = new ProxyToBookService(
+    "localhost:30044",
+    grpc.credentials.createInsecure()
+);
+
+module.exports = {
+  orderClient,
+  bookClient,
+}
+
+orderClient.getOrders(null, (err, data) => {
+  if (err !== null) {
+    console.log('err')
+    console.log(err)
+  }
+  console.log('getOrders response: ', data);
+})
+
+const harryPotter = {
+  title: 'Harry',
+  author: 'JK',
+  pageCount: 561,
+  publisher: 'Matt and Vince',
+}
+
+bookClient.addBook(harryPotter, (err, data) => {
+  if (err !== null) {
+    console.log('err')
+    console.log(err)
+  }
+  console.log('addBook response: ', data);
+})
+
+//this book server.js
+// const bookSErver = require('this file')
+// bookServer.bookClient 
+module.exports = orderClient;
+// exports.orderClient = orderClient;
