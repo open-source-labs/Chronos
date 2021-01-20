d45a0e5eb7417bf3f5c7dc5eaf83f30fd6eaf1d7
Matt Jiang <mattljiang@gmail.com>
2021-01-19 16:51:00 -0800

    Clean Up Frontend change text in html, change getOrders so it deletes currently displayed orders  when clicked again

--- examples/gRPC/reverse_proxy/index.js
+++ examples/gRPC/reverse_proxy/index.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('hello')
+  console.log('hello');
   const addBookBtn = document.getElementById('create1');
   addBookBtn.addEventListener('click', addBook);
-  const createOrderBtn = document.getElementById('create3');
-  createOrderBtn.addEventListener('click', createOrder);
+  const addOrderBtn = document.getElementById('create3');
+  addOrderBtn.addEventListener('click', addOrder);
   const getOrdersBtn = document.getElementById('orderInfo');
   getOrdersBtn.addEventListener('click', getOrders);
 });
@@ -21,46 +21,49 @@ const addBook = () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ title, author, numberOfPages, publisher, bookID }),
-    }).catch((err) => {
+    }).catch(err => {
       console.log(err);
     });
   }
-}
+};
 
-const createOrder = () => {
+const addOrder = () => {
   const customerID = document.getElementById('field_A3').value;
   const bookID = document.getElementById('field_B3').value;
   const purchaseDate = document.getElementById('field_C3').value;
   const deliveryDate = document.getElementById('field_D3').value;
   if (customerID !== '' && bookID !== '' && purchaseDate !== '' && deliveryDate !== '') {
-    fetch('/createOrder', {
+    fetch('/addOrder', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ customerID, bookID, purchaseDate, deliveryDate }),
-    }).catch((err) => {
+    }).catch(err => {
       console.log(err);
     });
   }
-}
+};
 
 const getOrders = () => {
-  console.log('hello')
+  const display = document.querySelector('#display');
+  
+  // remove currently displayed list items
+  while (display.firstChild) {
+    display.removeChild(display.firstChild);
+  }
+
   fetch('http://localhost:3000/orders', {
     method: 'GET',
   })
-  .then((response) => response.json())
-  .then((orders) => {
-    const display = document.querySelector('#display');
-    console.log(orders);
-    orders.orderList.forEach((order) => {
-      // create li for each order, that just displays all order properties
-      const orderListItem = document.createElement('li');
-      const orderListItemText = document.createTextNode(JSON.stringify(order));
-      orderListItem.appendChild(orderListItemText);
-      // append li to display ul
-      display.appendChild(orderListItem);
-    })
-  })
-}
+    .then(response => response.json())
+    .then(data => {
+      data.orderList.forEach(order => {
+        const orderListItem = document.createElement('li');
+        const orderListItemText = document.createTextNode(`CustomerID: ${order.customerID}. Purchase Date: ${order.purchaseDate}. Delivery Date: ${order.deliveryDate}. Book Info: ${order.title} by ${order.author}, ${order.publisher}, ${order.numberOfPages} pages.`);
+        orderListItem.appendChild(orderListItemText);
+
+        display.appendChild(orderListItem);
+      });
+    });
+};
