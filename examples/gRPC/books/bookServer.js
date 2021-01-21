b60c142c71a63e6fd9c003d1551d5106f76720d6
Matt Jiang <mattljiang@gmail.com>
2021-01-21 11:15:53 -0800

    Try Adding Metadata to Client Wrapper currently bookServer and bookClient are wrapped. adding metadata din client wrapper does not work. but adding metadata in reverseProxyServer and passing it as parameter to wrapper works.

--- examples/gRPC/books/bookServer.js
+++ examples/gRPC/books/bookServer.js
@@ -23,49 +23,11 @@ const server = new grpc.Server();
 /**
  * SERVER WRAPPER
  */
-// const proxyToBookServerWrapper = new HorusServerWrapper(server, bookProto.ProxyToBook.service, {
-//   addBook: (call, callback) => {
-//     // get the properties from the gRPC client call
-//     const { title, author, numberOfPages, publisher, bookID } = call.request;
-//     // create a book in our book collection
-//     BookModel.create({
-//       title,
-//       author,
-//       numberOfPages,
-//       publisher,
-//       bookID,
-//     })
-//     .then((data) => {
-//       callback(null, {});
-//     })
-//     .catch((err) => {
-//       if (err.code === 11000) {
-//         callback({
-//           code: grpc.status.ALREADY_EXISTS,
-//           details: "BookID already exists"
-//         })
-//       }
-//     })
-//   },
-// });
-
-// const orderToBookServerWrapper = new HorusServerWrapper(server, bookProto.OrderToBook.service, {
-//   getBookInfo: (call, callback) => {
-//     BookModel.findOne({ bookID: call.request.bookID }, (err, data) => {
-//       callback(null, data);
-//     })
-//   }
-// })
-
-/**
- * SERVER WITHOUT WRAPPER
- */
-server.addService(bookProto.ProxyToBook.service, {
+const proxyToBookServerWrapper = new HorusServerWrapper(server, bookProto.ProxyToBook.service, {
   addBook: (call, callback) => {
-    var myVals = call.metadata.get("key"); 
-    var myVal = myVals[0];
+    const myVals = call.metadata.get('key'); 
+    const myVal = myVals[0];
     console.log('metadata received by bookServer', myVals);
-
     // get the properties from the gRPC client call
     const { title, author, numberOfPages, publisher, bookID } = call.request;
     // create a book in our book collection
@@ -90,13 +52,55 @@ server.addService(bookProto.ProxyToBook.service, {
   },
 });
 
-server.addService(bookProto.OrderToBook.service, {
-  getBookInfo: (call, callback) => {
-    BookModel.findOne({ bookID: call.request.bookID }, (err, data) => {
-      callback(null, data);
-    })
-    },
-})
+// const orderToBookServerWrapper = new HorusServerWrapper(server, bookProto.OrderToBook.service, {
+//   getBookInfo: (call, callback) => {
+//     BookModel.findOne({ bookID: call.request.bookID }, (err, data) => {
+//       callback(null, data);
+//     })
+//   }
+// })
+
+/**
+ * SERVER WITHOUT WRAPPER
+ */
+// server.addService(bookProto.ProxyToBook.service, {
+//   addBook: (call, callback) => {
+//     var myVals = call.metadata.get('key'); 
+//     var myVal = myVals[0];
+//     console.log('metadata received by bookServer', myVals);
+//     // console.log('call.metadata: ', call.metadata);
+
+//     // get the properties from the gRPC client call
+//     const { title, author, numberOfPages, publisher, bookID } = call.request;
+//     // create a book in our book collection
+//     BookModel.create({
+//       title,
+//       author,
+//       numberOfPages,
+//       publisher,
+//       bookID,
+//     })
+//     .then((data) => {
+//       callback(null, {});
+//     })
+//     .catch((err) => {
+//       if (err.code === 11000) {
+//         callback({
+//           code: grpc.status.ALREADY_EXISTS,
+//           details: "BookID already exists"
+//         })
+//       }
+//     })
+//   },
+// });
+
+// server.addService(bookProto.OrderToBook.service, {
+//   getBookInfo: (call, callback) => {
+//     BookModel.findOne({ bookID: call.request.bookID }, (err, data) => {
+//       callback(null, data);
+//     })
+//     },
+// })
 
 // console.log('server wrapper: ', server);
 // console.log('server without wrapper: ', server);
