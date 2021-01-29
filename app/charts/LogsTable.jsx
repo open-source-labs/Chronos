f57bd71f180dae77d419ca92e141ddd0ff6c8683
Kit Loong Yee <kitloong@gmail.com>
2021-01-29 16:41:57 -0600

    fix hardcorded data to correct data type

--- app/charts/LogsTable.jsx
+++ app/charts/LogsTable.jsx
@@ -2,26 +2,23 @@ import React, { useContext } from 'react';
 import styled from 'styled-components';
 import {
   useTable,
-  usePagination,
-  useSortBy,
-  useFilters,
   useGroupBy,
   useExpanded,
 } from 'react-table';
 
-import { CommsContext } from '../context/CommsContext';
+// import { CommsContext } from '../context/CommsContext';
 
 
 const commsData = [
   {
-    "_id":{"$oid":"6004b4e1d3f0bbd0f6a4081b"},
+    "_id":"6004b4e1d3f0bbd0f6a4081b",
     "microservice":"reverse-proxy",
     "endpoint":"/books/createbook",
     "request":"POST",
     "correlatingid":"700e36a8-334f-4ffb-babd-c0b3ec2a27f0",
-    "responsestatus":{"$numberInt":"200"},
+    "responsestatus": "200",
     "responsemessage":"OK",
-    "time":{"$date":{"$numberLong":"1610921185595"}},
+    "time": "2021-01-17T22:06:25.595+00:00",
     "__v":{"$numberInt":"0"}
   },
   {
@@ -30,9 +27,9 @@ const commsData = [
     "endpoint":"/books/createbook",
     "request":"POST",
     "correlatingid":"700e36a8-334f-4ffb-babd-c0b3ec2a27f0",
-    "responsestatus":{"$numberInt":"200"},
+    "responsestatus": "200",
     "responsemessage":"OK",
-    "time":{"$date":{"$numberLong":"1610921185582"}},
+    "time": "2021-01-17T22:06:25.582+00:00",
     "__v":{"$numberInt":"0"}
   },
   {
@@ -41,9 +38,9 @@ const commsData = [
     "endpoint":"/customers/createcustomer",
     "request":"POST",
     "correlatingid":"8d77bfc8-fce2-4279-ae6f-cccbb9277686",
-    "responsestatus":{"$numberInt":"200"},
+    "responsestatus": "200",
     "responsemessage":"OK",
-    "time":{"$date":{"$numberLong":"1610921223980"}},
+    "time":"2021-01-17T22:07:03.980+00:00",
     "__v":{"$numberInt":"0"}
   },
   {
@@ -52,9 +49,9 @@ const commsData = [
     "endpoint":"/customers/createcustomer",
     "request":"POST",
     "correlatingid":"8d77bfc8-fce2-4279-ae6f-cccbb9277686",
-    "responsestatus":{"$numberInt":"200"},
+    "responsestatus": "200",
     "responsemessage":"OK",
-    "time":{"$date":{"$numberLong":"1610921223980"}},
+    "time": "2021-01-17T22:07:03.989+00:00",
     "__v":{"$numberInt":"0"}
   },
   {
@@ -63,9 +60,9 @@ const commsData = [
     "endpoint":"/customers/createcustomer",
     "request":"POST",
     "correlatingid":"1086e81d-01b6-4b52-aabb-ae47430fccaf",
-    "responsestatus":{"$numberInt":"200"},
+    "responsestatus": "200",
     "responsemessage":"OK",
-    "time":{"$date":{"$numberLong":"1610921262603"}},
+    "time": "2021-01-17T22:07:42.603+00:00",
     "__v":{"$numberInt":"0"}
   },
   {
@@ -74,9 +71,9 @@ const commsData = [
     "endpoint":"/customers/createcustomer",
     "request":"POST",
     "correlatingid":"1086e81d-01b6-4b52-aabb-ae47430fccaf",
-    "responsestatus":{"$numberInt":"200"},
+    "responsestatus": "200",
     "responsemessage":"OK",
-    "time":{"$date":{"$numberLong":"1610921262603"}},
+    "time":"2021-01-17T22:07:42.603+00:00",
     "__v":{"$numberInt":"0"}
   },
   {
@@ -85,9 +82,9 @@ const commsData = [
     "endpoint":"/orders/createorder",
     "request":"POST",
     "correlatingid":"43f0cc03-7e1d-49b0-84bd-740e82fa4326",
-    "responsestatus":{"$numberInt":"200"},
+    "responsestatus": "200",
     "responsemessage":"OK",
-    "time":{"$date":{"$numberLong":"1610921283121"}},
+    "time":"2021-01-17T22:08:03.121+00:00",
     "__v":{"$numberInt":"0"}
   },
   {
@@ -96,9 +93,9 @@ const commsData = [
     "endpoint":"/orders/createorder",
     "request":"POST",
     "correlatingid":"43f0cc03-7e1d-49b0-84bd-740e82fa4326",
-    "responsestatus":{"$numberInt":"200"},
+    "responsestatus": "200",
     "responsemessage":"OK",
-    "time":{"$date":{"$numberLong":"1610921283123"}},
+    "time":"2021-01-17T22:08:03.123+00:00",
     "__v":{"$numberInt":"0"}
   },
 ]
@@ -140,8 +137,8 @@ const Styles = styled.div`
 
 const data = commsData;
 
-const Table = () => {
-
+const Table = ({ columns, data }) => {
+  
   const {
     getTableProps,
     getTableBodyProps,
@@ -149,27 +146,21 @@ const Table = () => {
     rows,
     prepareRow,
     state: { groupBy, expanded },
-  } = useTable(
-    {
-      columns, data,
-    },
-    useGroupBy,
-    useExpanded
-  )
+  } = useTable({ columns, data }, useGroupBy, useExpanded)
 
   // set the number of rows to render
-  const numberOfRows = 20
-  const firstPageRows = rows.slice(0, numberOfRows)
+  const numberOfRows = 20;
+  const firstPageRows = rows.slice(0, numberOfRows);
 
   return (
     <div>
-      <Legend />   
+      {/* <Legend />    */}
       <table {...getTableProps()}>
         <thead>
           {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroups.headers.map(column => (
-                <th {...column.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
                   {column.canGroupBy ? (
                     <span {...column.getGroupByToggleProps()}>
                       {column.isGrouped ? '➖ ' : '➕ '} 
@@ -182,10 +173,10 @@ const Table = () => {
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {/* {firstPageRows.map((row, 0) => {
-            prepareRow(row) */}
+          {firstPageRows.map((row) => {
+            prepareRow(row)
             return (
-              <tr {...row.getTowProps()}>
+              <tr {...row.getRowProps()}>
               {row.cells.map(cell => {
                 return (
                   <td 
@@ -209,7 +200,6 @@ const Table = () => {
                         </span>{' '}
                         {cell.render('Cell')} ({row.subRows.length})
                       </>
-
                     ) : cell.isAggregated ? (
                       // If the cell is aggregated, use the Aggregated renderer for cell
                       cell.render('Aggregated')  
@@ -221,16 +211,14 @@ const Table = () => {
                 )
               })}
               </tr>
-            );
-          {/* })} */}
-
+            )
+          })}
         </tbody>
       </table>
-      <div>Showing the first {numberOfRows} of {row.length} rows</div>
+      <div>Showing the first {rows.length} of {rows.length} rows</div>
     </div>
   );
 };
-
 
 const LogsTable = () => {
   const columns = React.useMemo(() => [
@@ -265,112 +253,17 @@ const LogsTable = () => {
       Header: 'Response Message',
       accessor: 'responsemessage',
     }
-  ], [])
+  ], []);
+
+  const data = React.useMemo(() => [], []);
 
   return (
     <Styles>
       <Table columns={columns} data={data} />
     </Styles>
-  )
-}
+  );
+};
 
 
-// const data = React.useMemo(() => {
-//   const communicationsData = useContext(CommsContext).commsData;
-// })
 
 export default LogsTable;
-
-
-// import { useTable } from 'react-table';
-// import React, { useContext } from 'react';
-
-
-//  export default function LogsTable() {
-//    const data = React.useMemo(
-//      () => [
-//        {
-//          col1: 'Hello',
-//          col2: 'World',
-//        },
-//        {
-//          col1: 'react-table',
-//          col2: 'rocks',
-//        },
-//        {
-//          col1: 'whatever',
-//          col2: 'you want',
-//        },
-//      ],
-//      []
-//    )
- 
-//    const columns = React.useMemo(
-//      () => [
-//        {
-//          Header: 'Column 1',
-//          accessor: 'col1', // accessor is the "key" in the data
-//        },
-//        {
-//          Header: 'Column 2',
-//          accessor: 'col2',
-//        },
-//      ],
-//      []
-//    )
- 
-//    const {
-//      getTableProps,
-//      getTableBodyProps,
-//      headerGroups,
-//      rows,
-//      prepareRow,
-//    } = useTable({ columns, data })
- 
-//    return (
-//      <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-//        <thead>
-//          {headerGroups.map(headerGroup => (
-//            <tr {...headerGroup.getHeaderGroupProps()}>
-//              {headerGroup.headers.map(column => (
-//                <th
-//                  {...column.getHeaderProps()}
-//                  style={{
-//                    borderBottom: 'solid 3px red',
-//                    background: 'aliceblue',
-//                    color: 'black',
-//                    fontWeight: 'bold',
-//                  }}
-//                >
-//                  {column.render('Header')}
-//                </th>
-//              ))}
-//            </tr>
-//          ))}
-//        </thead>
-//        <tbody {...getTableBodyProps()}>
-//          {rows.map(row => {
-//            prepareRow(row)
-//            return (
-//              <tr {...row.getRowProps()}>
-//                {row.cells.map(cell => {
-//                  return (
-//                    <td
-//                      {...cell.getCellProps()}
-//                      style={{
-//                        padding: '10px',
-//                        border: 'solid 1px gray',
-//                        background: 'papayawhip',
-//                      }}
-//                    >
-//                      {cell.render('Cell')}
-//                    </td>
-//                  )
-//                })}
-//              </tr>
-//            )
-//          })}
-//        </tbody>
-//      </table>
-//    )
-//  }
