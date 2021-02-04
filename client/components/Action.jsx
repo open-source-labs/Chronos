108c988514bb0f37521037b478ceb2a26456d2c6
Matt Jiang <mattljiang@gmail.com>
2021-02-03 22:33:22 -0800

    add OS icons to Action

--- client/components/Action.jsx
+++ client/components/Action.jsx
@@ -8,8 +8,16 @@ const Action = () => {
       <div id="action-header">
         <h1>Ready to Begin?</h1>
       </div>
+      <div className="action">
+        <div className="download-btns">
+          <img src="https://img.icons8.com/metro/52/000000/mac-os.png"/>
+          <img src="https://img.icons8.com/metro/52/000000/linux.png"/>
+          <img src="https://img.icons8.com/material-sharp/48/000000/windows-client.png"/>
+        </div>
+        <p>Compatible with Mac, Linux, and Windows.</p>
+        <p>Follow our step-by-step installation on <a href="https://github.com/open-source-labs/Chronos">Github.</a></p>
+      </div>
         {/* <p>Download Chronos as an Electron app, then follow our step-by-step instructions on <a href="https://github.com/open-source-labs/Chronos">Github.</a></p> */}
-        <p>Follow our step-by-step instructions on <a href="https://github.com/open-source-labs/Chronos">Github.</a></p>
       {/* <div className="action">
         <div className="download-btns">
           <div>
