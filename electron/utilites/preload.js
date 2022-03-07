aa41de34d8fbc054bf53b436e36f47f7148d6751
William Owen <william.owen.bsc@gmail.com>
2022-03-07 10:08:53 -0800

    executable works mostly works, with a few minor bugs.

--- electron/utilites/preload.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-const script = document.querySelector('[src=index_bundle.js]');
-
-console.log('from preload', path.resolve(), script);
---
+++ nodeUtilities/deleteDist.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+
+function deleteFolderRecursive(path) {
+  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+    fs.readdirSync(path).forEach(function (file, index) {
+      var curPath = path + '/' + file;
+
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+
+    console.log(`Deleting directory "${path}"...`);
+    fs.rmdirSync(path);
+  }
+}
+
+console.log('Cleaning working tree...');
+
+deleteFolderRecursive('./dist');
