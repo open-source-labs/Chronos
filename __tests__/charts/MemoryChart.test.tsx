857d80397d600b5291a4b4823d3beaf60f4080cc
Justinlkirk <justinleekirk@gmail.com>
2022-03-09 16:39:22 -0600

    Filling out the rest of the chart tests
    Co-authored-by: Alexander Holden <astholden@gmail.com>
    Co-authored-by: William Owen <islandhopper152@gmail.com>
    Co-authored-by: Jessica Lee <jlouielee@gmail.com>

--- __tests__/charts/MemoryChart.test.tsx
+++ __tests__/charts/MemoryChart.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import SpeedChart from '../../app/charts/SpeedChart';
+import MemoryChart from '../../app/charts/MemoryChart';
 import { HealthContext } from '../../app/context/HealthContext';
 import mockData from '../mock_data.json';
 import '@testing-library/jest-dom';
@@ -11,6 +11,7 @@ jest.mock('electron', () => ({
     on: () => mockData,
   },
 }));
+
 describe('Speed Chart', () => {
   const props = {
     healthData: mockData,
@@ -19,14 +20,12 @@ describe('Speed Chart', () => {
     parseHealthData: jest.fn(),
     services: jest.fn(),
   };
-  let graph;
   beforeEach(() => {
     render(
       <HealthContext.Provider value={props}>
-        <SpeedChart sizing="solo" colourGenerator={jest.fn()} />
+        <MemoryChart sizing="solo" colourGenerator={jest.fn()} />
       </HealthContext.Provider>
     );
-    graph = screen.getByTestId('Speed Chart').firstChild;
   });
 
   it('Should render', () => {
@@ -34,22 +33,10 @@ describe('Speed Chart', () => {
   });
 
   it('Should render graph', () => {
-    expect(graph).toBeTruthy();
-  });
-
-  it('Should be alone', () => {
-    expect(graph.previousSibling).toBe(null);
-    expect(graph.nextSibling).toBe(null);
-  });
-
-  it('Should not scroll', () => {
-    expect(graph.scrollWidth).toBe(0);
-    expect(graph.scrollHeight).toBe(0);
-    expect(graph.scrollLeft).toBe(0);
-    expect(graph.scrollTop).toBe(0);
+    expect(screen.getByTestId('Speed Chart').firstChild).toBeTruthy();
   });
 
   it('Should render graph', () => {
-    console.log(graph.outerHTML);
+    expect(screen.getByTestId('Speed Chart').firstChild).toBeTruthy();
   });
 });
--- jest_setup/windowMock.js
+++ jest_setup/windowMock.js
@@ -1,15 +1,8 @@
-/* istanbul ignore file */
-/**
- * Mock window environment
- */
+// Mock window environment
 window.require = require;
 
-/**
-* Mock import statements for Plotly
-*/
+// Mock import statements for Plotly
 window.URL.createObjectURL = () => {};
 
-/**
-* Mock getContext
-*/
+// Mock get context
 HTMLCanvasElement.prototype.getContext = () => {};
