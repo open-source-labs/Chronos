6de3c2bc1447fa9c665447ae2912364e5210cc56
Yang Song <yangsong531@gmail.com>
2022-05-28 16:18:42 -0700

    edit eventcontainer, create boilerplate code for eventchart, fix bug in graphscontainer Co-authored-by: Alex Kolb <akolb@g.clemson.edu>

---
+++ app/charts/EventChart.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface EventChartProps {
+  metric: string;
+  metricValue: number;
+  sizing: string;
+  colourGenerator: Function; 
+}
+
+const EventChart: React.FC<EventChartProps> = React.memo((props) => {
+
+
+
+
+
+})
+
+export default EventChart;
--- app/containers/EventContainer.tsx
+++ app/containers/EventContainer.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
-
+//import EventContext, EventChart
+import { EventContext } from '../context/EventContext';
+import EventChart from '../charts/EventChart';
 
 interface EventContainerProps {
   sizing: string;
@@ -7,7 +9,31 @@ interface EventContainerProps {
 }
 
 
-const GraphsContainer: React.FC<EventContainerProps> = React.memo(({ sizing, colourGenerator }) => {
+const GraphsContainer: React.FC<EventContainerProps> = React.memo((props) => {
+  //get eventData from EventContext using react hook useContext
+  const { eventData } = useContext(EventContext);
+  /* eventData is an object which looks like:
+  eventData =
+   {
+    ActiveControllerCount: 10, 
+    OfflinePartitionsCount: 5, 
+    UncleanLeaderElectionsPerSec: 2, 
+    DiskUsage: 60
+  }
+  */
+   //render a list of EventChart for each eventData metric
+  const eventChartsArr : JSX.Element[] = [];
+  for(let metric in eventData){
+    const metricValue:number = eventData[metric];
+    eventChartsArr.push(<EventChart metric = {metric} metricValue = {metricValue} sizing = {props.sizing} colourGenerator = {props.colourGenerator}/>)
+  }
+ 
+  return(
+    <div>
+    {eventChartsArr}
+    </div>
+  )
+
 
 });
 
