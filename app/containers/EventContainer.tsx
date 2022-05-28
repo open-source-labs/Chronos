016cdd614ead9dd2a5cb6935e175f81e43052c26
Yang Song <yangsong531@gmail.com>
2022-05-28 11:19:36 -0700

    add event in graphcontainer and add boilerplate for eventcontainer

---
+++ app/containers/EventContainer.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState, useContext } from 'react';
+
+
+interface EventContainerProps {
+  sizing: string;
+  colourGenerator: Function;
+}
+
+
+const GraphsContainer: React.FC<EventContainerProps> = React.memo(({ sizing, colourGenerator }) => {
+
+});
+
+
+export default GraphsContainer;
