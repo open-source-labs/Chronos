a395ca4a74ed8405462b609772125a3f29818510
Yang Song <yangsong531@gmail.com>
2022-06-14 11:04:53 -0700

    Co-authored-by: Hannah <hann7@users.noreply.github.com> Co-authored-by: alexkolb1 <alexkolb1@users.noreply.github.com> Co-authored-by: gfloresl <gfloresl@users.noreply.github.com>

--- electron/routes/data.ts
+++ electron/routes/data.ts
@@ -12,6 +12,7 @@ import DockerModelFunc from '../models/DockerModel';
 import KafkaModel from '../models/KafkaModel';
 import fetch, { FetchError } from 'electron-fetch';
 import { lightWhite } from 'material-ui/styles/colors';
+import { postgresFetch, mongoFetch } from './dataHelpers';
 
 
 require('dotenv').config();
@@ -46,7 +47,6 @@ ipcMain.on('connect', async (message: Electron.IpcMainEvent, index: number) => {
     // Connect to the proper database
     if (databaseType === 'MongoDB') await connectMongo(index, URI);
     if (databaseType === 'SQL') pool = await connectPostgres(index, URI);
-
     // console.log('electron/routes/data.ts, ipcMain.on(connect): 3 connected');
 
     // Currently set to a global variable
@@ -131,71 +131,32 @@ ipcMain.on('healthRequest', async (message: Electron.IpcMainEvent, service: stri
     // Mongo Database
     if (currentDatabaseType === 'MongoDB') {
       // // Get document count
-      // let num = await HealthModelFunc(service).countDocuments({});
-      // // Get last 50 documents. If less than 50 documents, get all
-      // num = Math.max(num, 10);
-      // result = await HealthModelFunc(service)
-      //   .find(
-      //     {},
-      //     {
-      //       cpuspeed: 1,
-      //       cputemp: 1,
-      //       cpuloadpercent: 1,
-      //       totalmemory: 1,
-      //       freememory: 1,
-      //       usedmemory: 1,
-      //       activememory: 1,
-      //       totalprocesses: 1,
-      //       runningprocesses: 1,
-      //       blockedprocesses: 1,
-      //       sleepingprocesses: 1,
-      //       latency: 1,
-      //       time: 1,
-      //       __v: 1,
-      //       service,
-      //     }
-      //   )
-      //   .skip(num - 50);
-      if(service === 'chronos-mon'){
-        result = [{
-          "chronos-mon": [
-            { "metric": "disk_usage", "category": "Memory", "time": 1655162673100, "value": 2 },
-            { "metric": "disk_usage", "category": "Memory", "time": 1655162673200, "value": 10 },
-            { "metric": "disk_usage", "category": "Memory", "time": 1655162673300, "value": 8},
-            { "metric": "disk_usage", "category": "Memory", "time": 1655162673400, "value": 9 },
-            { "metric": "clockSpeed", "category": "Memory", "time": 1655162673100, "value": 10 },
-            { "metric": "clockSpeed", "category": "Memory", "time": 1655162673200, "value": 15 },
-            { "metric": "clockSpeed", "category": "Memory", "time": 1655162673300, "value": 10 },
-            { "metric": "clockSpeed", "category": "Memory", "time": 1655162673400, "value": 15 },
-            { "metric": "cpu_temp", "category": "CPU", "time": 1655162673100, "value": 20},
-            { "metric": "cpu_temp", "category": "CPU", "time": 1655162673200, "value": 30 },
-            { "metric": "cpu_temp", "category": "CPU", "time": 1655162673300, "value": 50},
-            { "metric": "cpu_temp", "category": "CPU", "time": 1655162673400, "value": 40 },
-          ],
-        },
-      ];
-      }
-      if(service === 'chronos-mon-2'){
-        console.log("in the chronos-mon-2 electron!!")
-        result = [
+      let num = await HealthModelFunc(service).countDocuments({});
+      // Get last 50 documents. If less than 50 documents, get all
+      num = Math.max(num, 10);
+      result = await HealthModelFunc(service)
+        .find(
+          {},
           {
-            "chronos-mon-2": [
-              { "metric": "disk_usage", "category": "Memory", "time": 1655162673100, "value": 2000 },
-              { "metric": "disk_usage", "category": "Memory", "time": 1655162673200, "value": 10000 },
-              { "metric": "disk_usage", "category": "Memory", "time": 1655162673300, "value": 8000},
-              { "metric": "disk_usage", "category": "Memory", "time": 1655162673400, "value": 9000 },
-              { "metric": "clockSpeed", "category": "Memory", "time": 1655162673100, "value": 10000 },
-              { "metric": "clockSpeed", "category": "Memory", "time": 1655162673200, "value": 15000 },
-              { "metric": "clockSpeed", "category": "Memory", "time": 1655162673300, "value": 10000 },
-              { "metric": "clockSpeed", "category": "Memory", "time": 1655162673400, "value": 15000 },
-              { "metric": "cpu_temp", "category": "CPU", "time": 1655162673100, "value": 20000},
-              { "metric": "cpu_temp", "category": "CPU", "time": 1655162673200, "value": 30000 },
-              { "metric": "cpu_temp", "category": "CPU", "time": 1655162673300, "value": 50000},
-              { "metric": "cpu_temp", "category": "CPU", "time": 1655162673400, "value": 40000},
-            ],
-          },
-        ];
-      }
+            cpuspeed: 1,
+            cputemp: 1,
+            cpuloadpercent: 1,
+            totalmemory: 1,
+            freememory: 1,
+            usedmemory: 1,
+            activememory: 1,
+            totalprocesses: 1,
+            runningprocesses: 1,
+            blockedprocesses: 1,
+            sleepingprocesses: 1,
+            latency: 1,
+            time: 1,
+            __v: 1,
+            service,
+          }
+        )
+        .skip(num - 50);
+
     }
 
     /**
@@ -210,18 +171,7 @@ ipcMain.on('healthRequest', async (message: Electron.IpcMainEvent, service: stri
     // SQL Database
     if (currentDatabaseType === 'SQL') {
       // Get last 50 documents. If less than 50 get all
-      const query = `
-          SELECT * FROM ${service}
-          ORDER BY _id DESC
-          LIMIT 50
-          `;
-      // Execute query
-      result = await pool.query(query);
-      result = result.rows.reverse();
-      result = result.map(res => ({
-        ...res,
-        service,
-      }));
+    result = postgresFetch(service, pool);
     }
 
     // Async event emitter - send response'
@@ -300,22 +250,30 @@ function extractWord(str: string) {
 }
 
 ipcMain.on('kafkaRequest', async (message) => {
-  let result50: any[] =[];
-  let result: any;
-  //we do not need loop 50 (very slow) if we get the data from database and just extract 50 rows from db.
-  for (let i = 0; i < 50; i++){
-    await fetch('http://localhost:12345/metrics')
-    .then(data => data.text())
-    .then(data => {
-      result = extractWord(data);
-      result50.push(...result);
-     
-    })
-    .catch(err => console.log(err));
+  try {
+    let result: any;
+
+    // Mongo Database
+    if (currentDatabaseType === 'MongoDB') {
+
+    }
+    // SQL Database
+    if (currentDatabaseType === 'SQL') {
+      // Get last 50 documents. If less than 50 get all
+    result = postgresFetch('kafkametrics', pool);
+    }
+
+    // Async event emitter - send response'
+
+    // console.log(result[0], service);
+    // console.log(result, JSON.stringify(result));
+
+    message.sender.send('healthResponse', JSON.stringify(result));
+  } catch (error) {
+    // Catch errors
+    console.log('Error in "kakfaRequest" event', message);
+    message.sender.send('kafkaResponse', {});
   }
-  console.log("kafka response: ");
-  console.log(JSON.stringify(result50));
-  message.sender.send('kafkaResponse', JSON.stringify(result50));
 
   
 });
