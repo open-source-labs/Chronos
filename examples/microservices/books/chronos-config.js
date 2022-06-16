7615d174736745dcc6fc990700676a10dbdfb71e
James Edwards <j.olden.edwards@gmail.com>
2022-06-16 08:40:35 -0700

    Deleted .env files from the microservices example

--- examples/microservices/books/chronos-config.js
+++ examples/microservices/books/chronos-config.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
-// const chronos = require('chronos-tracker');
 const chronos = require('chronos7test');
 
 chronos.use({
   microservice: 'books',
   interval: 2000,
-  jmxuri: 'http://localhost:12345/metrics',
   // dockerized: true,
   database: {
     connection: 'REST',
