f4ef1a77b747bd0218e664a82f6f09846a420061
Troy Prejusa <troyprejusa@icloud.com>
2022-12-17 15:17:28 -0800

    Adding entry into MainContainer and incremental inclusions of more components. At this moment, Occupied.tsx throws an error in the return JSX

--- app/containers/MainContainer.tsx
+++ app/containers/MainContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { redirect, Route, Routes } from 'react-router-dom';
-import LandingPageContainer from './LandingPageContainer';
+// import LandingPageContainer from './LandingPageContainer';
 import WindowBar from '../components/WindowBar';
 import About from '../components/About';
 import Contact from '../components/Contact';
@@ -16,37 +16,61 @@ const MainContainer = React.memo(() => {
   const { mode, landingPage, getLandingPage, authStatus } = useContext(DashboardContext);
   const currentModeCSS =
     mode === 'light mode' ? lightAndDark.lightModeMain : lightAndDark.darkModeMain;
-  const checkAuth = (Component: any) => {
-    if (landingPage === 'dashBoard' || authStatus) return <Component />;
-    return redirect('/');
-  };
+  // const checkAuth = (Component: any) => {
+  //   if (landingPage === 'dashBoard' || authStatus) return <Component />;
+  //   return redirect('/');
+  // };
+
+  /* <h1 style={{color: 'red', fontSize: '400px'}}>Hello world</h1> */
 
   useEffect(() => {
     getLandingPage();
   }, []);
-
   return (
     <>
       {/* <WindowBar /> */}
       <div className="main-container" style={currentModeCSS}>
         <div className="main-routes">
           <Routes>
-            <Route path="/" element={LandingPageContainer} />
-            <Route path="/awaitingApproval" element={AwaitingApproval} />
+            <Route path="/" element={Occupied} />
+            {/* <Route path="/awaitingApproval" element={AwaitingApproval} />
             <Route path="/about" element={() => checkAuth(About)} />
             <Route path="/contact" element={() => checkAuth(Contact)} />
             <Route path="/settings" element={() => checkAuth(Settings)} />
-            <Route path="/applications" element={() => checkAuth(Occupied)} />
-            <Route
+            <Route path="/applications" element={() => checkAuth(Occupied)} /> */}
+            {/* <Route
               path="/applications/:app/:service"
               element={() => checkAuth(GraphsContainer)}
             />
-            <Route path="*" element={() => <h1>Not found</h1>} />
+            <Route path="*" element={() => <h1>Not found</h1>} /> */}
           </Routes>
         </div>
       </div>
     </>
   );
+
+  // return (
+  //   <>
+  //     {/* <WindowBar /> */}
+  //     <div className="main-container" style={currentModeCSS}>
+  //       <div className="main-routes">
+  //         <Routes>
+  //           <Route path="/" element={LandingPageContainer} />
+  //           <Route path="/awaitingApproval" element={AwaitingApproval} />
+  //           <Route path="/about" element={() => checkAuth(About)} />
+  //           <Route path="/contact" element={() => checkAuth(Contact)} />
+  //           <Route path="/settings" element={() => checkAuth(Settings)} />
+  //           <Route path="/applications" element={() => checkAuth(Occupied)} />
+  //           <Route
+  //             path="/applications/:app/:service"
+  //             element={() => checkAuth(GraphsContainer)}
+  //           />
+  //           <Route path="*" element={() => <h1>Not found</h1>} />
+  //         </Routes>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 });
 
 export default MainContainer;
--- app/index.tsx
+++ app/index.tsx
@@ -5,11 +5,11 @@ import ReactDOM from 'react-dom';
 // import { createRoot } from 'react-dom/client';
 
 import './stylesheets/index.scss';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/';
+import { createTheme, ThemeProvider } from '@material-ui/core/';
 import App from './App';
 import WindowBar from './components/WindowBar';
 
-const theme = createMuiTheme({
+const theme = createTheme({
   typography: {
     fontFamily: ['Roboto', 'sans-serif'].join(','),
   },
@@ -17,18 +17,18 @@ const theme = createMuiTheme({
 
 
 // React 17 Syntax below
-// ReactDOM.render(
-//   <ThemeProvider theme={theme}>
-//     <WindowBar />
-//     <App />
-//   </ThemeProvider>,
-//   document.getElementById('app')
-// );
-
 ReactDOM.render(
-    <h1 style={{color: 'red'}}>Hi Gang</h1>,
-    document.getElementById('app')
-)
+  <ThemeProvider theme={theme}>
+    <WindowBar />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('app')
+);
+
+// ReactDOM.render(
+//     <h1 style={{color: 'red'}}>Hi Gang</h1>,
+//     document.getElementById('app')
+// )
 
 
 // React 18 Syntax below
