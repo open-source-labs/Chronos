a9b20eca06e18f8c650ce9dce735f467ae974921
GP3-RS <gahlpeled@gmail.com>
2022-12-27 13:37:10 -0800

    Application Context now provides setSavedMetrics to children. Button to update savedMetrics now updates state correctly. It does not yet Patch db to update which metrics are saved.

--- app/containers/ModifyMetricsContainer.tsx
+++ app/containers/ModifyMetricsContainer.tsx
@@ -7,7 +7,7 @@ import '../stylesheets/ModifyMetrics.scss'
 
 const MetricsContainer:React.FC = React.memo(props => {
   
-  const { savedMetrics } = useContext(ApplicationContext)
+  const { savedMetrics, setSavedMetrics } = useContext(ApplicationContext);
   const kubernetesMetrics: any[] = [];
   const kafkaMetrics: any[] = [];
   const healthMetrics: any[] = [];
@@ -15,25 +15,41 @@ const MetricsContainer:React.FC = React.memo(props => {
 
   const currentMode = mode === 'light mode' ? lightAndDark.lightModeText : lightAndDark.darkModeText;
 
-  const metricsToChange = {}
+  const metricsToChange = {};
 
-  savedMetrics.forEach(el => {
+  const changeMetric = (event) => {
+    const m = {...savedMetrics[event.target.id]};
+    m['selected'] = event.target.checked;
+    metricsToChange[event.target.id] = m;
+  }
+
+  const updateMetrics = () => {
+    const newMetrics = {...savedMetrics};
+    for (let key in metricsToChange) {
+      newMetrics[key] = metricsToChange[key];
+    }
+    setSavedMetrics(newMetrics);
+    // SEND PATCH REQUEST TO APPLICATION CONTEXT > DATA.TS AND HANDLE FROM THERE
+  }
+
+  Object.values(savedMetrics).forEach((el: any) => {
     let jsxEl = (
       <div key={el.metric} className="modifyMetric">
         <label style={currentMode}>
-          <input type="checkbox" key={el._id} id={el._id} defaultChecked={el.selected}></input>
+          <input type="checkbox" key={el.metric} id={el.metric} defaultChecked={el.selected} onClick={changeMetric}></input>
           {el.metric}
         </label>
       </div>)
     if (el.mode === 'kubernetes') kubernetesMetrics.push(jsxEl);
     else if (el.mode === 'kafka') kafkaMetrics.push(jsxEl);
     else healthMetrics.push(jsxEl);
-  })
+  }) 
 
   return (
     <div className="metricsSelector">
       <h2 style={currentMode}>Modify which metrics your Chronos app will track by selecting or deselecting from the list below.</h2>
       <p style={currentMode}>This can be helpful if you find that you and your team often only care to track a small handful of metrics, and don't want your database to be overwhelmed with thousands of datapoints that you don't necessarily need.</p>
+      <button className="select" onClick={updateMetrics}>Change Database Settings</button>
       {!!kubernetesMetrics.length && <div className='metricsSublist'><h3 style={currentMode}>Kubernetes Metrics:</h3>{kubernetesMetrics}</div>}
       {!!kafkaMetrics.length && <div className='metricsSublist'><h3 style={currentMode}>Kafka Metrics:</h3>{kafkaMetrics}</div>}
       {!!healthMetrics.length && <div className='metricsSublist'><h3 style={currentMode}>Health Metrics:</h3>{healthMetrics}</div>}
