d235220020fae9c58fc3e0eb818c232aef04f6c1
Troy Prejusa <troyprejusa@icloud.com>
2022-12-27 17:52:12 -0800

    More incremental progress toward bundling the application

--- utilities/deleteDist.js
+++ utilities/deleteDist.js
@@ -1,5 +1,6 @@
+const path = require('path');
 const fse = require('fs-extra');
 
 // Delete the existing dist folder
 console.log('Deleting contents of /dist...');
-fse.emptyDirSync(path.resolve(__dirname, 'dist'));
+fse.emptyDirSync(path.resolve(__dirname, '../dist'));
