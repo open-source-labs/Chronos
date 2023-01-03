93f14012d27b7b866375d8b26a3a57698fd293d8
Troy Prejusa <troyprejusa@icloud.com>
2023-01-03 12:27:37 -0800

    Copy in fix from Gahl Peled fixing the display of the communications chart

--- app/containers/GraphsContainer.tsx
+++ app/containers/GraphsContainer.tsx
@@ -179,7 +179,6 @@ const GraphsContainer: React.FC = React.memo(props => {
           onClick={() => {
             if (!location.href.includes('communications')) setPrevRoute(services.join(' '));
             setChart('communications');
-            navigate('communications');
           }}
           key="3"
         >
@@ -188,7 +187,7 @@ const GraphsContainer: React.FC = React.memo(props => {
       </nav>
       <Header app={app} service={service} live={live} setLive={setLive} />
       <div className="graphs-container">
-        {service === 'communications' ? (
+        {chart === 'communications' ? (
           <div className="graphs">
             <RequestTypesChart />
             <ResponseCodesChart />
