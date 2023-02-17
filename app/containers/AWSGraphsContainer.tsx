2fa7d1d4599931672f9346b4cc08966a96a6a006
taylrzhang <neil0725@hotmail.com>
2023-02-17 15:59:17 -0800

    aws container css added

--- app/containers/AWSGraphsContainer.tsx
+++ app/containers/AWSGraphsContainer.tsx
@@ -2,36 +2,51 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { ApplicationContext } from '../context/ApplicationContext';
 import * as DashboardContext from '../context/DashboardContext';
-import lightAndDark from '../components/Styling';
+import { Typography } from '@material-ui/core';
 import { useQuery } from 'react-query';
 import { AwsContext } from '../context/AwsContext';
 
 import '../stylesheets/AWSGraphsContainer.scss';
 
-const AWSGraphsContainer: React.FC = React.memo(props => {
+const AwsGraphsContainer: React.FC = React.memo(props => {
   const { awsData, fetchAwsData, setAwsData } = useContext(AwsContext);
-  const [live, setLive] = useState(true);
+  const { app } = useContext(ApplicationContext);
+
+  const [awsLive, setAwsLive] = useState<boolean>(false);
 
   useEffect(() => {
-    if(live) {
-      fetchAwsData();
-
-      setInterval(() => {
-        fetchAwsData();
-      }, 100000);
-    }
-  }, []);
+    fetchAwsData();
+  });
 
   return (
     <div className="AWS-container">
-      <p>
+      {/* <p>
         AWS TEST
         DATAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
       </p>
-      <p>Here is our data: {awsData}</p>
+      <p>Here is our data: {awsData}</p> */}
+      <div className="AWSheader">
+        <Typography variant="h3">{app}</Typography>
+        <p>Here you will see the health data of you AWS cluster</p>
+        <button onClick={() => setAwsLive(!awsLive)}>
+          {awsLive ? (
+            <div>
+              <span id="live">Live</span>
+            </div>
+          ) : (
+            <div id="gatherLiveData">Gather Live Data</div>
+          )}
+        </button>
+      </div>
+      <div className="charts">
+        <div className="CPU chart">chart</div>
+        <div className="memory chart">chart</div>
+        <div className="networkIn chart">chart</div>
+        <div className="networkOut chart">chart</div>
+      </div>
     </div>
   );
   // }
 });
 
-export default AWSGraphsContainer;
+export default AwsGraphsContainer;
