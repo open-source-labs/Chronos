acba05e720c6af66175b0868259bb6d1258aae68
Snow Xue Bai <xueapp@gmail.com>
2023-02-23 12:45:01 -0800

    Created README file for test folder with clear instruction on required packages and test enviroment setup and testing instruction

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   verbose: true,
   setupFilesAfterEnv: ['./jest_setup/windowMock.js'],
+  testEnvironment: "jsdom",
   preset: 'ts-jest/presets/js-with-ts',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
@@ -8,4 +9,5 @@ module.exports = {
     '\\.(css|less|scss)$': '<rootDir>/jest_setup/styleMock.js',
   },
   collectCoverage: true,
+  types: ["jest","node"],
 };
