640cb8d8a55dc32aafac7b63c420993ba26f2892
John Donato <johnlloyddonato@yahoo.com>
2023-04-27 12:15:05 -0500

    Deleted the script file - no longer needed

--- examples/docker/scripts/cleanupDockerExample.sh
+++
@@ -1,9 +0,0 @@
-cd ../orders
-rm -rf chronos_npm_package
-cd ../frontend
-rm -rf chronos_npm_package
-cd ../customers
-rm -rf chronos_npm_package
-cd ../books
-rm -rf chronos_npm_package
-cd ../scripts
