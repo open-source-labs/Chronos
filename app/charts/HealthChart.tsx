c8d29971e0056ef287dede539576a8759568c297
Justin Poirier <poirierj94@gmail.com>
2023-06-03 11:35:36 -0500

    Multiple plots on chart in progress. Finishing plotly data generator function.

--- app/charts/HealthChart.tsx
+++ app/charts/HealthChart.tsx
@@ -2,13 +2,16 @@ import moment from 'moment';
 import React, { useState } from 'react';
 import Plot from 'react-plotly.js';
 import { all, solo as soloStyle } from './sizeSwitch';
+import { getTime } from '../context/helpers';
 
 interface HealthChartProps {
   key: string;
-  renderService: string;
-  metric: string;
-  timeList: any;
-  valueList: any;
+  serviceName: string;
+  // metric: string;
+  categoryName: string;
+  metrics: any[];
+  timeList: any[];
+  // valueList: any;
   sizing: string;
   colourGenerator: Function;
 }
@@ -19,8 +22,23 @@ interface SoloStyles {
 }
 
 const HealthChart: React.FC<HealthChartProps> = React.memo(props => {
-  const { renderService, metric, timeList, valueList, sizing, colourGenerator } = props;
+  // const { renderService, metric, timeList, valueList, sizing, colourGenerator } = props;
+  const { serviceName, categoryName, metrics, timeList, sizing, colourGenerator } = props;
   const [solo, setSolo] = useState<SoloStyles | null>(null);
+  // metrics = specific metrics in categories
+  // temporary y-axis values for testing purposes
+  const valueList = metrics[0].books[0].activememory_in_bytes
+  // filter through metrics to desired metrics and then plot them
+
+  function generatePlotlyDataObjects(metricsArray) {
+    // iterate through the metricsArray
+      // each element is an array of num data (y-axis)
+      // generate a plotly data object to add to an array that will be passed into our plotly chart's data prop
+    
+  }
+
+
+
   setInterval(() => {
     if (solo !== soloStyle) {
       setSolo(soloStyle);
@@ -28,74 +46,74 @@ const HealthChart: React.FC<HealthChartProps> = React.memo(props => {
   }, 20);
 
   const createChart = () => {
-      // 
-      const timeArr = timeList.map((el: any) => moment(el).format('kk:mm:ss'));
-      const reverseTimeArr = timeArr.reverse()
-      const hashedColour = colourGenerator(renderService);
-      const re = /_/g;
-      let plotlyData: {
-        name: any;
-        x: any;
-        y: any;
-        type: any;
-        mode: any;
-        marker: { color: string };
-      };
-      plotlyData = {
-        name: metric.replace(re, " "),
-        x: reverseTimeArr, //reversed for better UX
-        y: valueList,
-        type: 'scattergl',
-        mode: 'lines',
-        marker: { color: hashedColour },
-      };
-      const sizeSwitch = sizing === 'all' ? all : solo;
+    //
+    const timeArr = timeList.map((el: any) => moment(el).format('kk:mm:ss'));
+    const reverseTimeArr = timeArr.reverse();
+    const hashedColour = colourGenerator(serviceName);
+    const re = /_/g;
+    type plotlyData = {
+      name: any;
+      x: any;
+      y: any;
+      type: any;
+      mode: any;
+      marker: { color: string };
+    };
+    // const plotlyData = {
+    //   name: metric.replace(re, ' '),
+    //   x: reverseTimeArr, //reversed for better UX
+    //   y: valueList,
+    //   type: 'scattergl',
+    //   mode: 'lines',
+    //   marker: { color: hashedColour },
+    // };
+    const sizeSwitch = sizing === 'all' ? all : solo;
 
-      return (
-        <Plot
-          data={[plotlyData]}
-          config={{ displayModeBar: false }}
-          layout={{
-            title: `${renderService} | ${metric}`,
-            ...sizeSwitch,
-            font: {
-              color: '#444d56',
-              size: 11.5,
-              family: 'Roboto',
-            },
-            paper_bgcolor: 'white',
-            plot_bgcolor: 'white',
-            showlegend: true,
-            legend: {
-              orientation: 'h',
-              xanchor: 'center',
-              x: 0.5,
-              y: 5,
-            },
-            xaxis: {
-              title: 'Time',
-              tickmode: 'auto',
-              tick0: 0,
-              dtick: 10,
-              rangemode: 'nonnegative',
-              mirror: false,
-              ticks: 'outside',
-              showline: true,
-            },
-            yaxis: {
-              rangemode: 'nonnegative',
-              title: metric,
-            },
-          }}
-        />
-      ); 
+    return (
+      <Plot
+        // data takes an array of objects that each have x, y, type, mode, marker
+        data={[plotlyData]}
+        config={{ displayModeBar: false }}
+        layout={{
+          title: `${serviceName} | ${categoryName}`,
+          ...sizeSwitch,
+          font: {
+            color: '#444d56',
+            size: 11.5,
+            family: 'Roboto',
+          },
+          paper_bgcolor: 'white',
+          plot_bgcolor: 'white',
+          showlegend: true,
+          legend: {
+            orientation: 'h',
+            xanchor: 'center',
+            x: 0.5,
+            y: 5,
+          },
+          xaxis: {
+            title: 'Time',
+            tickmode: 'auto',
+            tick0: 0,
+            dtick: 10,
+            rangemode: 'nonnegative',
+            mirror: false,
+            ticks: 'outside',
+            showline: true,
+          },
+          yaxis: {
+            rangemode: 'nonnegative',
+            //! change this later :^)
+            title: 'Value',
+          },
+        }}
+      />
+    );
   };
 
   return (
     <div className="chart" data-testid="Health Chart">
-      {
-      createChart()
-      }
+      {createChart()}
     </div>
   );
 });
