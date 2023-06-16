cf9afe3d07638134fc3eec97c7d39dce4ac0ee2a
Justin Poirier <110320397+jcpoirier20@users.noreply.github.com>
2023-06-16 18:52:31 -0500

    Update HealthChart.test.tsx

--- __tests__/charts/HealthChart.test.tsx
+++ __tests__/charts/HealthChart.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import HealthChart from '../../app/charts/HealthChart';
 import { render, screen } from '@testing-library/react';
 
-// import mockData from '../mock_data.json';
-
+// mockData used for testing suite
 const mockData = {
   ServiceName: {
     Metric: {
@@ -26,11 +25,7 @@ jest.mock('electron', () => ({
   },
 }));
 
-// jest.mock('react-plotly.js', () => (props) {
-//   const div = document.createElement('div');
-
-// })
-
+// test suite for HealthChart.tsx
 describe('HealthChart', () => {
   const props = {
     key: 'testKey',
@@ -63,39 +58,8 @@ describe('HealthChart', () => {
     expect(graph.scrollTop).toBe(0);
   });
 
-  // it('Should have width 800, height 600, and white background', () => {
-  //   // console.log('graph', graph.firstChild);
-  //   console.log('graph.outerHTML: ', graph.outerHTML);
-
-  //   expect(graph.outerHTML.includes('width: 800px')).toBeTruthy();
-  //   expect(graph.outerHTML.includes('height: 600px')).toBeTruthy();
-  //   expect(graph.outerHTML.includes('style="background: white;"')).toBeTruthy();
-  // });
-
-  // it('Should have correct colors', () => {
-  //   expect(graph.outerHTML.includes('{fill: #fc4039;}')).toBeTruthy();
-  // });
-
   it('Should have correct data on y-axis based off mock data', () => {
     expect(graph.data[0].y[0]).toBe((mockData.ServiceName.Metric.value[0] / 1000000).toFixed(2));
     expect(graph.data[0].y[1]).toBe((mockData.ServiceName.Metric.value[1] / 1000000).toFixed(2));
   });
-
-  // it('Should have correct time on x-axis based off mock data', () => {
-  //   const expectedOutput = ['15:18:25', '15:18:20', '15:18:15', '15:18:10', '15:18:05'];
-
-  //   expect(graph.data[0].x[0]).toEqual(expectedOutput[0]);
-  //   expect(graph.data[0].x[1]).toEqual(expectedOutput[1]);
-  // });
-
-  // it('Should have the correct service name on the graph', () => {
-  //   expect(graph).outerHTML.includes('title').toBe('ServiceName');
-  // });
-
-  // describe('prettyTimeInReverse', () => {
-  //   test('Should reverse time and format properly', () => {
-  //     const result = HealthChart.prettyTimeInReverse(mockData.ServiceName.Metric.time);
-  //     expect(result).toEqual(expectedOutput);
-  //   });
-  // });
 });
