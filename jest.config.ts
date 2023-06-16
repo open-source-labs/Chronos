fd255069c644fafe2de8122e582e81676c566679
Justin Poirier <110320397+jcpoirier20@users.noreply.github.com>
2023-06-16 18:56:06 -0500

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,6 @@
 import type { Config } from '@jest/types';
 
-// module.exports = {
-//   verbose: true,
-//   setupFilesAfterEnv: ['./jest_setup/windowMock.js'],
-//   testEnvironment: "jsdom",
-//   preset: 'ts-jest/presets/js-with-ts',
-//   moduleNameMapper: {
-//     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-//       '<rootDir>/jest_setup/fileMock.js',
-//     '\\.(css|less|scss)$': '<rootDir>/jest_setup/styleMock.js',
-//   },
-//   collectCoverage: true,
-//   // types: ["jest","node"],
-// };
-
-// Convert to TypeScript
-// Note: Configuring Jest to TypeScript following the docs seemed incorrect? 
-// @jest/types is an npm package to use to create a typed Jest config 
-
+// describes the jest testing configuration for all test files
 const config: Config.InitialOptions = {
   verbose: true,
   setupFilesAfterEnv: ['./jest_setup/windowMock.js'],
