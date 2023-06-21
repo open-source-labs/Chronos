95af2e884ac65a42f7f2891c9f4261765b06d6fc
Justin Poirier <poirierj94@gmail.com>
2023-06-21 16:56:19 -0500

    Updated comments in the microservices example models. Edited the startMicroservices script file. Updated the Microservices README.

--- examples/microservices/scripts/startMicroservices.sh
+++ examples/microservices/scripts/startMicroservices.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 cd ../books
 dir=$(pwd)
-com="cd $dir && npm run start"
+com="cd $dir && npm install && npm run start"
 echo $dir
 osascript -e "tell app \"Terminal\" to do script \"$com\""
+
 cd ../customers
 dir=`pwd`
-com="cd $dir && npm run start"
+com="cd $dir && npm install && npm run start"
 echo $dir
 osascript -e "tell app \"Terminal\" to do script \"$com\""
 
 cd ../orders
 dir=$(pwd)
-com="cd $dir && npm run start"
+com="cd $dir && npm install && npm run start"
 echo $dir
 osascript -e "tell app \"Terminal\" to do script \"$com\""
 
 cd ../reverse_proxy
 dir=$(pwd)
-com="cd $dir && npm run start"
+com="cd $dir && npm install && npm run start"
 echo $dir
 osascript -e "tell app \"Terminal\" to do script \"$com\""
