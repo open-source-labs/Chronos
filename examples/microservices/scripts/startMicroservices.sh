038fbda701d2edfd935cfb313e544e3282c3cef5
Justin Poirier <poirierj94@gmail.com>
2023-06-21 17:27:40 -0500

    More script edits.

--- examples/microservices/scripts/startMicroservices.sh
+++ examples/microservices/scripts/startMicroservices.sh
@@ -2,23 +2,23 @@
 cd ../books
 dir=$(pwd)
 com="cd $dir && npm install && npm run start"
-echo Starting 'books' microservice. A Mac Terminal App should be open and contains more information.
+echo Starting books microservice. A Mac Terminal App should be open and contains more information.
 osascript -e "tell app \"Terminal\" to do script \"$com\""
 
 cd ../customers
 dir=`pwd`
 com="cd $dir && npm install && npm run start"
-echo Starting 'customers' microservice. A Mac Terminal App should be open and contains more information.
+echo Starting customers microservice. A Mac Terminal App should be open and contains more information.
 osascript -e "tell app \"Terminal\" to do script \"$com\""
 
 cd ../orders
 dir=$(pwd)
 com="cd $dir && npm install && npm run start"
-echo Starting 'orders' microservice. A Mac Terminal App should be open and contains more information.
+echo Starting orders microservice. A Mac Terminal App should be open and contains more information.
 osascript -e "tell app \"Terminal\" to do script \"$com\""
 
 cd ../reverse_proxy
 dir=$(pwd)
 com="cd $dir && npm install && npm run start"
-echo Starting 'reverse_proxy' microservice. A Mac Terminal App should be open and contains more information.
+echo Starting reverse_proxy microservice. A Mac Terminal App should be open and contains more information.
 osascript -e "tell app \"Terminal\" to do script \"$com\""
