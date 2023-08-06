3e55a1a9981b618fb9761bd5e835305f34ea30a1
Haoyu <liu_haoyu@berkeley.edu>
2023-08-06 00:11:48 -0700

    add data source

--- chronos_npm_package/controllers/createGrafanaPanelObject.js
+++ chronos_npm_package/controllers/createGrafanaPanelObject.js
@@ -1,125 +1,122 @@
 export default function createGrafanaPanelObject(
     metric,
-    promDatasource
+    datasource
 ) {
     // Create overrides property for fieldConfigsObject
-    const panelOverrides = [
-        {
-            matcher: {
-                id: 'byName',
-                options: 'Time',
-            },
-            properties: [
-                {
-                    id: 'custom.fillBelowTo',
-                    value: 'Value',
-                },
-            ],
-        },
-    ];
+    // const panelOverrides = [
+    //     {
+    //         matcher: {
+    //             id: 'byName',
+    //             options: 'Time',
+    //         },
+    //         properties: [
+    //             {
+    //                 id: 'custom.fillBelowTo',
+    //                 value: 'Value',
+    //             },
+    //         ],
+    //     },
+    // ];
 
     // Create fieldConfig key for Grafana panel.
-    const fieldConfigObject = {
-        defaults: {
-            color: {
-                mode: 'palette-classic',
-            },
-            custom: {
-                axisCenteredZero: false,
-                axisColorMode: 'text',
-                axisLabel: '',
-                axisPlacement: 'auto',
-                barAlignment: 0,
-                drawStyle: 'line',
-                fillOpacity: 10, // Fill area under line
-                gradientMode: 'none',
-                hideFrom: {
-                    legend: false,
-                    tooltip: false,
-                    viz: false,
-                },
-                lineInterpolation: 'linear',
-                lineWidth: 1,
-                pointSize: 5,
-                scaleDistribution: {
-                    type: 'linear',
-                },
-                showPoints: 'never',
-                spanNulls: false,
-                stacking: {
-                    group: 'A',
-                    mode: 'none',
-                },
-                thresholdsStyle: {
-                    mode: 'off',
-                },
-            },
-            mappings: [],
-            thresholds: {
-                mode: 'absolute',
-                steps: [
-                    {
-                        color: 'green',
-                        value: null,
-                    },
-                    {
-                        color: 'red',
-                        value: 80,
-                    },
-                ],
-            },
-            min: 0,
-        },
-        overrides: panelOverrides,
-    };
+    // const fieldConfigObject = {
+    //     defaults: {
+    //         color: {
+    //             mode: 'palette-classic',
+    //         },
+    //         custom: {
+    //             axisCenteredZero: false,
+    //             axisColorMode: 'text',
+    //             axisLabel: '',
+    //             axisPlacement: 'auto',
+    //             barAlignment: 0,
+    //             drawStyle: 'line',
+    //             fillOpacity: 0, // Fill area under line
+    //             gradientMode: 'none',
+    //             hideFrom: {
+    //                 legend: false,
+    //                 tooltip: false,
+    //                 viz: false,
+    //             },
+    //             lineInterpolation: 'linear',
+    //             lineWidth: 1,
+    //             pointSize: 5,
+    //             scaleDistribution: {
+    //                 type: 'linear',
+    //             },
+    //             showPoints: 'never',
+    //             spanNulls: false,
+    //             stacking: {
+    //                 group: 'A',
+    //                 mode: 'none',
+    //             },
+    //             thresholdsStyle: {
+    //                 mode: 'off',
+    //             },
+    //         },
+    //         mappings: [],
+    //         thresholds: {
+    //             mode: 'absolute',
+    //             steps: [
+    //                 {
+    //                     color: 'green',
+    //                     value: null,
+    //                 },
+    //                 {
+    //                     color: 'red',
+    //                     value: 80,
+    //                 },
+    //             ],
+    //         },
+    //         min: 0,
+    //     },
+    //     overrides: panelOverrides,
+    // };
 
     // Create options key for Grafana panel
-    const optionsObject = {
-        legend: {
-            calcs: [],
-            displayMode: 'list',
-            placement: 'bottom',
-            showLegend: true,
-        },
-        tooltip: {
-            mode: 'single',
-            sort: 'none',
-        },
-    };
+    // const optionsObject = {
+    //     legend: {
+    //         calcs: [],
+    //         displayMode: 'list',
+    //         placement: 'bottom',
+    //         showLegend: true,
+    //     },
+    //     tooltip: {
+    //         mode: 'single',
+    //         sort: 'none',
+    //     },
+    // };
 
     // Create grafana panel using previously built objects.
-    const grafanaPanel = {
-        datasource: promDatasource,
-        fieldConfig: fieldConfigObject,
-        gridPos: {
-            h: 8,
-            w: 12,
-            x: 0,
-            y: 0,
-        },
-        options: optionsObject,
-        id: 1,
-        targets: [],
-        title: '', // No title on panel to conserve vertical space
-        type: 'timeseries',
-        interval: '2s',
-    };
+    // const grafanaPanel = {
+    //     datasource: promDatasource,
+    //     fieldConfig: fieldConfigObject,
+    //     gridPos: {
+    //         h: 8,
+    //         w: 12,
+    //         x: 0,
+    //         y: 0,
+    //     },
+    //     options: optionsObject,
+    //     id: 1,
+    //     targets: [],
+    //     title: '', // No title on panel to conserve vertical space
+    //     type: 'timeseries',
+    //     interval: '2s',
+    // };
 
     const panel = {
         "dashboard": {
             "id": null,
-            "uid": "tes3",
-            "title": "tes3",
+            "uid": metric.id,
+            "title": metric.name,
             "tags": ["templated"],
             "timezone": "browser",
             "schemaVersion": 16,
             "version": 0,
             "refresh": "10s",
             "panels": [{
-                "datasource": {
-                    "type": "prometheus",
-                    "uid": "P1809F7CD0C75ACF3"
-                },
+                "datasource": datasource,
                 "fieldConfig": {
                     "defaults": {
                         "color": {
@@ -193,10 +190,7 @@ export default function createGrafanaPanelObject(
                 },
                 "id": 1,
                 "targets": [{
-                    "datasource": {
-                        "type": "prometheus",
-                        "uid": "P1809F7CD0C75ACF3"
-                    },
+                    "datasource": datasource,
                     "editorMode": "builder",
                     "expr": "access_evaluation_duration_bucket",
                     "instant": false,
@@ -306,10 +300,10 @@ export default function createGrafanaPanelObject(
     //}
 
     // Push updated objects into correct arrays in panel object.
-    grafanaPanel.targets.push(targetsObject);
+    //grafanaPanel.targets.push(targetsObject);
     //panelOverrides.push(metricsOverrides);
     //});
 
     // Return the compiled grafana panel object
-    return grafanaPanel;
+    return panel;
 }
