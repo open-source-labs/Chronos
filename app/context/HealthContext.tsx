b941ab49d987ce699bda6f588e3525a60aa2cf00
edwinlny <ed.leong89@gmail.com>
2023-08-08 18:30:51 -0400

    fixed HealthContext syntax

--- app/context/HealthContext.tsx
+++ app/context/HealthContext.tsx
@@ -58,40 +58,50 @@ const HealthContextProvider: React.FC<Props> = React.memo(({ children }) => {
 
     let temp: HealthDataObject[] = [];
     //Promise.all(
-      let promises = await Promise.all(serv.map( async (service: string) => {
-
-          try {
-            const newPromise: any = await new Promise((resolve, reject) => {
-              ipcRenderer.send('healthRequest', `${service}-containerinfos`);
-              ipcRenderer.on('healthResponse', (event: Electron.Event, data: string) => {
-                let result: object[];
-                if (JSON.stringify(data) !== '{}' && tryParseJSON(data)) {
-                  result = JSON.parse(data);
-                  console.log('HealthContext.tsx line 68 result: ', result, 'service', service, 'Obj key', Object.keys(result[0])[0]);
-                  //result exists, has a length prop, and the service name and database name are same
-                  if (result && result.length && `${service}-containerinfos` === Object.keys(result[0])[0]) {
-                    resolve(result[0]);
-                  }
+    let promises = await Promise.all(
+      serv.map(async (service: string) => {
+        try {
+          const newPromise: any = await new Promise((resolve, reject) => {
+            ipcRenderer.send('healthRequest', `${service}-containerinfos`);
+            ipcRenderer.on('healthResponse', (event: Electron.Event, data: string) => {
+              let result: object[];
+              if (JSON.stringify(data) !== '{}' && tryParseJSON(data)) {
+                result = JSON.parse(data);
+                console.log(
+                  'HealthContext.tsx line 68 result: ',
+                  result,
+                  'service',
+                  service,
+                  'Obj key',
+                  Object.keys(result[0])[0]
+                );
+                //result exists, has a length prop, and the service name and database name are same
+                if (
+                  result &&
+                  result.length &&
+                  `${service}-containerinfos` === Object.keys(result[0])[0]
+                ) {
+                  resolve(result[0]);
                 }
-              });
-            })
-            temp.push(newPromise);
-            console.log('HealthContext.tsx line 80 temp populates?: ', temp, serv)
-            if (checkServicesComplete(temp, [`${service}-containerinfos`])) {
-              setServices([`${service}-containerinfos`]);
-              let transformedData: any = {};
-              console.log('original healthData before transformation: ', temp);
-              // transformedData = {
-              //   healthDataList: [1,2,3,4,5],
-              //   healthTimeList: [1,2,3,4,5]
-              // } //testing typescript, transformedDATA of type 2 arrays with basic entries?
-              transformedData = healthTransformer(temp); //must match the setHealthData STATE format
-              console.log('healthData after tranformation: ', transformedData);
-              setHealthData(transformedData);
-            }
-            } catch (err) {
-            console.log("healthcontext.tsx ERROR: ", err);
-          };
+              }
+            });
+          });
+          temp.push(newPromise);
+          console.log('HealthContext.tsx line 80 temp populates?: ', temp, serv);
+          if (checkServicesComplete(temp, [`${service}-containerinfos`])) {
+            setServices([`${service}-containerinfos`]);
+            let transformedData: any = {};
+            console.log('original healthData before transformation: ', temp);
+            // transformedData = {
+            //   healthDataList: [1,2,3,4,5],
+            //   healthTimeList: [1,2,3,4,5]
+            // } //testing typescript, transformedDATA of type 2 arrays with basic entries?
+            transformedData = healthTransformer(temp); //must match the setHealthData STATE format
+            console.log('healthData after tranformation: ', transformedData);
+            setHealthData(transformedData);
+          }
+        } catch (err) {
+          console.log('healthcontext.tsx ERROR: ', err);
         }
       })
     );
@@ -141,7 +151,7 @@ const HealthContextProvider: React.FC<Props> = React.memo(({ children }) => {
     for (let i = 0; i < temp.length; i++) {
       arr1.push(Object.keys(temp[i])[0]);
     }
-    console.log('in checkServicesComplete line 139: ', arr1)
+    console.log('in checkServicesComplete line 139: ', arr1);
     return arr1.sort().toString() === serv.sort().toString();
   };
 
