48ee148b9fd647554b536dad3fb200ab0ad00401
Haoyu <liu_haoyu@berkeley.edu>
2023-08-07 23:12:45 -0700

    allow allow_embedding to be true

--- examples/kubernetes/scripts/startKuber.sh
+++ examples/kubernetes/scripts/startKuber.sh
@@ -3,6 +3,7 @@ kubectl apply -f ../launch/promConfig.yml
 kubectl apply -f ../launch/prometheus.yml
 kubectl apply -f ../launch/grafana-datasource-config.yml
 kubectl apply -f ../launch/grafana.yml
+kubectl apply -f ../launch/grafanaService.yml
 sleep 2
 kubectl apply -f ../launch/backend.yml
 kubectl apply -f ../launch/frontend.yml
