2dfe730a35c048c94ebd041080a8910e0355cda4
Haoyu <liu_haoyu@berkeley.edu>
2023-08-10 22:31:08 -0700

    make api/updateDashboard

--- chronos_npm_package/server/server.js
+++ chronos_npm_package/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = 1111;
-const utilities = require('./utilities');
+const utilities = require('../controllers/utilities');
 
 
 app.use(cors());
@@ -23,26 +23,28 @@ app.get('api/updateDashboard', async (req, res) => {
     const { graphType } = req.body;
     console.log('updateDashboard endpoint hit');
     const datasource = await utilities.getGrafanaDatasource();
-    utilities.updateGrafanaDashboard(graphType, datasource);
+    await utilities.updateGrafanaDashboard(graphType, token, metric, datasource);
+    return res.status(200).send('Dashboard Updated');
+});
 
 
 
 
 
-    app.use('*', (req, res) => {
-        res.status(404).send('Not Found');
-    });
+app.use('*', (req, res) => {
+    res.status(404).send('Not Found');
+});
 
-    // global error handler
-    app.use((err, req, res, next) => {
-        const defaultErr = {
-            log: 'Express error handler caught unknown middleware error',
-            status: 500,
-            message: { err: 'An error occurred' },
-        };
-        const errorObj = Object.assign({}, defaultErr, err);
-        console.log(errorObj.log);
-        return res.status(errorObj.status).json(errorObj.message);
-    });
+// global error handler
+app.use((err, req, res, next) => {
+    const defaultErr = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 500,
+        message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+});
 
-    app.listen(port, () => console.log(`Example app listening on port ${port}! chronos_npm_package Server Loaded`))
+app.listen(port, () => console.log(`Example app listening on port ${port}! chronos_npm_package Server Loaded`))
