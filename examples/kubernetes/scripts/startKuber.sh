b7c782779978f1ba2c340baf7a2400a227c84a12
Haoyu <liu_haoyu@berkeley.edu>
2023-08-17 09:17:44 -0700

    change workflow

--- examples/kubernetes/scripts/startKuber.sh
+++ examples/kubernetes/scripts/startKuber.sh
@@ -1,9 +1,2 @@
-kubectl apply -f ../launch/clusterRole.yml
-kubectl apply -f ../launch/promConfig.yml
-kubectl apply -f ../launch/prometheus.yml
-kubectl apply -f ../launch/grafana-datasource-config.yml
-kubectl apply -f ../launch/grafana.yml
-kubectl apply -f ../launch/grafanaService.yml
-sleep 10
 kubectl apply -f ../launch/backend.yml
 kubectl apply -f ../launch/frontend.yml
