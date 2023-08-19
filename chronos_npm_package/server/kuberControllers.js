a02eaaba78b42dafd3db69061db25a8fc7bdc23e
Haoyu <liu_haoyu@berkeley.edu>
2023-08-18 23:38:41 -0700

    start kenermetes getting data

---
+++ chronos_npm_package/server/kuberControllers.js
@@ -0,0 +1,26 @@
+const k8s = require('@kubernetes/client-node');
+const kuberControllers = {};
+
+kuberControllers.getData = async (req, res, next) => {
+    try {
+        const kc = new k8s.KubeConfig();
+        kc.loadFromDefault();
+
+        const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+        const main = async () => {
+            try {
+                const podRes = await k8sApi.listNamespacedPod('default');
+                console.log('Pod: ', podRes.body);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        main();
+    } catch (err) {
+        return next(err);
+    }
+}
+
+module.exports = kuberControllers;
