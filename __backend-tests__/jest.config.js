079127a8b50d55dff6288ea9e22882b69aa3f62b
Eisha Kaushal <eishakaushal@MacBook-Pro.local>
2023-08-22 13:08:12 -0400

    modified backend tests

--- __backend-tests__/jest.config.js
+++ __backend-tests__/jest.config.js
@@ -1,18 +1,5 @@
 module.exports = {
-  //   testEnvironment: 'node', // Use the Node.js environment for testing
-  //   roots: ['<rootDir>/controllers'], // Set the root directory for test files
 
-  //   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-
-  //   // Code coverage settings
-  //   collectCoverage: true,
-  //   coverageDirectory: 'coverage',
-
-  //   // Specify the test path patterns to ignore frontend tests
-  //   testPathIgnorePatterns: ['/node_modules/', '/__tests__/'],
-  // };
-
-  // =======
   roots: ['<rootDir>'], // Set the root directory for test files (adjust this path to your test folder)
 
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
@@ -22,4 +9,5 @@ module.exports = {
   coverageDirectory: 'coverage',
   // Specify the test path patterns to ignore (frontend tests)
   testPathIgnorePatterns: ['/node_modules/', '/__tests__/'],
+  testTimeout: 40000
 };
