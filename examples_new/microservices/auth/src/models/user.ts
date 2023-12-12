a606c4997e47f414f17e32f4e19597c844414e65
seantokuzo <seantokuzo@gmail.com>
2023-12-12 14:40:08 -0800

    common modules update

--- examples_new/microservices/auth/src/models/user.ts
+++ examples_new/microservices/auth/src/models/user.ts
@@ -1,16 +1,25 @@
-import { DbConnectionError,errorHandler } from "@chronosrx/common";
+import { DbConnectionError, errorHandler } from '@chronosrx/common';
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-.connect(MONGO_URI,{
+  .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(()=>console.log(`Connected to MongoDB Database`))
-.catch((err)=> {throw new DbConnectionError()} );
+  })
+  .then(() => console.log(`Connected to MongoDB Database`))
+  .catch(err => {
+    throw new DbConnectionError();
+  });
 
 const Schema = mongoose.Schema;
-const
+const UserSchema = new Schema({
+  username: { type: String, require: true },
+  password: { type: String, require: true },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
