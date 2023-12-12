78a2b9f7aff252446c9f58541b2d31bed26640eb
seantokuzo <seantokuzo@gmail.com>
2023-12-12 09:15:23 -0800

    Updates to common package

--- examples_new/microservices/common/src/errors/not-found-error.ts
+++ examples_new/microservices/common/src/errors/not-found-error.ts
@@ -1,14 +1,17 @@
 import { CustomError } from './custom-error';
 
+// Custom error for NotFound
 export class NotFoundError extends CustomError {
   statusCode = 404;
 
   constructor() {
+    // This message will be console logged in error handling middleware
     super('Not found error');
 
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 
+  // Formats the error message to send back to the client
   formatError() {
     return { message: 'Not found error' };
   }
