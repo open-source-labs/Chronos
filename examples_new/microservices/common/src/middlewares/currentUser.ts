bd0fe2578fd2cddc75fb7ffab3e474dcbcd536d6
seantokuzo <seantokuzo@gmail.com>
2023-12-14 08:21:02 -0800

    Updates to common package

---
+++ examples_new/microservices/common/src/middlewares/currentUser.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+interface UserPayload extends JwtPayload {
+  userId: string;
+}
+
+export interface CurrentUserRequest extends Request {
+  currentUser?: string;
+}
+//middleware to check if there is a cookie
+// if there is a cookie, we decode jwt and attach the current user id
+export const currentUser = (req: CurrentUserRequest, res: Response, next: NextFunction) => {
+  const { token } = req.cookies;
+
+  if (!token) return next();
+
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_KEY!) as UserPayload;
+    req.currentUser = payload.userId;
+    return next()
+  } catch (err) {
+    throw new Error('🎃Internal Error');
+  }
+};
