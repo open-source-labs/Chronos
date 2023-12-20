dcab1d1874c9d2784b4387e9c9709a26511d91b5
Zhongyan Liang <liangzhongyan831@gmail.com>
2023-12-19 20:34:46 -0500

    orders

---
+++ examples_new/microservices/orders/src/models/Order.ts
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose';
+//define type of objects that is being passed into build method
+interface OrderAttrs {
+  itemId: string;
+  amount: number;
+  totalPrice: number;
+  buyerId: string;
+  sellerId: string;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+//create inventory data in the database in this shape
+interface OrderDoc extends mongoose.Document {
+  itemId: string;
+  amount: number;
+  totalPrice: number;
+  buyerId: string;
+  sellerId: string;
+}
+
+//create the Schema in mongoose with defined requirements
+const OrderSchema = new mongoose.Schema(
+  {
+    itemId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    buyerId: {
+      type: String,
+      require: true,
+    },
+    sellerId: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    //anytime we create Json formatted data, transform the user document as following
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+OrderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    itemId: attrs.itemId,
+    amount: attrs.amount,
+    totalPrice: attrs.totalPrice,
+    buyerId: attrs.buyerId,
+    sellerId: attrs.sellerId,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', OrderSchema);
+
+export { Order };
--- examples_new/microservices/orders/src/routes/event-router.ts
+++ examples_new/microservices/orders/src/routes/event-router.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import { Events } from '@chronosrx/common';
+import { Order } from '../models/Order';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { event } = req.body;
-  // console.log('Event received:', event);
+  console.log(event);
+  switch (event.type) {
+    case Events.ITEM_CREATED:
+      const newOrder = Order.build(event.payload);
+      await newOrder.save();
+      break;
+    default:
+      res.send({});
+  }
 
-  res.send({ message: 'Event received' });
+  res.send({ message: '🎃 Event received' });
 });
 
 export default router;
