6c6bbe06c26a5703e15e9d6b1428bb865081b0b3
Zhongyan Liang <liangzhongyan831@gmail.com>
2023-12-21 14:12:47 -0500

    order last push

--- examples_new/microservices/orders/src/routes/event-router.ts
+++ examples_new/microservices/orders/src/routes/event-router.ts
@@ -1,21 +1,20 @@
-import { EventTypes } from '@chronosrx/common';
+import { EventTypes, Events } from '@chronosrx/common';
 import express from 'express';
-import { User } from '../models/user';
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
   const event: Events = req.body.event;
   console.log(event);
-  switch (event.type) {
-    case EventTypes.USER_CREATED:
-      // console.log(event);
-      const newUser = User.build(event.payload);
-      await newUser.save();
-      break;
-    default:
-      res.send({});
-  }
+  // switch (event.type) {
+  //   case EventTypes.USER_CREATED:
+  //     // console.log(event);
+  //     const newUser = User.build(event.payload);
+  //     await newUser.save();
+  //     break;
+  // default:
+  //   res.send({});
+  // }
   res.send({ message: 'Event received' });
 });
 
