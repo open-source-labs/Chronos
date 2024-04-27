a6903d68ef22789c6e98809cad038e29c763923a
Mike Tagg <mdtagg91@gmail.com>
2024-04-26 20:17:57 -0700

    extracted styles from occupied component
    styles block at the top of occupied component was moved
    into a helper function. Tried to only switch the different
    styles (background-color, box shadow and another background color)
    Ran into trouble becuase those styles depend on the mode state which
    is defined in a context which cant be used outside a function component.
    Ended up just moving both styling functions whole

--- app/components/Occupied/Occupied.tsx
+++ app/components/Occupied/Occupied.tsx
@@ -8,8 +8,6 @@ import React, { useContext, useEffect, useState } from 'react';
 
 // MATERIAL UI METHODS
 import { Modal, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { BaseCSSProperties } from '@material-ui/core/styles/withStyles';
 
 // MATERIAL UI ICONS
 import AddCircleOutlineTwoToneIcon from '@material-ui/icons/AddCircleOutlineTwoTone';
@@ -35,10 +33,7 @@ import SearchBar from '../SearchBar';
 import DashboardIcons from '../DashboardIcons/DashboardIcons';
 import ApplicationsCard from '../ApplicationsCard/ApplicationsCard';
 
-// TYPESCRIPT
-interface StyleProps {
-  root: BaseCSSProperties;
-}
+import { useStylesLight, useStylesDark } from './helpers/muiHelper'
 
 //v10: Memoized function, without any props. Should theoretically be called only once.
 const Occupied = React.memo(() => {
@@ -59,117 +54,10 @@ const Occupied = React.memo(() => {
     getApplications();
   }, [user]);
 
-  // Conditional Rendering of UI Modals for Light and Dark Mode
-  // Theme, StyleProps
-  const useStylesDark = makeStyles<StyleProps>(theme => ({
-    // ALL CARDS
-    paper: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignContent: 'center',
-      alignItems: 'center',
-      position: 'relative',
-      overflow: 'visible',
-      height: 280,
-      width: 280,
-      textAlign: 'center',
-      color: '#888888',
-      whiteSpace: 'nowrap',
-      backgroundColor: 'lightgray',
-      borderRadius: 3,
-      border: '0',
-      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-      '&:hover, &.Mui-focusVisible': {
-        backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        color: '#ffffff',
-        fontWeight: 600,
-      },
-    },
-    iconbutton: {
-      boxShadow: 'none',
-      color: 'none',
-      visibility: 'hidden',
-    },
-    btnStyle: {
-      position: 'absolute',
-      top: -10,
-      left: -72,
-      margin: '0',
-      color: '#eeeeee',
-      borderRadius: '0',
-      backgroundColor: 'none',
-      visibility: 'visible',
-    },
-    icon: {
-      width: '75px',
-      height: '75px',
-      boxShadow: 'none',
-    },
-
-    // ALL CARDS: CONTENT
-    fontStyles: {
-      fontSize: '18px',
-      fontFamily: 'Roboto',
-      fontWeight: 300,
-      color: '#444d56',
-    },
-  }));
-
-  const useStylesLight = makeStyles<StyleProps>(theme => ({
-    // ALL CARDS
-    paper: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignContent: 'center',
-      alignItems: 'center',
-      position: 'relative',
-      overflow: 'visible',
-      height: 280,
-      width: 280,
-      textAlign: 'center',
-      color: '#888888',
-      whiteSpace: 'nowrap',
-      backgroundColor: 'white',
-      borderRadius: 3,
-      border: '0',
-      boxShadow:
-        '0 6px 6px 0 rgba(153, 153, 153, 0.14), 0 6px 6px -2px rgba(153, 153, 153, 0.2), 0 6px 8px 0 rgba(153, 153, 153, 0.12)',
-      '&:hover, &.Mui-focusVisible': {
-        backgroundColor: `#3788fc`,
-        color: '#ffffff',
-        fontWeight: 600,
-      },
-    },
-    iconbutton: {
-      boxShadow: 'none',
-      color: 'none',
-      visibility: 'hidden',
-    },
-    btnStyle: {
-      position: 'absolute',
-      top: -10,
-      left: -72,
-      margin: '0',
-      color: '#eeeeee',
-      borderRadius: '0',
-      backgroundColor: 'none',
-      visibility: 'visible',
-    },
-    icon: {
-      width: '75px',
-      height: '75px',
-      boxShadow: 'none',
-    },
-    // ALL CARDS: CONTENT
-    fontStyles: {
-      fontSize: '18px',
-      fontFamily: 'Roboto',
-      fontWeight: 300,
-      color: '#444d56',
-    },
-  }));
-
   const classes = mode === 'light' ? useStylesLight({}) : useStylesDark({});
+
+  // const classes = useStylesTest({})
+  console.log(classes)
 
   return (
     <div className="entireArea">
