3e0afbf6d4f633e8d1ea87dd38ebab866e25b5fe
Mike Tagg <mdtagg91@gmail.com>
2024-04-28 16:36:46 -0700

    explanations added

--- app/containers/MainContainer.tsx
+++ app/containers/MainContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import About from '../components/About';
 import Contact from '../components/Contact';
