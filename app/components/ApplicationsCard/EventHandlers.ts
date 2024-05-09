73895c5f48c36e25ad20720a0ab65b3ac1b2d024
TedPham397 <thinhphamart@gmail.com>
2024-05-09 15:28:03 -0700

    created eventHandler.ts for ApplicationCard.tsx, the file itself hold so many functions

---
+++ app/components/ApplicationsCard/EventHandlers.ts
@@ -0,0 +1,42 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { DashboardContext } from "../../context/DashboardContext";
+import { ApplicationContext } from "../../context/ApplicationContext";
+
+type ClickEvent = React.MouseEvent<HTMLElement>;
+
+interface EventHandlersProps {
+  application: any[];
+  setModal: (modalState: { isOpen: boolean; type: string }) => void;
+}
+
+export const useEventHandlers = ({ application, setModal }: EventHandlersProps) => {
+  const { deleteApp, user } = useContext(DashboardContext);
+  const { setAppIndex, setApp, setServicesData, app, example, connectToDB, setChart } = useContext(ApplicationContext);
+  const navigate = useNavigate();
+
+  const handleClick = (selectedApp: string, selectedService: string, i: number) => {
+    const services = ['auth', 'client', 'event-bus', 'items', 'inventory', 'orders'];
+    setAppIndex(i);
+    setApp(selectedApp);
+    if (['AWS', 'AWS/EC2', 'AWS/ECS', 'AWS/EKS'].includes(selectedService)) {
+      navigate(`/aws/:${app}`, { state: { typeOfService: selectedService } });
+    } else if (example) {
+      setServicesData([]);
+      setChart('communications');
+      connectToDB(user, i, app, application[2], application[1]);
+      navigate(`/applications/example/${services.join(' ')}`);
+    } else {
+      setServicesData([]);
+      setModal({ isOpen: true, type: 'serviceModal' });
+    }
+  };
+
+  const confirmDelete = (event: ClickEvent, i: number) => {
+    event.stopPropagation();
+    const message = `The application '${app}' will be permanently deleted. Continue?`;
+    if (confirm(message)) deleteApp(i, "");
+  };
+
+  return { handleClick, confirmDelete };
+};
