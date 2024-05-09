bde42f80adf1cebcf5d9f7a7c8657ce1f517183d
Stephen Yang <stephen.yang98@gmail.com>
2024-05-09 16:54:24 -0700

    updated serviceslink component with deconstructed props

--- app/modals/ServicesModal/ServicesModal.tsx
+++ app/modals/ServicesModal/ServicesModal.tsx
@@ -26,6 +26,7 @@ const ServicesModal: React.FC<ServicesModalProps> = React.memo(({ i, app }) => {
   const { user, applications } = useContext(DashboardContext);
   const { servicesData, connectToDB } = useContext(ApplicationContext);
   const [services, setServices] = useState<Array<string>>([]);
+  console.log(services);
   const [ cardName,dbType,dbURI,description,serviceType ] = applications[i]
 
 
