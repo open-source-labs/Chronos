966571a5c63c43bf7ff9280fc03fbd6225a40244
ashleybisram <115745574+ashleybisram@users.noreply.github.com>
2025-01-21 12:42:31 -0500

    update names in TeamMembers. update names in PastContributors.tsx

--- app/components/About/PastContributors.tsx
+++ app/components/About/PastContributors.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useStylingContext } from './StylingContext';
 
 const contributors = [
-  'Haoyu', 'Eisha', 'Edwin', 'Tyler', 'Snow', 'Taylor', 'Tim', 'Roberto', 'Nachiket', 'Tiffany', 'Bruno', 'Danny', 'Vince',
+  'Mike', 'Stephen', 'Ted', 'Sofia', 'Haoyu', 'Eisha', 'Edwin', 'Tyler', 'Snow', 'Taylor', 'Tim', 'Roberto', 'Nachiket', 'Tiffany', 'Bruno', 'Danny', 'Vince',
   'Matt', 'Derek', 'Kit', 'Grace', 'Jen', 'Patty', 'Stella', 'Michael', 'Ronelle', 'Todd',
   'Greg', 'Brianna', 'Brian', 'Alon', 'Alan', 'Ousman', 'Ben', 'Chris', 'Jenae', 'Tim',
   'Kirk', 'Jess', 'William', 'Alexander', 'Elisa', 'Josh', 'Troy', 'Gahl', 'Brisa', 'Kelsi', 
